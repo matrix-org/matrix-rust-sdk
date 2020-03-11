2b389a84401d5f94b50e5d265188945060b750ef
Damir Jelić <poljar@termina.org.uk>
2020-03-11 11:44:45 +0100

    crypto: Mark the Account as Send.

--- src/crypto/olm.rs
+++ src/crypto/olm.rs
@@ -32,6 +32,12 @@ impl fmt::Debug for Account {
     }
 }
 
+/// Marking this as Send is safe because nothing will modify the pointer under
+/// us from the C side. Sync on the other hand is unsafe since libolm doesn't do
+/// any synchronization. We're wrapping the whole Olm machine inside a Mutex to
+/// get Sync for it
+unsafe impl Send for Account {}
+
 impl Account {
     /// Create a new account.
     pub fn new() -> Self {
