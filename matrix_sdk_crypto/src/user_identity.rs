8b5bb7d8c5620f78e47426d7f373bcc544191ada
Damir Jelić <poljar@termina.org.uk>
2020-09-02 13:54:04 +0200

    crypto: Remove the deserialize implementations for our user identity.
    Deriving Serialize/Deserialize for an AtomicBool doesn't seem to be
    implemented under WASM. So remove the derives for now.

--- matrix_sdk_crypto/src/user_identity.rs
+++ matrix_sdk_crypto/src/user_identity.rs
@@ -234,7 +234,7 @@ impl SelfSigningPubkey {
 }
 
 /// Enum over the different user identity types we can have.
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub enum UserIdentities {
     /// Our own user identity.
     Own(OwnUserIdentity),
@@ -371,7 +371,7 @@ impl UserIdentity {
 ///
 /// This identity can verify other identities as well as devices belonging to
 /// the identity.
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone)]
 pub struct OwnUserIdentity {
     user_id: Arc<UserId>,
     master_key: MasterPubkey,
