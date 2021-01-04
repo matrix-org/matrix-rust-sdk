d84a852ae95d944e6e22b3c5c1b44be1a7d31702
Damir Jelić <poljar@termina.org.uk>
2021-01-04 14:06:07 +0100

    matrix-sdk: Bump ruma to a released version.

--- matrix_sdk_common/src/lib.rs
+++ matrix_sdk_common/src/lib.rs
@@ -1,13 +1,14 @@
 pub use assign::assign;
 pub use instant;
-pub use js_int;
 pub use ruma::{
     api::{
         client as api,
         error::{FromHttpRequestError, FromHttpResponseError, IntoHttpError, ServerError},
         AuthScheme, EndpointError, OutgoingRequest,
     },
-    directory, encryption, events, identifiers, presence, push,
+    directory, encryption, events,
+    events::exports::js_int,
+    identifiers, presence, push,
     serde::{CanonicalJsonValue, Raw},
     thirdparty, Outgoing,
 };
