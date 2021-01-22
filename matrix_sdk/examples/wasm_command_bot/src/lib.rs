d10b85a05d906ee1c8b845c491fdfe34d2435150
Damir Jelić <poljar@termina.org.uk>
2021-01-22 18:14:08 +0100

    matrix-sdk: Fix our wasm command bot example

--- matrix_sdk/examples/wasm_command_bot/src/lib.rs
+++ matrix_sdk/examples/wasm_command_bot/src/lib.rs
@@ -1,13 +1,11 @@
-#![type_length_limit = "1702124"]
-
 use matrix_sdk::{
-    api::r0::sync::sync_events::Response as SyncResponse,
+    deserialized_responses::SyncResponse,
     events::{
         room::message::{MessageEventContent, TextMessageEventContent},
         AnyMessageEventContent, AnySyncMessageEvent, AnySyncRoomEvent, SyncMessageEvent,
     },
     identifiers::RoomId,
-    Client, ClientConfig, LoopCtrl, SyncSettings,
+    Client, LoopCtrl, SyncSettings,
 };
 use url::Url;
 use wasm_bindgen::prelude::*;
@@ -46,10 +44,8 @@ impl WasmBot {
 
         for (room_id, room) in response.rooms.join {
             for event in room.timeline.events {
-                if let Ok(event) = event.deserialize() {
-                    if let AnySyncRoomEvent::Message(AnySyncMessageEvent::RoomMessage(ev)) = event {
-                        self.on_room_message(&room_id, ev).await
-                    }
+                if let AnySyncRoomEvent::Message(AnySyncMessageEvent::RoomMessage(ev)) = event {
+                    self.on_room_message(&room_id, ev).await
                 }
             }
         }
@@ -60,13 +56,14 @@ impl WasmBot {
 
 #[wasm_bindgen]
 pub async fn run() -> Result<JsValue, JsValue> {
-    let homeserver_url = "http://localhost:8008";
-    let username = "user";
-    let password = "password";
+    console_error_panic_hook::set_once();
 
-    let client_config = ClientConfig::new();
+    let homeserver_url = "http://localhost:8008";
+    let username = "example";
+    let password = "wordpass";
+
     let homeserver_url = Url::parse(&homeserver_url).unwrap();
-    let client = Client::new_with_config(homeserver_url, client_config).unwrap();
+    let client = Client::new(homeserver_url).unwrap();
 
     client
         .login(username, password, None, Some("rust-sdk-wasm"))
