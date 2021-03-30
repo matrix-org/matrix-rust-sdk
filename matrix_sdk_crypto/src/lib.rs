ff683602f250e90e83acbbd580252fa8ce6c541a
Damir Jelić <poljar@termina.org.uk>
2021-03-30 13:52:57 +0200

    crypto: Export the KeysExport error

--- matrix_sdk_crypto/src/lib.rs
+++ matrix_sdk_crypto/src/lib.rs
@@ -42,7 +42,7 @@ mod verification;
 pub use error::{MegolmError, OlmError};
 pub use file_encryption::{
     decrypt_key_export, encrypt_key_export, AttachmentDecryptor, AttachmentEncryptor,
-    DecryptorError, EncryptionInfo,
+    DecryptorError, EncryptionInfo, KeyExportError,
 };
 pub use identities::{
     Device, LocalTrust, OwnUserIdentity, ReadOnlyDevice, UserDevices, UserIdentities, UserIdentity,
