4cdb03e64b54bb82175889fc5d5ad8ebbd53f37e
Julian Sparber <julian@sparber.net>
2021-06-15 13:07:34 +0200

    matrix-sdk: use media cache for avatar requests

--- matrix_sdk/src/client.rs
+++ matrix_sdk/src/client.rs
@@ -781,20 +781,20 @@ impl Client {
 
     /// Gets the avatar of the owner of the client, if set.
     ///
-    /// Returns the avatar. No guarantee on the size of the image is given.
-    /// If no size is given the full-sized avatar will be returned.
+    /// Returns the avatar.
+    /// If a thumbnail is requested no guarantee on the size of the image is
+    /// given.
     ///
     /// # Arguments
     ///
-    /// * `width` - The desired width of the avatar.
-    ///
-    /// * `height` - The desired height of the avatar.
+    /// * `format` - The desired format of the avatar.
     ///
     /// # Example
     /// ```no_run
     /// # use futures::executor::block_on;
     /// # use matrix_sdk::Client;
     /// # use matrix_sdk::identifiers::room_id;
+    /// # use matrix_sdk::media::MediaFormat;
     /// # use url::Url;
     /// # let homeserver = Url::parse("http://example.com").unwrap();
     /// # block_on(async {
@@ -802,24 +802,15 @@ impl Client {
     /// let client = Client::new(homeserver).unwrap();
     /// client.login(user, "password", None, None).await.unwrap();
     ///
-    /// if let Some(avatar) = client.avatar(Some(96), Some(96)).await.unwrap() {
+    /// if let Some(avatar) = client.avatar(MediaFormat::File).await.unwrap() {
     ///     std::fs::write("avatar.png", avatar);
     /// }
     /// # })
     /// ```
-    pub async fn avatar(&self, width: Option<u32>, height: Option<u32>) -> Result<Option<Vec<u8>>> {
-        // TODO: try to offer the avatar from cache, requires avatar cache
+    pub async fn avatar(&self, format: MediaFormat) -> Result<Option<Vec<u8>>> {
         if let Some(url) = self.avatar_url().await? {
-            if let (Some(width), Some(height)) = (width, height) {
-                let request =
-                    get_content_thumbnail::Request::from_url(&url, width.into(), height.into())?;
-                let response = self.send(request, None).await?;
-                Ok(Some(response.file))
-            } else {
-                let request = get_content::Request::from_url(&url)?;
-                let response = self.send(request, None).await?;
-                Ok(Some(response.file))
-            }
+            let request = MediaRequest { media_type: MediaType::Uri(url), format };
+            Ok(Some(self.get_media_content(&request, true).await?))
         } else {
             Ok(None)
         }
