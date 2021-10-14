matrix-org/matrix-rust-sdk

commit cb8a42a529f15e7755816415da842d6145ea2e3f

Upgrade to latest Ruma git main

crates/matrix-sdk/src/room/joined.rs
@@ -29,12 +29,13 @@ use ruma::{
         room::{
             message::{
                 AudioMessageEventContent, FileMessageEventContent, ImageMessageEventContent,
-                MessageEventContent, MessageType, VideoMessageEventContent,
+                MessageEventContent as RoomMessageEventContent, MessageType,
+                VideoMessageEventContent,
             },
             EncryptedFile,
         },
         tag::TagInfo,
-        AnyMessageEventContent, AnyStateEventContent, EventContent,
+        MessageEventContent, StateEventContent,
     },
     receipt::ReceiptType,
     serde::Raw,
@@ -332,9 +333,6 @@ impl Joined {
     /// If the encryption feature is enabled this method will transparently
     /// encrypt the room message if this room is encrypted.
     ///
-    /// **Note**: This method does not support sending custom events, the
-    /// [`send_raw()`] method can be used for that instead.
-    ///
     /// # Arguments
     ///
     /// * `content` - The content of the message event.
@@ -362,10 +360,7 @@ impl Joined {
     /// # use std::convert::TryFrom;
     /// use matrix_sdk::{
     ///     uuid::Uuid,
-    ///     ruma::events::{
-    ///         AnyMessageEventContent,
-    ///         room::message::{MessageEventContent, TextMessageEventContent},
-    ///     }
+    ///     ruma::events::room::message::{MessageEventContent, TextMessageEventContent},
     /// };
     /// # block_on(async {
     /// # let homeserver = Url::parse("http://localhost:8080")?;
@@ -382,20 +377,18 @@ impl Joined {
     /// # matrix_sdk::Result::Ok(()) });
     /// ```
     ///
-    /// [`send_raw()`]: #method.send_raw
     /// [`SyncMessageEvent`]: ruma::events::SyncMessageEvent
     /// [`Unsigned`]: ruma::events::Unsigned
     /// [`transaction_id`]: ruma::events::Unsigned#structfield.transaction_id
     pub async fn send(
         &self,
-        content: impl Into<AnyMessageEventContent>,
+        content: impl MessageEventContent,
         txn_id: Option<Uuid>,
     ) -> Result<send_message_event::Response> {
-        let content = content.into();
-        let event_type = content.event_type().to_owned();
-        let content = serde_json::to_value(content)?;
-
-        self.send_raw(content, &event_type, txn_id).await
+        let txn_id = txn_id.unwrap_or_else(Uuid::new_v4).to_string();
+        let request = send_message_event::Request::new(self.inner.room_id(), &txn_id, &content)?;
+        let response = self.client.send(request, None).await?;
+        Ok(response)
     }
 
     /// Send a room message to this room from a json `Value`.
@@ -464,7 +457,7 @@ impl Joined {
         let txn_id = txn_id.unwrap_or_else(Uuid::new_v4).to_string();
 
         #[cfg(not(feature = "encryption"))]
-        let content = Raw::from_json(serde_json::value::to_raw_value(&content)?);
+        let content = serde_json::value::to_raw_value(&content)?;
 
         #[cfg(feature = "encryption")]
         let (content, event_type) = if self.is_encrypted() {
@@ -480,17 +473,19 @@ impl Joined {
 
             let encrypted_content =
                 olm.encrypt_raw(self.inner.room_id(), content, event_type).await?;
+            let raw_content = serde_json::value::to_raw_value(&encrypted_content)
+                .expect("Failed to serialize encrypted event");
 
-            (AnyMessageEventContent::RoomEncrypted(encrypted_content).into(), "m.room.encrypted")
+            (raw_content, "m.room.encrypted")
         } else {
-            (Raw::from_json(serde_json::value::to_raw_value(&content)?), event_type)
+            (serde_json::value::to_raw_value(&content)?, event_type)
         };
 
         let request = send_message_event::Request::new_raw(
             self.inner.room_id(),
             &txn_id,
             event_type,
-            content,
+            Raw::from_json(content),
         );
 
         let response = self.client.send(request, None).await?;
@@ -609,8 +604,7 @@ impl Joined {
             )),
         };
 
-        self.send(AnyMessageEventContent::RoomMessage(MessageEventContent::new(content)), txn_id)
-            .await
+        self.send(RoomMessageEventContent::new(content), txn_id).await
     }
 
     /// Send a room state event to the homeserver.
@@ -623,11 +617,6 @@ impl Joined {
     ///
     /// * `state_key` - A unique key which defines the overwriting semantics for
     /// this piece of room state. This value is often a zero-length string.
-    ///
-    /// **Note**: To send custom events use the raw variant of this method,
-    /// [`send_state_event_raw()`].
-    ///
-    /// [`send_state_event_raw()`]: #method.send_state_event_raw
     ///
     /// # Example
     ///
@@ -656,13 +645,12 @@ impl Joined {
     /// ```
     pub async fn send_state_event(
         &self,
-        content: impl Into<AnyStateEventContent>,
+        content: impl StateEventContent,
         state_key: &str,
-    ) -> HttpResult<send_state_event::Response> {
-        let content = content.into();
-        let request = send_state_event::Request::new(self.inner.room_id(), state_key, &content);
-
-        self.client.send(request, None).await
+    ) -> Result<send_state_event::Response> {
+        let request = send_state_event::Request::new(self.inner.room_id(), state_key, &content)?;
+        let response = self.client.send(request, None).await?;
+        Ok(response)
     }
 
     /// Send a raw room state event to the homeserver.

crates/matrix-qrcode/Cargo.toml
@@ -28,5 +28,5 @@ byteorder = "1.4.3"
 image = { version = "0.23.14", optional = true }
 qrcode = { version = "0.12.0", default-features = false }
 rqrr = { version = "0.3.2", optional = true }
-ruma-identifiers = "0.20.0"
+ruma-identifiers = { git = "https://github.com/ruma/ruma", rev = "0101e110f" }
 thiserror = "1.0.25"

crates/matrix-sdk-appservice/Cargo.toml
@@ -40,7 +40,8 @@ url = "2"
 warp = { version = "0.3.1", optional = true, default-features = false }
 
 [dependencies.ruma]
-version = "0.4.0"
+git = "https://github.com/ruma/ruma"
+rev = "0101e110f"
 features = ["client-api-c", "appservice-api-s", "unstable-pre-spec"]
 
 [dev-dependencies]

crates/matrix-sdk-base/Cargo.toml
@@ -35,7 +35,7 @@ matrix-sdk-common = { version = "0.4.0", path = "../matrix-sdk-common" }
 matrix-sdk-crypto = { version = "0.4.0", path = "../matrix-sdk-crypto", optional = true }
 pbkdf2 = { version = "0.9.0", default-features = false, optional = true }
 rand = { version = "0.8.4", optional = true }
-ruma = { version = "0.4.0", features = ["client-api-c", "unstable-pre-spec"] }
+ruma = { git = "https://github.com/ruma/ruma", rev = "0101e110f", features = ["client-api-c", "unstable-pre-spec"] }
 serde = { version = "1.0.126", features = ["rc"] }
 serde_json = "1.0.64"
 sha2 = { version = "0.9.5", optional = true }

crates/matrix-sdk-base/src/client.rs
@@ -648,19 +648,8 @@ impl BaseClient {
         &self,
         response: api::sync::sync_events::Response,
     ) -> Result<SyncResponse> {
-        #[cfg(test)]
-        let api::sync::sync_events::Response {
-            next_batch,
-            rooms,
-            presence,
-            account_data,
-            to_device,
-            device_lists,
-            device_one_time_keys_count,
-            __test_exhaustive: _,
-        } = response;
-
-        #[cfg(not(test))]
+        #[allow(unknown_lints)] // New lint to be released in 1.57.0
+        #[warn(non_exhaustive_omitted_patterns)]
         let api::sync::sync_events::Response {
             next_batch,
             rooms,

crates/matrix-sdk-common/Cargo.toml
@@ -14,7 +14,7 @@ version = "0.4.1"
 
 [dependencies]
 async-trait = "0.1.50"
-ruma = { version = "0.4.0", features = ["client-api-c"] }
+ruma = { git = "https://github.com/ruma/ruma", rev = "0101e110f", features = ["client-api-c"] }
 serde = "1.0.126"
 
 [target.'cfg(not(target_arch = "wasm32"))'.dependencies]

crates/matrix-sdk-crypto/Cargo.toml
@@ -36,7 +36,7 @@ matrix-qrcode = { version = "0.2.0", path = "../matrix-qrcode", optional = true 
 matrix-sdk-common = { version = "0.4.0", path = "../matrix-sdk-common" }
 olm-rs = { version = "2.0.1", features = ["serde"] }
 pbkdf2 = { version = "0.9.0", default-features = false }
-ruma = { version = "0.4.0", features = ["client-api-c", "unstable-pre-spec"] }
+ruma = { git = "https://github.com/ruma/ruma", rev = "0101e110f", features = ["client-api-c", "unstable-pre-spec"] }
 serde = { version = "1.0.126", features = ["derive", "rc"] }
 serde_json = "1.0.64"
 sha2 = "0.9.5"

crates/matrix-sdk-crypto/src/gossiping/machine.rs
@@ -27,13 +27,13 @@ use matrix_sdk_common::uuid::Uuid;
 use ruma::{
     api::client::r0::keys::claim_keys::Request as KeysClaimRequest,
     events::{
-        forwarded_room_key::ForwardedRoomKeyToDeviceEventContent,
-        room_key_request::{Action, RequestedKeyInfo, RoomKeyRequestToDeviceEventContent},
+        forwarded_room_key::ToDeviceForwardedRoomKeyEventContent,
+        room_key_request::{Action, RequestedKeyInfo, ToDeviceRoomKeyRequestEventContent},
         secret::{
             request::{
-                RequestAction, RequestToDeviceEventContent as SecretRequestEventContent, SecretName,
+                RequestAction, SecretName, ToDeviceRequestEventContent as SecretRequestEventContent,
             },
-            send::SendToDeviceEventContent as SecretSendEventContent,
+            send::ToDeviceSendEventContent as SecretSendEventContent,
         },
         AnyToDeviceEvent, AnyToDeviceEventContent, ToDeviceEvent,
     },
@@ -141,7 +141,7 @@ impl GossipMachine {
     /// Receive a room key request event.
     pub fn receive_incoming_key_request(
         &self,
-        event: &ToDeviceEvent<RoomKeyRequestToDeviceEventContent>,
+        event: &ToDeviceEvent<ToDeviceRoomKeyRequestEventContent>,
     ) {
         self.receive_event(event.clone().into())
     }
@@ -313,7 +313,7 @@ impl GossipMachine {
     /// Handle a single incoming key request.
     async fn handle_key_request(
         &self,
-        event: &ToDeviceEvent<RoomKeyRequestToDeviceEventContent>,
+        event: &ToDeviceEvent<ToDeviceRoomKeyRequestEventContent>,
     ) -> OlmResult<Option<Session>> {
         let key_info = match &event.content.action {
             Action::Request => {
@@ -694,7 +694,7 @@ impl GossipMachine {
     /// Get an outgoing key info that matches the forwarded room key content.
     async fn get_key_info(
         &self,
-        content: &ForwardedRoomKeyToDeviceEventContent,
+        content: &ToDeviceForwardedRoomKeyEventContent,
     ) -> Result<Option<GossipRequest>, CryptoStoreError> {
         let info = RequestedKeyInfo::new(
             content.algorithm.clone(),
@@ -846,7 +846,7 @@ impl GossipMachine {
     pub async fn receive_forwarded_room_key(
         &self,
         sender_key: &str,
-        event: &mut ToDeviceEvent<ForwardedRoomKeyToDeviceEventContent>,
+        event: &mut ToDeviceEvent<ToDeviceForwardedRoomKeyEventContent>,
     ) -> Result<(Option<AnyToDeviceEvent>, Option<InboundGroupSession>), CryptoStoreError> {
         let key_info = self.get_key_info(&event.content).await?;
 
@@ -910,10 +910,10 @@ mod test {
     use matrix_sdk_test::async_test;
     use ruma::{
         events::{
-            forwarded_room_key::ForwardedRoomKeyToDeviceEventContent,
-            room::encrypted::EncryptedToDeviceEventContent,
-            room_key_request::RoomKeyRequestToDeviceEventContent,
-            secret::request::{RequestAction, RequestToDeviceEventContent, SecretName},
+            forwarded_room_key::ToDeviceForwardedRoomKeyEventContent,
+            room::encrypted::ToDeviceEncryptedEventContent,
+            room_key_request::ToDeviceRoomKeyRequestEventContent,
+            secret::request::{RequestAction, SecretName, ToDeviceRequestEventContent},
             AnyToDeviceEvent, ToDeviceEvent,
         },
         room_id,
@@ -1118,7 +1118,7 @@ mod test {
 
         let export = session.export_at_index(10).await;
 
-        let content: ForwardedRoomKeyToDeviceEventContent = export.try_into().unwrap();
+        let content: ToDeviceForwardedRoomKeyEventContent = export.try_into().unwrap();
 
         let mut event = ToDeviceEvent { sender: alice_id(), content };
 
@@ -1165,7 +1165,7 @@ mod test {
 
         let export = session.export_at_index(15).await;
 
-        let content: ForwardedRoomKeyToDeviceEventContent = export.try_into().unwrap();
+        let content: ToDeviceForwardedRoomKeyEventContent = export.try_into().unwrap();
 
         let mut event = ToDeviceEvent { sender: alice_id(), content };
 
@@ -1176,7 +1176,7 @@ mod test {
 
         let export = session.export_at_index(0).await;
 
-        let content: ForwardedRoomKeyToDeviceEventContent = export.try_into().unwrap();
+        let content: ToDeviceForwardedRoomKeyEventContent = export.try_into().unwrap();
 
         let mut event = ToDeviceEvent { sender: alice_id(), content };
 
@@ -1368,7 +1368,7 @@ mod test {
             .unwrap()
             .get(&DeviceIdOrAllDevices::AllDevices)
             .unwrap();
-        let content: RoomKeyRequestToDeviceEventContent = content.deserialize_as().unwrap();
+        let content: ToDeviceRoomKeyRequestEventContent = content.deserialize_as().unwrap();
 
         alice_machine.mark_outgoing_request_as_sent(id).await.unwrap();
 
@@ -1397,7 +1397,7 @@ mod test {
             .unwrap()
             .get(&DeviceIdOrAllDevices::DeviceId(alice_device_id()))
             .unwrap();
-        let content: EncryptedToDeviceEventContent = content.deserialize_as().unwrap();
+        let content: ToDeviceEncryptedEventContent = content.deserialize_as().unwrap();
 
         bob_machine.mark_outgoing_request_as_sent(id).await.unwrap();
 
@@ -1462,7 +1462,7 @@ mod test {
 
         let event = ToDeviceEvent {
             sender: bob_account.user_id().to_owned(),
-            content: RequestToDeviceEventContent::new(
+            content: ToDeviceRequestEventContent::new(
                 RequestAction::Request(SecretName::CrossSigningMasterKey),
                 bob_account.device_id().to_owned(),
                 "request_id".to_owned(),
@@ -1491,7 +1491,7 @@ mod test {
 
         let event = ToDeviceEvent {
             sender: alice_id(),
-            content: RequestToDeviceEventContent::new(
+            content: ToDeviceRequestEventContent::new(
                 RequestAction::Request(SecretName::CrossSigningMasterKey),
                 second_account.device_id().into(),
                 "request_id".to_owned(),
@@ -1576,7 +1576,7 @@ mod test {
             .unwrap()
             .get(&DeviceIdOrAllDevices::AllDevices)
             .unwrap();
-        let content: RoomKeyRequestToDeviceEventContent = content.deserialize_as().unwrap();
+        let content: ToDeviceRoomKeyRequestEventContent = content.deserialize_as().unwrap();
 
         alice_machine.mark_outgoing_request_as_sent(id).await.unwrap();
 
@@ -1625,7 +1625,7 @@ mod test {
             .unwrap()
             .get(&DeviceIdOrAllDevices::DeviceId(alice_device_id()))
             .unwrap();
-        let content: EncryptedToDeviceEventContent = content.deserialize_as().unwrap();
+        let content: ToDeviceEncryptedEventContent = content.deserialize_as().unwrap();
 
         bob_machine.mark_outgoing_request_as_sent(id).await.unwrap();
 

commit 1c36e62e2c47fb587e7eff6f7b5a8dbe00a8bb40

fix: Unbreak encryption.
And add some docs and logging statements.

cb8a42a529f15e7755816415da842d6145ea2e3f broke encryption because it
elided the call to `send_raw` from `send` and instead did a direct HTTP
request. Since event encryption is only called from `send_raw`, this
made all events cleartext.

crates/matrix-sdk/src/room/joined.rs
@@ -42,6 +42,7 @@ use ruma::{
     EventId, UserId,
 };
 use serde_json::Value;
+use tracing::debug;
 #[cfg(feature = "encryption")]
 use tracing::instrument;
 
@@ -333,6 +334,9 @@ impl Joined {
     /// If the encryption feature is enabled this method will transparently
     /// encrypt the room message if this room is encrypted.
     ///
+    /// **Note**: If you just want to send a custom JSON payload to a room, you
+    /// can use the [`send_raw()`] method for that.
+    ///
     /// # Arguments
     ///
     /// * `content` - The content of the message event.
@@ -407,6 +411,7 @@ impl Joined {
     /// # matrix_sdk::Result::Ok(()) });
     /// ```
     ///
+    /// [`send_raw()`]: #method.send_raw
     /// [`SyncMessageEvent`]: ruma::events::SyncMessageEvent
     /// [`Unsigned`]: ruma::events::Unsigned
     /// [`transaction_id`]: ruma::events::Unsigned#structfield.transaction_id
@@ -415,10 +420,10 @@ impl Joined {
         content: impl MessageEventContent,
         txn_id: Option<Uuid>,
     ) -> Result<send_message_event::Response> {
-        let txn_id = txn_id.unwrap_or_else(Uuid::new_v4).to_string();
-        let request = send_message_event::Request::new(self.inner.room_id(), &txn_id, &content)?;
-        let response = self.client.send(request, None).await?;
-        Ok(response)
+        let event_type = content.event_type().to_owned();
+        let content = serde_json::to_value(content)?;
+
+        self.send_raw(content, &event_type, txn_id).await
     }
 
     /// Send a room message to this room from a json `Value`.
@@ -487,10 +492,18 @@ impl Joined {
         let txn_id = txn_id.unwrap_or_else(Uuid::new_v4).to_string();
 
         #[cfg(not(feature = "encryption"))]
-        let content = serde_json::value::to_raw_value(&content)?;
+        let content = {
+            debug!(
+                "Sending plaintext event to room {} because we don't have encryption support.",
+                self.room_id()
+            );
+            serde_json::value::to_raw_value(&content)?
+        };
 
         #[cfg(feature = "encryption")]
         let (content, event_type) = if self.is_encrypted() {
+            debug!("Sending encrypted event because the room {} is encrypted.", self.room_id());
+
             if !self.are_members_synced() {
                 self.request_members().await?;
                 // TODO query keys here?
@@ -508,6 +521,8 @@ impl Joined {
 
             (raw_content, "m.room.encrypted")
         } else {
+            debug!("Sending plaintext event because the room {} is NOT encrypted.", self.room_id());
+
             (serde_json::value::to_raw_value(&content)?, event_type)
         };
 