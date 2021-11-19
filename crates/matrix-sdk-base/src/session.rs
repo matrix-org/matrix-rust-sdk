66067030012e2c26355bb8edef34f34959f2d7df
Damir Jelić <poljar@termina.org.uk>
2021-11-19 09:12:51 +0100

    docs(base): Remove anyhow from an example since it isn't used

--- crates/matrix-sdk-base/src/session.rs
+++ crates/matrix-sdk-base/src/session.rs
@@ -27,7 +27,6 @@ use serde::{Deserialize, Serialize};
 /// use matrix_sdk_base::Session;
 /// use ruma::{DeviceIdBox, user_id};
 ///
-/// # fn main() -> anyhow::Result<()> {
 /// let session = Session {
 ///     access_token: "My-Token".to_owned(),
 ///     user_id: user_id!("@example:localhost"),
@@ -35,9 +34,6 @@ use serde::{Deserialize, Serialize};
 /// };
 ///
 /// assert_eq!(session.device_id.as_str(), "MYDEVICEID");
-///
-/// # Ok(())
-/// # }
 /// ```
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
 pub struct Session {
