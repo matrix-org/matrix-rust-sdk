4fdada55b87a41826cd997949dec8ea240f6ecd9
Benjamin Kampmann <ben@gnunicorn.org>
2021-11-30 18:50:50 +0100

    fixing typo

--- crates/matrix-sdk-common/src/util.rs
+++ crates/matrix-sdk-common/src/util.rs
@@ -6,6 +6,6 @@ pub fn milli_seconds_since_unix_epoch() -> MilliSecondsSinceUnixEpoch {
     let duration =
         SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("now is always higher");
     let millis =
-        duration.as_millis().try_into().expect("can't convert miliseconds since UNIXEPOCH");
+        duration.as_millis().try_into().expect("can't convert milliseconds since UNIXEPOCH");
     MilliSecondsSinceUnixEpoch(millis)
 }
