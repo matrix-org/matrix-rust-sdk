3de6f60f90a1dd0282588674278633b3c41e6d8f
Benjamin Kampmann <ben@gnunicorn.org>
2021-12-08 13:42:44 +0100

    remove unneccessary imports and compiler flags

--- crates/matrix-sdk-base/src/store/indexeddb_store.rs
+++ crates/matrix-sdk-base/src/store/indexeddb_store.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[allow(unused_imports)]
-use core::convert::{TryFrom, TryInto};
 use std::collections::BTreeSet;
 
 use indexed_db_futures::{prelude::*, web_sys::IdbKeyRange};
