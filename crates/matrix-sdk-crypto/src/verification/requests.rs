f6c4d6b4cac1117778f62be05a5bffadcd66d0a8
Travis Ralston <travisr@matrix.org>
2021-12-27 15:33:00 -0700

    Appease clippy
    This also fails on `main`, but looks to be a change in clippy between 19 days ago and now

--- crates/matrix-sdk-crypto/src/verification/requests.rs
+++ crates/matrix-sdk-crypto/src/verification/requests.rs
@@ -524,9 +524,7 @@ impl VerificationRequest {
 
         if let OutgoingContent::ToDevice(c) = cancel_content {
             let recipients: Vec<Box<DeviceId>> = self
-                .recipient_devices
-                .to_vec()
-                .into_iter()
+                .recipient_devices.iter().cloned()
                 .filter(|d| if let Some(device) = filter_device { &**d != device } else { true })
                 .collect();
 
