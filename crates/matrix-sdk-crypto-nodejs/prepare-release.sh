812cf0750016c801d220c6b1e0ac7b6bb4eea25b
Travis Ralston <travisr@matrix.org>
2021-12-27 14:28:59 -0700

    Fix release steps for reliability

--- crates/matrix-sdk-crypto-nodejs/prepare-release.sh
+++ crates/matrix-sdk-crypto-nodejs/prepare-release.sh
@@ -6,7 +6,9 @@ yarn install
 
 # Remove generated file to ensure the default `yarn install` on the consumer side
 # ends up building its own platform-specific version (if needed).
-rm lib/index.node
+#
+# Forced to ensure repeated builds work (file might not exist)
+rm -f lib/index.node
 
 docker build -t rust-sdk-napi -f release/Dockerfile.linux .
 docker run --rm -v $(pwd)/../..:/src rust-sdk-napi
