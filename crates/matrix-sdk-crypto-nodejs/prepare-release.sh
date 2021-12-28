7274cb4897ce12cb8a820898771d504c3cacb675
Travis Ralston <travisr@matrix.org>
2021-12-28 15:54:23 -0700

    Properly exclude debug build from release

--- crates/matrix-sdk-crypto-nodejs/prepare-release.sh
+++ crates/matrix-sdk-crypto-nodejs/prepare-release.sh
@@ -4,11 +4,5 @@ set -ex
 
 yarn install
 
-# Remove generated file to ensure the default `yarn install` on the consumer side
-# ends up building its own platform-specific version (if needed).
-#
-# Forced to ensure repeated builds work (file might not exist)
-rm -f lib/index.node
-
 docker build -t rust-sdk-napi -f release/Dockerfile.linux .
 docker run --rm -v $(pwd)/../..:/src rust-sdk-napi
