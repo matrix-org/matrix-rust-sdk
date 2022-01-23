c6594edc9260d0cb4da364372943690966a00e16
Jonas Platte <jplatte+git@posteo.de>
2022-01-23 15:52:10 +0100

    Use faster sorting algorithm
    … because Clippy complained.

--- crates/matrix-sdk-crypto/src/verification/sas/helpers.rs
+++ crates/matrix-sdk-crypto/src/verification/sas/helpers.rs
@@ -322,7 +322,7 @@ pub fn get_mac_content(sas: &OlmSas, ids: &SasIds, flow_id: &FlowId) -> Outgoing
     // TODO Add the cross signing master key here if we trust/have it.
 
     let mut keys: Vec<_> = mac.keys().map(|s| s.as_str()).collect();
-    keys.sort();
+    keys.sort_unstable();
     let keys = sas
         .calculate_mac(&keys.join(","), &format!("{}KEY_IDS", &info))
         .expect("Can't calculate SAS MAC");
