95384f0f3378f21921cdee43ff61001243c5cbf5
Benjamin Kampmann <ben@gnunicorn.org>
2022-02-18 18:50:35 +0100

    make encryption in new crates optional

--- crates/matrix-sdk-indexeddb/src/lib.rs
+++ crates/matrix-sdk-indexeddb/src/lib.rs
@@ -1,5 +1,12 @@
 #[cfg(not(target_arch="wasm32"))]
 compile_error!("indexeddb is only support on wasm32. You may want to add --target wasm32-unknown-unknown");
 
-pub mod state_store;
-pub mod cryptostore;
+mod state_store;
+
+#[cfg(feature = "encryption")]
+mod cryptostore;
+
+#[cfg(feature = "encryption")]
+pub use cryptostore::IndexeddbStore as CryptoStore;
+
+pub use state_store::IndexeddbStore as StateStore;
