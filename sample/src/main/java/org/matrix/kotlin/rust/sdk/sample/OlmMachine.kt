c6c05a0ffedcb50d3b92acad99e5253d82ecad5f
ganfra <francoisg@element.io>
2022-03-07 18:38:11 +0100

    Try the generated aar

--- sample/src/main/java/org/matrix/kotlin/rust/sdk/sample/OlmMachine.kt
+++ sample/src/main/java/org/matrix/kotlin/rust/sdk/sample/OlmMachine.kt
@@ -1,16 +1,13 @@
 package org.matrix.kotlin.rust.sdk.sample
 
-import uniffi.olm.OlmMachine
+
 import java.io.File
 class OlmMachine(
     user_id: String,
     device_id: String,
     path: File,
 ) {
-    private val inner: OlmMachine = OlmMachine(user_id, device_id, path.toString())
-
-
-
+    private val inner: uniffi.olm.OlmMachine = uniffi.olm.OlmMachine(user_id, device_id, path.absolutePath)
 
 
 
