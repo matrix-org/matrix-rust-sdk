fd156cefec5bb17b01a0e12bf5c21cc03afe6c18
Jonas Platte <jplatte@element.io>
2022-03-25 16:43:48 +0100

    fix(indexeddb): Add missing as_encoded_string call

--- crates/matrix-sdk-indexeddb/src/safe_encode.rs
+++ crates/matrix-sdk-indexeddb/src/safe_encode.rs
@@ -131,7 +131,7 @@ impl<T: SafeEncode + ?Sized> SafeEncode for Box<T> {
 
 impl SafeEncode for TransactionId {
     fn as_encoded_string(&self) -> String {
-        self.to_string()
+        self.to_string().as_encoded_string()
     }
 }
 
