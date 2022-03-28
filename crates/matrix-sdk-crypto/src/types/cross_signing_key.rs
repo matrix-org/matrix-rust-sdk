4b580179515d41bea58a31a42004b8e7fbb6c2a4
Damir Jelić <poljar@termina.org.uk>
2022-03-28 14:24:38 +0200

    chore(crypto): Add umlauts to a copyright holder
    Co-authored-by: Jonas Platte <jplatte@element.io>

--- crates/matrix-sdk-crypto/src/types/cross_signing_key.rs
+++ crates/matrix-sdk-crypto/src/types/cross_signing_key.rs
@@ -1,5 +1,5 @@
 // Copyright 2021 Devin Ragotzy.
-// Copyright 2021 Timo Kosters.
+// Copyright 2021 Timo Kösters.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
