9c807741406335ee71025b01616301cfc72b8654
Benjamin Kampmann <ben@gnunicorn.org>
2022-04-28 11:06:03 +0200

    feat(common-utils): helper for modified MilliSecondsSinceUnixEpoch

--- crates/matrix-sdk-common/src/util.rs
+++ crates/matrix-sdk-common/src/util.rs
@@ -1,4 +1,4 @@
-use instant::SystemTime;
+use instant::{SystemTime, Duration};
 use ruma::{MilliSecondsSinceUnixEpoch, SecondsSinceUnixEpoch};
 
 /// Platform agnostic helper function to create MilliSecondsSinceUnixEpoch
@@ -12,8 +12,15 @@ pub fn milli_seconds_since_unix_epoch() -> MilliSecondsSinceUnixEpoch {
 
 /// Platform agnostic helper function to create SecondsSinceUnixEpoch
 pub fn seconds_since_unix_epoch() -> SecondsSinceUnixEpoch {
-    let duration =
-        SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("now is always higher");
+    modified_seconds_since_unix_epoch(|e| e)
+}
+
+/// Platform agnostic helper function to create SecondsSinceUnixEpoch with modifications
+pub fn modified_seconds_since_unix_epoch<F: Fn(Duration) -> Duration>(f: F) -> SecondsSinceUnixEpoch {
+    let duration = f(
+        SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).expect("now is always higher")
+    );
+
     let millis = duration.as_secs().try_into().expect("can't convert seconds since UNIXEPOCH");
     SecondsSinceUnixEpoch(millis)
 }
