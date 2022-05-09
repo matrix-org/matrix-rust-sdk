29bc4a8ff61d686af0c80af4dad57f8ea531626b
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-09 09:13:06 +0100

    fix(sql): Fiv mysql statestore_kv migration to use varbinary

--- migrations/mysql/20220508193917_statestore_kv.up.sql
+++ migrations/mysql/20220508193917_statestore_kv.up.sql
@@ -1,5 +1,5 @@
 -- save_filter, sync_token, set_custom_value all are kv-like
 CREATE TABLE statestore_kv (
-  kv_key BLOB PRIMARY KEY NOT NULL,
+  kv_key VARBINARY(256) PRIMARY KEY NOT NULL,
   kv_value MEDIUMBLOB NOT NULL
 );
