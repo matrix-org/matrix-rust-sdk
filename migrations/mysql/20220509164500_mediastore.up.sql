f4fde5389d8d868d83cbe65cc88fdc7b0d4cf7d2
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-09 21:10:23 +0100

    fix(sql): Mysql does not support datetime with time zone

--- migrations/mysql/20220509164500_mediastore.up.sql
+++ migrations/mysql/20220509164500_mediastore.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE statestore_media (
   media_url VARCHAR(256) PRIMARY KEY NOT NULL,
   media_data LONGBLOB NOT NULL,
-  last_access TIMESTAMP WITH TIME ZONE NOT NULL -- Because this table is an LRU cache
+  last_access TIMESTAMP NOT NULL -- Because this table is an LRU cache
 );
 CREATE INDEX statestore_media_last_access ON statestore_media (last_access);
