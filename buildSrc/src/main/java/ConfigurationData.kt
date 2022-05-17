160b23b5053c516c83a83bd54465ab644035e0d6
ganfra <francoisg@element.io>
2022-05-17 18:24:12 +0200

    Bump version

--- buildSrc/src/main/java/ConfigurationData.kt
+++ buildSrc/src/main/java/ConfigurationData.kt
@@ -3,7 +3,7 @@ object ConfigurationData {
     const val targetSdk = 31
     const val minSdk = 21
     const val majorVersion = 0
-    const val minorVersion = 1
+    const val minorVersion = 2
     const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
