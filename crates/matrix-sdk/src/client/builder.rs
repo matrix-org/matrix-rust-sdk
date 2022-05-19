9e6e76e5ed506e2880c7ed57950104e3fa7b7a47
Denis Kasak <dkasak@termina.org.uk>
2022-05-19 13:38:13 +0000

    docs: Fix reference to ClientConfig in doc comment

--- crates/matrix-sdk/src/client/builder.rs
+++ crates/matrix-sdk/src/client/builder.rs
@@ -113,7 +113,7 @@ impl ClientBuilder {
         self
     }
 
-    /// Create a new `ClientConfig` with the given [`StoreConfig`].
+    /// Create a new `ClientBuilder` with the given [`StoreConfig`].
     ///
     /// The easiest way to get a [`StoreConfig`] is to use the
     /// [`make_store_config`] method from the [`store`] module or directly from
