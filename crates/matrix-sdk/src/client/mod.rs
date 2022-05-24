fc2bd9105a8d815829894babefebc241c1fddfbb
Jonas Platte <jplatte+git@posteo.de>
2022-05-24 07:42:40 +0200

    Update crates/matrix-sdk/src/client/mod.rs

--- crates/matrix-sdk/src/client/mod.rs
+++ crates/matrix-sdk/src/client/mod.rs
@@ -609,7 +609,7 @@ impl Client {
         self.store().get_room(room_id).and_then(|room| room::Left::new(self.clone(), room))
     }
 
-    /// Resolve a room alias to a room id and a list of servers which knows
+    /// Resolve a room alias to a room id and a list of servers which know
     /// about it.
     ///
     /// # Arguments
