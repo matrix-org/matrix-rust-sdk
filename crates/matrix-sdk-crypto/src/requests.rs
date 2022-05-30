0335fdd07f6c5689738e6e63b4a67fce3eda0a1b
Ivan Enderlin <ivan@mnt.io>
2022-05-30 10:00:54 +0200

    doc(crypto): Add missing module documentation.

--- crates/matrix-sdk-crypto/src/requests.rs
+++ crates/matrix-sdk-crypto/src/requests.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Modules containing customized request types.
+
 use std::{collections::BTreeMap, iter, sync::Arc, time::Duration};
 
 use ruma::{
