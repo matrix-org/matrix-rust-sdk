19bfa0c82f05279e117829693266bb55d05e70c0
Benjamin Kampmann <ben@gnunicorn.org>
2022-06-14 12:47:28 +0200

    feat(crypto-nodejs): Download lib binary in postinstall

---
+++ crates/matrix-sdk-crypto-nodejs/download-lib.js
@@ -0,0 +1,134 @@
+const { DownloaderHelper } = require('node-downloader-helper');
+const { platform, arch } = process
+
+let nativeBinding = null
+let localFileExisted = false
+let loadError = null
+const DOWNLOADS_BASE_URL = "https://github.com/matrix-org/matrix-rust-sdk/releases/download";
+const CURRENT_VERSION = "matrix-sdk-crypto-ffi-0.1.0";
+
+const byteHelper = function (value) {
+    if (value === 0) {
+        return '0 b';
+    }
+    const units = ['b', 'kB', 'MB', 'GB', 'TB'];
+    const number = Math.floor(Math.log(value) / Math.log(1024));
+    return (value / Math.pow(1024, Math.floor(number))).toFixed(1) + ' ' +
+        units[number];
+};
+
+function download_lib(libnamee) {
+    let startTime = new Date();
+    libname = "MatrixSDKCryptoFFI.zip";
+
+    const url = `${DOWNLOADS_BASE_URL}/${CURRENT_VERSION}/${libname}`;
+    console.log(`Downloading lib ${libname} from ${url}`);
+    const dl = new DownloaderHelper(url, __dirname, {
+        override: true,
+    });
+
+    dl.on('end', () => console.log('Download Completed'));
+    dl.on('error', (err) => console.log('Download Failed', err));
+    dl.on('progress', stats => {
+        const progress = stats.progress.toFixed(1);
+        const speed = byteHelper(stats.speed);
+        const downloaded = byteHelper(stats.downloaded);
+        const total = byteHelper(stats.total);
+
+        // print every one second (`progress.throttled` can be used instead)
+        const currentTime = new Date();
+        const elaspsedTime = currentTime - startTime;
+        if (elaspsedTime > 1000) {
+            startTime = currentTime;
+            console.log(`${speed}/s - ${progress}% [${downloaded}/${total}]`);
+        }
+    });
+    dl.start().catch(err => console.error(err));
+}
+
+function isMusl() {
+  // For Node 10
+  if (!process.report || typeof process.report.getReport !== 'function') {
+    try {
+      return readFileSync('/usr/bin/ldd', 'utf8').includes('musl')
+    } catch (e) {
+      return true
+    }
+  } else {
+    const { glibcVersionRuntime } = process.report.getReport().header
+    return !glibcVersionRuntime
+  }
+}
+
+switch (platform) {
+  case 'android':
+    switch (arch) {
+      case 'arm64':
+        download_lib('matrix-sdk-crypto.android-arm64.node')
+        break
+      case 'arm':
+        download_lib('matrix-sdk-crypto.android-arm-eabi.node')
+        break
+      default:
+        throw new Error(`Unsupported architecture on Android ${arch}`)
+    }
+    break
+  case 'win32':
+    switch (arch) {
+      case 'x64':
+        download_lib('matrix-sdk-crypto.win32-x64-msvc.node')
+        break
+      case 'ia32':
+        download_lib('matrix-sdk-crypto.win32-ia32-msvc.node')
+        break
+      case 'arm64':
+        download_lib('matrix-sdk-crypto.win32-arm64-msvc.node')
+        break
+      default:
+        throw new Error(`Unsupported architecture on Windows: ${arch}`)
+    }
+    break
+  case 'darwin':
+    switch (arch) {
+      case 'x64':
+        download_lib('matrix-sdk-crypto.darwin-x64.node')
+        break
+      case 'arm64':
+        download_lib('matrix-sdk-crypto.darwin-arm64.node')
+        break
+      default:
+        throw new Error(`Unsupported architecture on macOS: ${arch}`)
+    }
+    break
+  case 'freebsd':
+    if (arch !== 'x64') {
+      throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
+    }
+    download_lib('matrix-sdk-crypto.freebsd-x64.node')
+    break
+  case 'linux':
+    switch (arch) {
+      case 'x64':
+        if (isMusl()) {
+          download_lib('matrix-sdk-crypto.linux-x64-musl.node')
+        } else {
+          download_lib('matrix-sdk-crypto.linux-x64-gnu.node')
+        }
+        break
+      case 'arm64':
+        if (isMusl()) {
+          download_lib('matrix-sdk-crypto.linux-arm64-musl.node')
+        } else {
+          download_lib('matrix-sdk-crypto.linux-arm64-gnu.node')
+        }
+        break
+      case 'arm':
+        download_lib('matrix-sdk-crypto.linux-arm-gnueabihf.node')
+        break
+      default:
+        throw new Error(`Unsupported architecture on Linux: ${arch}`)
+    }
+    break
+  default:
+    throw new Error(`Unsupported OS: ${platform}, architecture: ${arch}`)
+}
