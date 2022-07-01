d3ae99eb22814f6d86fe02401484646848a862ca
Jonas Platte <jplatte@matrix.org>
2022-07-01 12:39:46 +0200

    chore: Silence new clippy lint

--- bindings/matrix-sdk-crypto-js/src/lib.rs
+++ bindings/matrix-sdk-crypto-js/src/lib.rs
@@ -15,6 +15,7 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![warn(missing_docs, missing_debug_implementations)]
+#![allow(clippy::drop_non_drop)] // triggered by wasm_bindgen code
 
 pub mod encryption;
 pub mod events;
