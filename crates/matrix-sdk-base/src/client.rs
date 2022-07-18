296e18e446131d0bbd2c724916655ceb8dc748c4
Marcel R <m.radzio@famedly.com>
2022-07-18 13:54:02 +0200

    refactor: Split out joined_rooms handling into a seperate function

--- crates/matrix-sdk-base/src/client.rs
+++ crates/matrix-sdk-base/src/client.rs
@@ -57,7 +57,7 @@ use ruma::{
     },
     push::{Action, PushConditionRoomCtx, Ruleset},
     serde::Raw,
-    MilliSecondsSinceUnixEpoch, OwnedUserId, RoomId, UInt, UserId,
+    MilliSecondsSinceUnixEpoch, OwnedRoomId, OwnedUserId, RoomId, UInt, UserId,
 };
 use tracing::{debug, info, trace, warn};
 
@@ -558,6 +558,103 @@ impl BaseClient {
         changes.account_data = account_data;
     }
 
+    async fn process_joined_rooms(
+        &self,
+        joined_rooms: BTreeMap<OwnedRoomId, api::sync::sync_events::v3::JoinedRoom>,
+        changes: &mut StateChanges,
+        ambiguity_cache: &mut AmbiguityCache,
+        push_rules: &Ruleset,
+        new_rooms: &mut Rooms,
+    ) -> Result<()> {
+        for (room_id, new_info) in joined_rooms {
+            let room = self.store.get_or_create_room(&room_id, RoomType::Joined).await;
+            let mut room_info = room.clone_info();
+            room_info.mark_as_joined();
+
+            room_info.update_summary(&new_info.summary);
+            room_info.set_prev_batch(new_info.timeline.prev_batch.as_deref());
+
+            let mut user_ids = self
+                .handle_state(&new_info.state.events, &mut room_info, changes, ambiguity_cache)
+                .await?;
+
+            if let Some(event) =
+                new_info.ephemeral.events.iter().find_map(|e| match e.deserialize() {
+                    Ok(AnySyncEphemeralRoomEvent::Receipt(event)) => Some(event.content),
+                    _ => None,
+                })
+            {
+                changes.add_receipts(&room_id, event);
+            }
+
+            if new_info.timeline.limited {
+                room_info.mark_members_missing();
+            }
+
+            let timeline = self
+                .handle_timeline(
+                    &room,
+                    new_info.timeline,
+                    push_rules,
+                    &mut user_ids,
+                    &mut room_info,
+                    changes,
+                    ambiguity_cache,
+                )
+                .await?;
+
+            self.handle_room_account_data(&room_id, &new_info.account_data.events, changes).await;
+
+            #[cfg(feature = "e2e-encryption")]
+            if room_info.is_encrypted() {
+                if let Some(o) = self.olm_machine() {
+                    if !room.is_encrypted() {
+                        // The room turned on encryption in this sync, we need
+                        // to also get all the existing users and mark them for
+                        // tracking.
+                        let joined = self.store.get_joined_user_ids(&room_id).await?;
+                        let invited = self.store.get_invited_user_ids(&room_id).await?;
+
+                        let user_ids: Vec<&UserId> =
+                            joined.iter().chain(&invited).map(Deref::deref).collect();
+                        o.update_tracked_users(user_ids).await
+                    }
+
+                    o.update_tracked_users(user_ids.iter().map(Deref::deref)).await;
+                }
+            }
+
+            let notification_count = new_info.unread_notifications.into();
+            room_info.update_notification_count(notification_count);
+
+            #[cfg(feature = "experimental-timeline")]
+            let timeline_slice = TimelineSlice::new(
+                timeline.events.clone(),
+                next_batch.clone(),
+                timeline.prev_batch.clone(),
+                timeline.limited,
+                true,
+            );
+
+            #[cfg(feature = "experimental-timeline")]
+            changes.add_timeline(&room_id, timeline_slice);
+
+            new_rooms.join.insert(
+                room_id,
+                JoinedRoom::new(
+                    timeline,
+                    new_info.state,
+                    new_info.account_data,
+                    new_info.ephemeral,
+                    notification_count,
+                ),
+            );
+
+            changes.add_room(room_info);
+        }
+        Ok(())
+    }
+
     /// Receive a response from a sync call.
     ///
     /// This will not check if we previously had this response.
@@ -622,98 +719,14 @@ impl BaseClient {
 
         let mut new_rooms = Rooms::default();
 
-        for (room_id, new_info) in rooms.join {
-            let room = self.store.get_or_create_room(&room_id, RoomType::Joined).await;
-            let mut room_info = room.clone_info();
-            room_info.mark_as_joined();
-
-            room_info.update_summary(&new_info.summary);
-            room_info.set_prev_batch(new_info.timeline.prev_batch.as_deref());
-
-            let mut user_ids = self
-                .handle_state(
-                    &new_info.state.events,
-                    &mut room_info,
-                    &mut changes,
-                    &mut ambiguity_cache,
-                )
-                .await?;
-
-            if let Some(event) =
-                new_info.ephemeral.events.iter().find_map(|e| match e.deserialize() {
-                    Ok(AnySyncEphemeralRoomEvent::Receipt(event)) => Some(event.content),
-                    _ => None,
-                })
-            {
-                changes.add_receipts(&room_id, event);
-            }
-
-            if new_info.timeline.limited {
-                room_info.mark_members_missing();
-            }
-
-            let timeline = self
-                .handle_timeline(
-                    &room,
-                    new_info.timeline,
-                    &push_rules,
-                    &mut user_ids,
-                    &mut room_info,
-                    &mut changes,
-                    &mut ambiguity_cache,
-                )
-                .await?;
-
-            self.handle_room_account_data(&room_id, &new_info.account_data.events, &mut changes)
-                .await;
-
-            #[cfg(feature = "e2e-encryption")]
-            if room_info.is_encrypted() {
-                if let Some(o) = self.olm_machine() {
-                    if !room.is_encrypted() {
-                        // The room turned on encryption in this sync, we need
-                        // to also get all the existing users and mark them for
-                        // tracking.
-                        let joined = self.store.get_joined_user_ids(&room_id).await?;
-                        let invited = self.store.get_invited_user_ids(&room_id).await?;
-
-                        let user_ids: Vec<&UserId> =
-                            joined.iter().chain(&invited).map(Deref::deref).collect();
-                        o.update_tracked_users(user_ids).await
-                    }
-
-                    o.update_tracked_users(user_ids.iter().map(Deref::deref)).await;
-                }
-            }
-
-            let notification_count = new_info.unread_notifications.into();
-            room_info.update_notification_count(notification_count);
-
-            #[cfg(feature = "experimental-timeline")]
-            let timeline_slice = TimelineSlice::new(
-                timeline.events.clone(),
-                next_batch.clone(),
-                timeline.prev_batch.clone(),
-                timeline.limited,
-                true,
-            );
-
-            #[cfg(feature = "experimental-timeline")]
-            changes.add_timeline(&room_id, timeline_slice);
-
-            new_rooms.join.insert(
-                room_id,
-                JoinedRoom::new(
-                    timeline,
-                    new_info.state,
-                    new_info.account_data,
-                    new_info.ephemeral,
-                    notification_count,
-                ),
-            );
-
-            changes.add_room(room_info);
-        }
+        self.process_joined_rooms(
+            rooms.join,
+            &mut changes,
+            &mut ambiguity_cache,
+            &push_rules,
+            &mut new_rooms,
+        )
+        .await?;
 
         for (room_id, new_info) in rooms.leave {
             let room = self.store.get_or_create_room(&room_id, RoomType::Left).await;
