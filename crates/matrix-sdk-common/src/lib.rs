6d7f9078417f7c289b7c3798b54c304997e7f752
Jonas Platte <jplatte@matrix.org>
2022-08-10 11:19:20 +0200

    fix(sdk): Further relax event / notification handler bounds on WASM
    This allows capturing non-`Send` / -`Sync` values in handler closures.

--- crates/matrix-sdk-common/src/lib.rs
+++ crates/matrix-sdk-common/src/lib.rs
@@ -1,8 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![warn(missing_debug_implementations)]
 
-use std::future::Future;
-
 pub use instant;
 
 pub mod deserialized_responses;
@@ -37,13 +35,6 @@ impl<T: Sync> SyncOutsideWasm for T {}
 pub trait SyncOutsideWasm {}
 #[cfg(target_arch = "wasm32")]
 impl<T> SyncOutsideWasm for T {}
-
-/// Alias for `Future + SendOutsideWasm`.
-///
-/// Useful as a separate trait because the former trait bound can't be used
-/// with `dyn`.
-pub trait FutureSendOutsideWasm: Future + SendOutsideWasm {}
-impl<T: Future + SendOutsideWasm> FutureSendOutsideWasm for T {}
 
 /// Super trait that is used for our store traits, this trait will differ if
 /// it's used on WASM. WASM targets will not require `Send` and `Sync` to have
