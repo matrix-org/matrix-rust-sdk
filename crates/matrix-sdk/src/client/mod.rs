cdaa4a5984db589c65bae04cc97cf99929eb4803
Timothy Hobbs <timothyhobbs@seznam.cz>
2022-08-17 10:45:42 +0000

    feat(sdk): Make created_dm_room public

--- crates/matrix-sdk/src/client/mod.rs
+++ crates/matrix-sdk/src/client/mod.rs
@@ -15,6 +15,7 @@
 // limitations under the License.
 
 use std::{
+    collections::BTreeMap,
     fmt::{self, Debug},
     future::Future,
     pin::Pin,
@@ -68,6 +69,7 @@ use ruma::{
             member::{MembershipState, RoomMemberEventContent},
         },
         SyncStateEvent,
+        GlobalAccountDataEventType,
     },
     presence::PresenceState,
     DeviceId, MilliSecondsSinceUnixEpoch, OwnedDeviceId, OwnedRoomId, OwnedServerName, RoomAliasId,
@@ -1823,6 +1825,12 @@ impl Client {
         let response = self.send(request, None).await?;
         let room_id = response.room_id;
 
+        // If the room is already in our store return it.
+        if let Some(room_from_store) = self.get_joined_room(&room_id) {
+            return Ok(room_from_store)
+        }
+        // otherwise wait for a SyncStateEvent which will give us the room.
+
         loop {
             let room = StreamExt::next(&mut rx)
                 .await
@@ -1845,6 +1853,48 @@ impl Client {
             };
         }
     }
+
+
+    /// Create a direct message room with the specified user.
+    pub async fn create_dm_room(
+        &self,
+        user_id: &UserId,
+    ) -> Result<room::Joined> {
+        use ruma::{api::client::room::create_room::v3::RoomPreset, events::direct::DirectEvent};
+
+        // First we create the DM room, where we invite the user and tell the
+        // invitee that the room should be a DM.
+        let invite = &[user_id.to_owned()];
+
+        let request = assign!(ruma::api::client::room::create_room::v3::Request::new(), {
+            invite,
+            is_direct: true,
+            preset: Some(RoomPreset::TrustedPrivateChat),
+        });
+
+        let room = self.create_room(request).await?;
+
+        // Now we need to mark the room as a DM for ourselves, we fetch the
+        // existing `m.direct` event and append the room to the list of DMs we
+        // have with this user.
+        let mut content = self
+            .store()
+            .get_account_data_event(GlobalAccountDataEventType::Direct)
+            .await?
+            .map(|e| e.deserialize_as::<DirectEvent>())
+            .transpose()?
+            .map(|e| e.content)
+            .unwrap_or_else(|| ruma::events::direct::DirectEventContent(BTreeMap::new()));
+
+        content.entry(user_id.to_owned()).or_default().push(room.room_id().to_owned());
+
+        // TODO We should probably save the fact that we need to send this out
+        // because otherwise we might end up in a state where we have a DM that
+        // isn't marked as one.
+        self.account().set_account_data(content).await?;
+        return Ok(room);
+    }
+
 
     /// Search the homeserver's directory for public rooms with a filter.
     ///
