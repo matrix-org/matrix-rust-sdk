4557494da627075d683b5eb38d6cf7db402fd0b2
Richard van der Hoff <richard@matrix.org>
2022-11-01 10:18:44 +0000

    Optimise unbase64
    Use a lookup table instead of a function with if statements

--- bindings/matrix-sdk-crypto-js/unbase64.js
+++ bindings/matrix-sdk-crypto-js/unbase64.js
@@ -1,27 +1,13 @@
 // Javascript module which exports a function which will un-base64 a string
 //
-// From the code at https://developer.mozilla.org/en-US/docs/Glossary/Base64#solution_2_%E2%80%93_rewriting_atob_and_btoa_using_typedarrays_and_utf-8
+// Based on the code at https://developer.mozilla.org/en-US/docs/Glossary/Base64#solution_2_%E2%80%93_rewriting_atob_and_btoa_using_typedarrays_and_utf-8
 
-function b64ToUint6(nChr) {
-  return nChr > 64 && nChr < 91
-    ? nChr - 65
-    : nChr > 96 && nChr < 123
-    ? nChr - 71
-    : nChr > 47 && nChr < 58
-    ? nChr + 4
-    : nChr === 43
-    ? 62
-    : nChr === 47
-    ? 63
-    : 0;
-}
+const lookup = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 62, 0, 62, 0, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 0, 0, 0, 0, 63, 0, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]);
 
-function base64DecToArr(sBase64, nBlocksSize) {
+function base64DecToArr(sBase64) {
   const sB64Enc = sBase64.replace(/[^A-Za-z0-9+/]/g, "");
   const nInLen = sB64Enc.length;
-  const nOutLen = nBlocksSize
-    ? Math.ceil(((nInLen * 3 + 1) >> 2) / nBlocksSize) * nBlocksSize
-    : (nInLen * 3 + 1) >> 2;
+  const nOutLen = (nInLen / 3 + 1) >> 2;
   const taBytes = new Uint8Array(nOutLen);
 
   let nMod3;
@@ -30,7 +16,7 @@ function base64DecToArr(sBase64, nBlocksSize) {
   let nOutIdx = 0;
   for (let nInIdx = 0; nInIdx < nInLen; nInIdx++) {
     nMod4 = nInIdx & 3;
-    nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << (6 * (3 - nMod4));
+    nUint24 |= lookup[sB64Enc.charCodeAt(nInIdx)] << (6 * (3 - nMod4));
     if (nMod4 === 3 || nInLen - nInIdx === 1) {
       nMod3 = 0;
       while (nMod3 < 3 && nOutIdx < nOutLen) {
