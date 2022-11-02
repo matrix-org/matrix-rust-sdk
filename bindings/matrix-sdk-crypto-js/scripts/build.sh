6497d6d67660fc383410f81262d7e3432e44bd2e
Ivan Enderlin <ivan@mnt.io>
2022-11-02 16:19:17 +0100

    feat(crypto-js): Make `scripts/build.sh` compatible with macOS.
    This patch also improves the “phrasing”, simplifies the code a little bit etc.
    Small stuff.

--- bindings/matrix-sdk-crypto-js/scripts/build.sh
+++ bindings/matrix-sdk-crypto-js/scripts/build.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 #
-# Build the javascript modules
+# Build the JavaScript modules
 #
 # This script is really a workaround for https://github.com/rustwasm/wasm-pack/issues/1074.
 #
-# Currently, the only reliable way to load webassembly in all the JS
+# Currently, the only reliable way to load WebAssembly in all the JS
 # environments we want to target (web-via-webpack, web-via-browserify, jest)
 # seems to be to pack the WASM into base64, and then unpack it and instantiate
 # it at runtime.
 #
 # Hopefully one day, https://github.com/rustwasm/wasm-pack/issues/1074 will be
 # fixed and this will be unnecessary.
+#
+# Run this script from the root of the project, i.e. from the parent directory of this file.
 
 set -e
 
 RUSTFLAGS='-C opt-level=z' WASM_BINDGEN_WEAKREF=1 wasm-pack build --release --target nodejs --scope matrix-org --out-dir ./pkg
 
-# convert the wasm into a js file that exports the b64'ed wasm
-{
-    echo 'module.exports = `'
-    base64 pkg/matrix_sdk_crypto_js_bg.wasm
-    echo '`;'
-} > pkg/matrix_sdk_crypto_js_bg.wasm.js
+# Convert the Wasm into a JS file that exports the base64'ed Wasm.
+echo "module.exports = '$(base64 pkg/matrix_sdk_crypto_js_bg.wasm)';" > pkg/matrix_sdk_crypto_js_bg.wasm.js
 
-# copy in the unbase64 module
-cp unbase64.js pkg/
+# Copy in the unbase64 module
+cp scripts/wasm.js pkg/
 
-# In the javascript:
-#  1. replace the lines that load the wasm
-#  2. remove the imports of TextDecoder and TextEncoder. We rely on the global defaults.
+# In the JavaScript:
+#  1. Replace the lines that load the Wasm,
+#  2. Remove the imports of `TextDecoder` and `TextEncoder`. We rely on the global defaults.
 loadwasm='const bytes = require("./unbase64.js")(require("./matrix_sdk_crypto_js_bg.wasm.js"));'
-sed -i -e "/^const path = /,+1 c$loadwasm" \
-    -e '/= require(`util`)/d' \
+sed -i '' \
+    -e "/^const path = /d" \
+    -e "s@^const bytes =.*@${loadwasm}@" \
+    -e '/Text..coder.*= require(.util.)/d' \
     pkg/matrix_sdk_crypto_js.js
