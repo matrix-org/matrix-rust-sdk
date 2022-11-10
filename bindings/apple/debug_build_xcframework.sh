c08631513260dddc867ee6d39887223e079757bc
Benjamin Kampmann <ben@gnunicorn.org>
2022-11-10 17:30:07 +0000

    feat(xtask): add fast sim-only builds support

--- bindings/apple/debug_build_xcframework.sh
+++ bindings/apple/debug_build_xcframework.sh
@@ -19,19 +19,6 @@ else
   echo "Running debug build"
 fi
 
-echo "Active architecture ${ACTIVE_ARCH}"
-
-# Path to the repo root
-SRC_ROOT=../..
-
-TARGET_DIR="${SRC_ROOT}/target"
-
-GENERATED_DIR="${SRC_ROOT}/bindings/apple/generated"
-mkdir -p ${GENERATED_DIR}
-
-REL_FLAG=""
-REL_TYPE_DIR="debug"
-
 # iOS Simulator arm64
 if [ "$ACTIVE_ARCH" = "arm64" ]; then
   TARGET="aarch64-apple-ios-sim"
@@ -39,52 +26,6 @@ if [ "$ACTIVE_ARCH" = "arm64" ]; then
 else
   TARGET="x86_64-apple-ios"
 fi
+echo "Active architecture ${ACTIVE_ARCH}"
 
-cargo build -p matrix-sdk-ffi ${REL_FLAG} --target "$TARGET"
-
-lipo -create \
-  "${TARGET_DIR}/$TARGET/${REL_TYPE_DIR}/libmatrix_sdk_ffi.a" \
-  -output "${GENERATED_DIR}/libmatrix_sdk_ffi_iossimulator.a"
-
-# Generate uniffi files
-uniffi-bindgen generate \
-  --language swift \
-  --lib-file "${TARGET_DIR}/$TARGET/${REL_TYPE_DIR}/libmatrix_sdk_ffi.a" \
-  --out-dir ${GENERATED_DIR} \
-  "${SRC_ROOT}/bindings/matrix-sdk-ffi/src/api.udl"
-
-# Move them to the right place
-HEADERS_DIR=${GENERATED_DIR}/headers
-mkdir -p ${HEADERS_DIR}
-
-mv ${GENERATED_DIR}/*.h ${HEADERS_DIR}
-
-# Rename and move modulemap to the right place
-mv ${GENERATED_DIR}/*.modulemap ${HEADERS_DIR}/module.modulemap
-
-SWIFT_DIR="${GENERATED_DIR}/swift"
-mkdir -p ${SWIFT_DIR}
-
-mv ${GENERATED_DIR}/*.swift ${SWIFT_DIR}
-
-# Build the xcframework
-
-if [ -d "${GENERATED_DIR}/MatrixSDKFFI.xcframework" ]; then rm -rf "${GENERATED_DIR}/MatrixSDKFFI.xcframework"; fi
-
-xcodebuild -create-xcframework \
-  -library "${GENERATED_DIR}/libmatrix_sdk_ffi_iossimulator.a" \
-  -headers ${HEADERS_DIR} \
-  -output "${GENERATED_DIR}/MatrixSDKFFI.xcframework"
-
-# Cleanup
-
-if [ -f "${GENERATED_DIR}/libmatrix_sdk_ffi_iossimulator.a" ]; then rm -rf "${GENERATED_DIR}/libmatrix_sdk_ffi_iossimulator.a"; fi
-if [ -d ${HEADERS_DIR} ]; then rm -rf ${HEADERS_DIR}; fi
-
-if [ "$IS_CI" = false ] ; then
-  echo "Preparing matrix-rust-components-swift"
-
-  # Debug -> Copy generated files over to ../../../matrix-rust-components-swift
-  rsync -a --delete "${GENERATED_DIR}/MatrixSDKFFI.xcframework" "${SRC_ROOT}/../matrix-rust-components-swift/"
-  rsync -a --delete "${GENERATED_DIR}/swift/" "${SRC_ROOT}/../matrix-rust-components-swift/Sources/MatrixRustSDK"
-fi
+cargo xtask swift build-framework --sim-only-target=${TARGET} $*
--- xtask/src/swift.rs
+++ xtask/src/swift.rs
@@ -1,6 +1,9 @@
+use std::{
+    fs::{self, create_dir_all},
+    path::PathBuf,
+};
+
 use clap::{Args, Subcommand};
-use std::fs::{self, create_dir_all};
-use std::path::PathBuf;
 use xshell::{cmd, pushd};
 
 use crate::{workspace, Result};
@@ -21,6 +24,9 @@ enum SwiftCommand {
         /// Build in release mode
         #[clap(long)]
         release: bool,
+        /// Build the simulator version only
+        #[clap(long)]
+        sim_only_target: Option<String>,
         /// Where to rsync the resulting framework to
         #[clap(long)]
         rsync_path: Option<PathBuf>,
@@ -33,8 +39,8 @@ impl SwiftArgs {
 
         match self.cmd {
             SwiftCommand::BuildLibrary => build_library(),
-            SwiftCommand::BuildFramework { release, rsync_path } => {
-                build_xcframework(release, rsync_path)
+            SwiftCommand::BuildFramework { release, rsync_path, sim_only_target } => {
+                build_xcframework(release, sim_only_target, rsync_path)
             }
         }
     }
@@ -101,82 +107,93 @@ fn generate_uniffi(library_file: &PathBuf, ffi_directory: &PathBuf) -> Result<()
 }
 
 fn build_for_target(target: &str, release: bool) -> Result<PathBuf> {
-    let (release_flag, rel_type_dir) =
-        if release { ("--release", "release") } else { ("", "debug") };
-    cmd!("cargo build -p matrix-sdk-ffi {release_flag} --target {target}").run()?;
+    let mut cmd = cmd!("cargo build -p matrix-sdk-ffi --target {target}");
+    if release {
+        cmd = cmd.arg("--release");
+    }
+    cmd.run()?;
     Ok(workspace::root_path()?
         .join("target")
         .join(target)
-        .join(rel_type_dir)
+        .join(if release { "release" } else { "debug" })
         .join("libmatrix_sdk_ffi.a"))
 }
 
-fn build_xcframework(release_mode: bool, rsync_path: Option<PathBuf>) -> Result<()> {
-    println!("-- Building for iOS [1/5]");
-    let ios_path = build_for_target("aarch64-apple-ios", release_mode)?;
-
-    println!("-- Building for macOS (Apple Silicon) [2/5]");
-    let darwin_arm_path = build_for_target("aarch64-apple-darwin", release_mode)?;
-    println!("-- Building for macOS (Intel) [3/5]");
-    let darwin_x86_path = build_for_target("x86_64-apple-darwin", release_mode)?;
-
-    println!("-- Building for iOS Simulator (Apple Silicon) [4/5]");
-    let ios_sim_arm_path = build_for_target("aarch64-apple-ios-sim", release_mode)?;
-    println!("-- Building for iOS Simulator (Intel) [5/5]");
-    let ios_sim_x86_path = build_for_target("x86_64-apple-ios", release_mode)?;
-
-    println!("-- Creating XCFramework");
-
+fn build_xcframework(
+    release_mode: bool,
+    sim_only_target: Option<String>,
+    rsync_path: Option<PathBuf>,
+) -> Result<()> {
     let root_dir = workspace::root_path()?;
     let generated_dir = root_dir.join("generated");
     let headers_dir = generated_dir.join("headers");
     create_dir_all(headers_dir.clone())?;
 
-    // # MacOS
-    let lipo_target_macos = generated_dir.join("libmatrix_sdk_ffi_macos.a");
-    println!("-- Running Lipo for macOS [1/4]");
-    cmd!(
-        "lipo -create {darwin_x86_path} {darwin_arm_path}
-        -output {lipo_target_macos}"
-    )
-    .run()?;
+    let (libs, uniff_lib_path) = if let Some(target) = sim_only_target {
+        println!("-- Building for iOS simulator 1/1");
+        let ios_sim_path = build_for_target(target.as_str(), release_mode)?;
 
-    // # iOS Simulator
-    let lipo_target_sim = generated_dir.join("libmatrix_sdk_ffi_iossimulator.a");
-    println!("-- Running Lipo for iOS [2/4]");
-    cmd!(
-        "lipo -create {ios_sim_arm_path} {ios_sim_x86_path}
-        -output {lipo_target_sim}"
-    )
-    .run()?;
+        println!("-- Running Lipo for iOS sim ({target}) [1/1]");
+        // # iOS Simulator
+        let lipo_target_sim = generated_dir.join("libmatrix_sdk_ffi_iossimulator.a");
+        cmd!(
+            "lipo -create {ios_sim_path}
+            -output {lipo_target_sim}"
+        )
+        .run()?;
 
-    println!("-- Generate uniffi files [3/4]");
-    generate_uniffi(&darwin_x86_path, &generated_dir)?;
+        (vec![lipo_target_sim], ios_sim_path)
+    } else {
+        println!("-- Building for iOS [1/5]");
+        let ios_path = build_for_target("aarch64-apple-ios", release_mode)?;
+
+        println!("-- Building for macOS (Apple Silicon) [2/5]");
+        let darwin_arm_path = build_for_target("aarch64-apple-darwin", release_mode)?;
+        println!("-- Building for macOS (Intel) [3/5]");
+        let darwin_x86_path = build_for_target("x86_64-apple-darwin", release_mode)?;
+
+        println!("-- Building for iOS Simulator (Apple Silicon) [4/5]");
+        let ios_sim_arm_path = build_for_target("aarch64-apple-ios-sim", release_mode)?;
+        println!("-- Building for iOS Simulator (Intel) [5/5]");
+        let ios_sim_x86_path = build_for_target("x86_64-apple-ios", release_mode)?;
+
+        println!("-- Running Lipo for MacOs [1/2]");
+        // # MacOS
+        let lipo_target_macos = generated_dir.join("libmatrix_sdk_ffi_macos.a");
+        cmd!(
+            "lipo -create {darwin_x86_path} {darwin_arm_path}
+            -output {lipo_target_macos}"
+        )
+        .run()?;
+
+        println!("-- Running Lipo for iOS Simulator [2/2]");
+        // # iOS Simulator
+        let lipo_target_sim = generated_dir.join("libmatrix_sdk_ffi_iossimulator.a");
+        cmd!(
+            "lipo -create {ios_sim_arm_path} {ios_sim_x86_path}
+            -output {lipo_target_sim}"
+        )
+        .run()?;
+        (vec![lipo_target_macos, lipo_target_sim, ios_path], darwin_x86_path)
+    };
+
+    println!("-- Generate uniffi files");
+    generate_uniffi(&uniff_lib_path, &generated_dir)?;
 
     fs::rename(generated_dir.join("matrix_sdk_ffiFFI.h"), headers_dir.join("matrix_sdk_ffiFFI.h"))?;
 
-    println!("-- Generate MatrixSDKFFI.xcframework framework [4/4]");
+    println!("-- Generate MatrixSDKFFI.xcframework framework");
     let xcframework_path = generated_dir.join("MatrixSDKFFI.xcframework");
     if xcframework_path.exists() {
         fs::remove_dir_all(xcframework_path.as_path())?;
     }
-
-    cmd!(
-        "xcodebuild -create-xcframework
-        -library {lipo_target_macos}
-        -headers {headers_dir}
-        -library {lipo_target_sim}
-        -headers {headers_dir}
-        -library {ios_path}
-        -headers {headers_dir}
-        -output {generated_dir}/MatrixSDKFFI.xcframework
-    "
-    )
-    .run()?;
+    let mut cmd = cmd!("xcodebuild -create-xcframework");
+    for p in libs {
+        cmd = cmd.arg("-library").arg(p).arg("-headers").arg(headers_dir.as_path())
+    }
+    cmd.arg("-output").arg(generated_dir.join("MatrixSDKFFI.xcframework")).run()?;
 
     // cleaning up the intermediate data
-    fs::remove_file(lipo_target_macos.as_path())?;
-    fs::remove_file(lipo_target_sim.as_path())?;
     fs::remove_dir_all(headers_dir.as_path())?;
 
     if let Some(path) = rsync_path {
