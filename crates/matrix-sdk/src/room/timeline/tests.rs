317965995a069b7144aac56c2cbcf32553101c9e
Jonas Platte <jplatte@matrix.org>
2022-11-18 12:03:13 +0100

    test(sdk): Add a test for retrying decryption of timeline items

--- crates/matrix-sdk/src/room/timeline/tests.rs
+++ crates/matrix-sdk/src/room/timeline/tests.rs
@@ -23,6 +23,7 @@ use assert_matches::assert_matches;
 use futures_core::Stream;
 use futures_signals::signal_vec::{SignalVecExt, VecDiff};
 use futures_util::StreamExt;
+use matrix_sdk_base::crypto::OlmMachine;
 use matrix_sdk_test::async_test;
 use once_cell::sync::Lazy;
 use ruma::{
@@ -38,6 +39,7 @@ use ruma::{
         },
         MessageLikeEventContent, OriginalSyncMessageLikeEvent,
     },
+    room_id,
     serde::Raw,
     server_name, user_id, EventId, MilliSecondsSinceUnixEpoch, OwnedUserId, UserId,
 };
@@ -145,19 +147,48 @@ async fn edit_redacted() {
     assert_eq!(timeline.inner.items.lock_ref().len(), 1);
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 #[async_test]
 async fn unable_to_decrypt() {
+    use std::io::Cursor;
+
+    use matrix_sdk_base::crypto::decrypt_room_key_export;
+
+    const SESSION_ID: &str = "gM8i47Xhu0q52xLfgUXzanCMpLinoyVyH7R58cBuVBU";
+    const SESSION_KEY: &[u8] = b"\
+        -----BEGIN MEGOLM SESSION DATA-----\n\
+        ASKcWoiAVUM97482UAi83Avce62hSLce7i5JhsqoF6xeAAAACqt2Cg3nyJPRWTTMXxXH7TXnkfdlmBXbQtq5\
+        bpHo3LRijcq2Gc6TXilESCmJN14pIsfKRJrWjZ0squ/XsoTFytuVLWwkNaW3QF6obeg2IoVtJXLMPdw3b2vO\
+        vgwGY3OMP0XafH13j1vcb6YLzvgLkZQLnYvd47hv3yK/9GmKS9tokuaQ7dCVYckYcIOS09EDTs70YdxUd5WG\
+        rQynATCLFP1p/NAGv70r9MK7Cy/mNpjD0r4qC7UEDIoi1kOWzHgnLo19wtvwsb8Fg8ATxcs3Wmtj8hIUYpDx\
+        ia4sM10zbytUuaPUAfCDf42IyxdmOnGe1CueXhgI71y+RW0s0argNqUt7jB70JT0o9CyX6UBGRaqLk2MPY9T\
+        hUu5J8X3UgIa6rcbWigzohzWm9rdbEHFrSWqjpfQYMaAKQQgETrjSy4XTrp2RhC2oNqG/hylI4ab+F4X6fpH\
+        DYP1NqNMP5g36xNu7LhDnrUB5qsPjYOmWORxGLfudpF3oLYCSlr3DgHqEIB6HjQblLZ3KQuPBse3zxyROTnS\
+        AhdPH4a/z1wioFtKNVph3hecsiKEdqnz4Y2coSIdhz58mJ9JWNQoFAENE5CSsoEZAGvafYZVpW4C75YY2zq1\
+        wIeiFi1dT43/jLAUGkslsi1VvnyfUu8qO404RxYO3XHoGLMFoFLOO+lZ+VGci2Vz10AhxJhEBHxRKxw4k2uB\
+        HztoSJUr/2Y\n\
+        -----END MEGOLM SESSION DATA-----";
+
     let timeline = TestTimeline::new(&ALICE);
+    let mut stream = timeline.stream();
+
     timeline
         .handle_live_message_event(
             &BOB,
             RoomEncryptedEventContent::new(
                 EncryptedEventScheme::MegolmV1AesSha2(
                     MegolmV1AesSha2ContentInit {
-                        ciphertext: "This can't be decrypted".to_owned(),
-                        sender_key: "whatever".to_owned(),
-                        device_id: "MyDevice".into(),
-                        session_id: "MySession".into(),
+                        ciphertext: "\
+                            AwgAEtABPRMavuZMDJrPo6pGQP4qVmpcuapuXtzKXJyi3YpEsjSWdzuRKIgJzD4P\
+                            cSqJM1A8kzxecTQNJsC5q22+KSFEPxPnI4ltpm7GFowSoPSW9+bFdnlfUzEP1jPq\
+                            YevHAsMJp2fRKkzQQbPordrUk1gNqEpGl4BYFeRqKl9GPdKFwy45huvQCLNNueql\
+                            CFZVoYMuhxrfyMiJJAVNTofkr2um2mKjDTlajHtr39pTG8k0eOjSXkLOSdZvNOMz\
+                            hGhSaFNeERSA2G2YbeknOvU7MvjiO0AKuxaAe1CaVhAI14FCgzrJ8g0y5nly+n7x\
+                            QzL2G2Dn8EoXM5Iqj8W99iokQoVsSrUEnaQ1WnSIfewvDDt4LCaD/w7PGETMCQ"
+                            .to_owned(),
+                        sender_key: "DeHIg4gwhClxzFYcmNntPNF9YtsdZbmMy8+3kzCMXHA".to_owned(),
+                        device_id: "NLAZCWIOCO".into(),
+                        session_id: SESSION_ID.into(),
                     }
                     .into(),
                 ),
@@ -165,16 +196,43 @@ async fn unable_to_decrypt() {
             ),
         )
         .await;
-    let timeline_items = timeline.inner.items.lock_ref();
-    assert_eq!(timeline_items.len(), 1);
-    let event = timeline_items[0].as_event().unwrap();
+
+    assert_eq!(timeline.inner.items.lock_ref().len(), 1);
+
+    let item = assert_matches!(stream.next().await, Some(VecDiff::Push { value }) => value);
+    let event = item.as_event().unwrap();
     let session_id = assert_matches!(
         event.content(),
         TimelineItemContent::UnableToDecrypt(
             EncryptedMessage::MegolmV1AesSha2 { session_id, .. },
         ) => session_id
     );
-    assert_eq!(session_id, "MySession");
+    assert_eq!(session_id, SESSION_ID);
+
+    let own_user_id = user_id!("@example:morheus.localhost");
+    let exported_keys = decrypt_room_key_export(Cursor::new(SESSION_KEY), "1234").unwrap();
+
+    let olm_machine = OlmMachine::new(own_user_id, "SomeDeviceId".into()).await;
+    olm_machine.import_room_keys(exported_keys, false, |_, _| {}).await.unwrap();
+
+    timeline
+        .inner
+        .retry_event_decryption(
+            room_id!("!DovneieKSTkdHKpIXy:morpheus.localhost"),
+            &olm_machine,
+            SESSION_ID,
+            own_user_id,
+        )
+        .await;
+
+    assert_eq!(timeline.inner.items.lock_ref().len(), 1);
+
+    let item =
+        assert_matches!(stream.next().await, Some(VecDiff::UpdateAt { index: 0, value }) => value);
+    let event = item.as_event().unwrap();
+    assert_matches!(&event.encryption_info, Some(_));
+    let text = assert_matches!(event.content(), TimelineItemContent::Message(msg) => msg.body());
+    assert_eq!(text, "It's a secret to everybody");
 }
 
 struct TestTimeline {
