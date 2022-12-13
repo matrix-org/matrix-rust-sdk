fc28e3c378089997f9f3ad403da04573368f74a6
Andy Balaam <mail@artificialworlds.net>
2022-12-13 12:38:54 +0000

    Remove allow(unused)
    Co-authored-by: Jonas Platte <jplatte+git@posteo.de>

--- crates/matrix-sdk-redis/src/redis_crypto_store.rs
+++ crates/matrix-sdk-redis/src/redis_crypto_store.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![allow(unused)] // TODO: consider removing when this code is used
 
 use std::{
     any::Any,
