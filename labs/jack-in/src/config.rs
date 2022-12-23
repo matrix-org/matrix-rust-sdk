ad1011ec534f15a243f6f20b4f1bdfd5df8b5579
Marcel Kräml <m.kraeml@kraeml.it>
2022-12-23 11:17:02 +0100

    Replace structopt with clap in jack-in
    Signed-off-by: Marcel Kräml <m.kraeml@kraeml.it>

--- labs/jack-in/src/config.rs
+++ labs/jack-in/src/config.rs
@@ -1,47 +1,45 @@
 use std::path::PathBuf;
 
+use clap::{Args, Parser, ValueEnum};
 use matrix_sdk::SlidingSyncMode;
-use structopt::{clap::arg_enum, StructOpt};
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "jack-in", about = "Your experimental sliding-sync jack into the matrix")]
+#[derive(Debug, Parser)]
+#[command(name = "jack-in", about = "Your experimental sliding-sync jack into the matrix")]
 pub struct Opt {
     /// The password of your account. If not given and no database found, it
     /// will prompt you for it
-    #[structopt(short, long, env = "JACKIN_PASSWORD")]
+    #[arg(short, long, env = "JACKIN_PASSWORD")]
     pub password: Option<String>,
 
     /// Create a fresh database, drop all existing cache
-    #[structopt(long)]
+    #[arg(long)]
     pub fresh: bool,
 
     /// RUST_LOG log-levels
-    #[structopt(short, long, env = "JACKIN_LOG", default_value = "jack_in=info,warn")]
+    #[arg(short, long, env = "JACKIN_LOG", default_value = "jack_in=info,warn")]
     pub log: String,
 
     /// The userID to log in with
-    #[structopt(short, long, env = "JACKIN_USER")]
+    #[arg(short, long, env = "JACKIN_USER")]
     pub user: String,
 
     /// The password to encrypt the store  with
-    #[structopt(long, env = "JACKIN_STORE_PASSWORD")]
+    #[arg(long, env = "JACKIN_STORE_PASSWORD")]
     pub store_pass: Option<String>,
 
-    #[structopt(long)]
+    #[arg(long)]
     /// Activate tracing and write the flamegraph to the specified file
     pub flames: Option<PathBuf>,
 
-    #[structopt(flatten)]
+    #[command(flatten)]
     /// Sliding Sync configuration flags
     pub sliding_sync: SlidingSyncConfig,
 }
 
-arg_enum! {
-    #[derive(Debug)]
-    pub enum FullSyncMode {
-        Growing,
-        Paging,
-    }
+#[derive(Debug, Clone, ValueEnum)]
+pub enum FullSyncMode {
+    Growing,
+    Paging,
 }
 
 impl From<FullSyncMode> for SlidingSyncMode {
@@ -53,10 +51,10 @@ impl From<FullSyncMode> for SlidingSyncMode {
     }
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, Args)]
 pub struct SlidingSyncConfig {
     /// The address of the sliding sync server to connect (probs the proxy)
-    #[structopt(
+    #[arg(
         long = "sliding-sync-proxy",
         default_value = "http://localhost:8008",
         env = "JACKIN_SYNC_PROXY"
@@ -64,19 +62,19 @@ pub struct SlidingSyncConfig {
     pub proxy: String,
 
     /// Activate growing window rather than pagination for full-sync
-    #[structopt(long, default_value = "SlidingSyncMode::Paging")]
+    #[arg(long, default_value = "SlidingSyncMode::Paging")]
     pub full_sync_mode: FullSyncMode,
 
     /// Limit the growing/paging to this number of maximum items to caonsider
     /// "done"
-    #[structopt(long)]
+    #[arg(long)]
     pub limit: Option<u32>,
 
     /// define the batch_size per request
-    #[structopt(long)]
+    #[arg(long)]
     pub batch_size: Option<u32>,
 
     /// define the timeline items to load
-    #[structopt(long)]
+    #[arg(long)]
     pub timeline_limit: Option<u32>,
 }
