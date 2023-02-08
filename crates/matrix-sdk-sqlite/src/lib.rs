efc2397267892286d71902d133c79c8f2e99ca73
Jonas Platte <jplatte@matrix.org>
2023-02-08 13:14:33 +0100

    refactor(sqlite): Always use rusqlite::Error for sqlite extension traits

--- crates/matrix-sdk-sqlite/src/lib.rs
+++ crates/matrix-sdk-sqlite/src/lib.rs
@@ -14,13 +14,10 @@
 #![cfg_attr(not(feature = "crypto-store"), allow(dead_code, unused_imports))]
 
 use async_trait::async_trait;
-#[cfg(feature = "crypto-store")]
 use deadpool_sqlite::Object as SqliteConn;
 #[cfg(feature = "crypto-store")]
 use matrix_sdk_crypto::{store::Result, CryptoStoreError};
-#[cfg(feature = "crypto-store")]
 use matrix_sdk_store_encryption::StoreCipher;
-#[cfg(feature = "crypto-store")]
 use rusqlite::OptionalExtension;
 
 #[cfg(feature = "crypto-store")]
@@ -28,15 +25,12 @@ mod crypto_store;
 mod error;
 mod utils;
 
-#[cfg(feature = "crypto-store")]
-pub use self::crypto_store::SqliteCryptoStore;
-pub use self::error::OpenStoreError;
-#[cfg(feature = "crypto-store")]
 use self::utils::SqliteObjectExt;
+pub use self::{crypto_store::SqliteCryptoStore, error::OpenStoreError};
 
 #[cfg(feature = "crypto-store")]
 async fn get_or_create_store_cipher(passphrase: &str, conn: &SqliteConn) -> Result<StoreCipher> {
-    let encrypted_cipher = conn.get_kv("cipher").await?;
+    let encrypted_cipher = conn.get_kv("cipher").await.map_err(CryptoStoreError::backend)?;
 
     let cipher = if let Some(encrypted) = encrypted_cipher {
         StoreCipher::import(passphrase, &encrypted)
@@ -47,7 +41,9 @@ async fn get_or_create_store_cipher(passphrase: &str, conn: &SqliteConn) -> Resu
         let export = cipher.export(passphrase);
         #[cfg(test)]
         let export = cipher._insecure_export_fast_for_testing(passphrase);
-        conn.set_kv("cipher", export.map_err(CryptoStoreError::backend)?).await?;
+        conn.set_kv("cipher", export.map_err(CryptoStoreError::backend)?)
+            .await
+            .map_err(CryptoStoreError::backend)?;
         cipher
     };
 
@@ -68,29 +64,23 @@ impl SqliteConnectionExt for rusqlite::Connection {
     }
 }
 
-#[cfg(feature = "crypto-store")]
 #[async_trait]
 trait SqliteObjectStoreExt: SqliteObjectExt {
-    async fn get_kv(&self, key: &str) -> Result<Option<Vec<u8>>> {
+    async fn get_kv(&self, key: &str) -> rusqlite::Result<Option<Vec<u8>>> {
         let key = key.to_owned();
         self.query_row("SELECT value FROM kv WHERE key = ?", (key,), |row| row.get(0))
             .await
             .optional()
-            .map_err(CryptoStoreError::backend)
     }
 
-    async fn set_kv(&self, key: &str, value: Vec<u8>) -> Result<()>;
+    async fn set_kv(&self, key: &str, value: Vec<u8>) -> rusqlite::Result<()>;
 }
 
-#[cfg(feature = "crypto-store")]
 #[async_trait]
 impl SqliteObjectStoreExt for deadpool_sqlite::Object {
-    async fn set_kv(&self, key: &str, value: Vec<u8>) -> Result<()> {
+    async fn set_kv(&self, key: &str, value: Vec<u8>) -> rusqlite::Result<()> {
         let key = key.to_owned();
-        self.interact(move |conn| conn.set_kv(&key, &value))
-            .await
-            .unwrap()
-            .map_err(CryptoStoreError::backend)?;
+        self.interact(move |conn| conn.set_kv(&key, &value)).await.unwrap()?;
 
         Ok(())
     }
