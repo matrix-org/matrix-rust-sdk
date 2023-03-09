2368ac74deef8196bd1e3f29a7a77412205ae044
valere <valeref@matrix.org>
2023-03-09 16:38:38 +0100

    feat(withheld) expose withheld code in error binding

--- bindings/matrix-sdk-crypto-ffi/src/error.rs
+++ bindings/matrix-sdk-crypto-ffi/src/error.rs
@@ -1,9 +1,10 @@
 #![allow(missing_docs)]
 
+use std::fmt::Display;
+
 use matrix_sdk_crypto::{
-    store::CryptoStoreError as InnerStoreError, types::events::room_key_withheld::WithheldCode,
-    KeyExportError, MegolmError, OlmError, SecretImportError as RustSecretImportError,
-    SignatureError as InnerSignatureError,
+    store::CryptoStoreError as InnerStoreError, KeyExportError, MegolmError, OlmError,
+    SecretImportError as RustSecretImportError, SignatureError as InnerSignatureError,
 };
 use matrix_sdk_sqlite::OpenStoreError;
 use ruma::{IdParseError, OwnedUserId};
@@ -57,25 +58,62 @@ pub enum CryptoStoreError {
     Identifier(#[from] IdParseError),
 }
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug)]
 pub enum DecryptionError {
-    #[error(transparent)]
-    Serialization(#[from] serde_json::Error),
-    #[error(transparent)]
-    Identifier(#[from] IdParseError),
-    #[error("Megolm decryption error: {error_message}")]
-    Megolm { error_message: String },
-    #[error("Can't find the room key to decrypt the event")]
-    MissingRoomKey { withheld_code: Option<WithheldCode> },
-    #[error(transparent)]
-    Store(#[from] InnerStoreError),
+    Serialization { error: String },
+    Identifier { error: String },
+    Megolm { error: String },
+    MissingRoomKey { error: String, withheld_code: Option<String> },
+    Store { error: String },
 }
+
+impl Display for DecryptionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DecryptionError::Serialization { error } => write!(f, "Serialization error: {}", error),
+            DecryptionError::Identifier { error } => {
+                write!(f, "Identifier parsing error: {}", error)
+            }
+            DecryptionError::Megolm { error } => write!(f, "Decryption error: {}", error),
+            DecryptionError::MissingRoomKey { withheld_code, .. } => match withheld_code {
+                Some(code) => {
+                    write!(f, "Missing inbound session id, was withheld reason: {:?}", code)
+                }
+                None => write!(f, "Unknown inbound session id."),
+            },
+            DecryptionError::Store { error } => write!(f, "Store error: {}", error),
+        }
+    }
+}
+
+impl std::error::Error for DecryptionError {}
 
 impl From<MegolmError> for DecryptionError {
     fn from(value: MegolmError) -> Self {
         match value {
-            MegolmError::MissingRoomKey(withheld_code) => Self::MissingRoomKey { withheld_code },
-            _ => Self::Megolm { error_message: value.to_string() },
+            MegolmError::MissingRoomKey(withheld_code) => Self::MissingRoomKey {
+                error: "Withheld Inbound group session".to_owned(),
+                withheld_code: withheld_code.map(|w| w.to_string()),
+            },
+            _ => Self::Megolm { error: value.to_string() },
         }
+    }
+}
+
+impl From<serde_json::Error> for DecryptionError {
+    fn from(err: serde_json::Error) -> Self {
+        Self::Serialization { error: err.to_string() }
+    }
+}
+
+impl From<IdParseError> for DecryptionError {
+    fn from(err: IdParseError) -> Self {
+        Self::Identifier { error: err.to_string() }
+    }
+}
+
+impl From<InnerStoreError> for DecryptionError {
+    fn from(err: InnerStoreError) -> Self {
+        Self::Store { error: err.to_string() }
     }
 }
