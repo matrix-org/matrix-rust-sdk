a9580152a061f3c68f0f302d4aa4ff56e6ee8be2
Mauro Romito <mauro.romito@element.io>
2023-03-20 16:38:41 +0100

    chore: adding missing file

---
+++ bindings/matrix-sdk-ffi/src/room_member.rs
@@ -0,0 +1,92 @@
+use matrix_sdk::room::RoomMember as SdkRoomMember;
+
+#[derive(Clone)]
+pub enum MembershipState {
+    /// The user is banned.
+    Ban,
+
+    /// The user has been invited.
+    Invite,
+
+    /// The user has joined.
+    Join,
+
+    /// The user has requested to join.
+    Knock,
+
+    /// The user has left.
+    Leave,
+}
+
+impl From<matrix_sdk::ruma::events::room::member::MembershipState> for MembershipState {
+    fn from(m: matrix_sdk::ruma::events::room::member::MembershipState) -> Self {
+        match m {
+            matrix_sdk::ruma::events::room::member::MembershipState::Ban => MembershipState::Ban,
+            matrix_sdk::ruma::events::room::member::MembershipState::Invite => {
+                MembershipState::Invite
+            }
+            matrix_sdk::ruma::events::room::member::MembershipState::Join => MembershipState::Join,
+            matrix_sdk::ruma::events::room::member::MembershipState::Knock => {
+                MembershipState::Knock
+            }
+            matrix_sdk::ruma::events::room::member::MembershipState::Leave => {
+                MembershipState::Leave
+            }
+            _ => todo!(
+                "Handle Custom case: https://github.com/matrix-org/matrix-rust-sdk/issues/1254"
+            ),
+        }
+    }
+}
+
+pub struct RoomMember {
+    inner: SdkRoomMember,
+}
+
+#[uniffi::export]
+impl RoomMember {
+    fn user_id(&self) -> String {
+        self.inner.user_id().to_string()
+    }
+
+    fn display_name(&self) -> String {
+        self.inner.display_name()
+    }
+
+    fn avatar_url(&self) -> String {
+        self.inner.avatar_url().to_string()
+    }
+
+    fn membership(&self) -> MembershipState {
+        self.inner.membership().into()
+    }
+
+    fn is_name_ambiguous(&self) -> bool {
+        self.inner.name_ambiguous()
+    }
+
+    fn power_level(&self) -> i64 {
+        self.inner.power_level()
+    }
+
+    fn normalized_power_level(&self) -> i64 {
+        self.inner.normalized_power_level()
+    }
+
+    fn is_ignored(&self) -> bool {
+        self.inner.is_ignored()
+    }
+}
+
+impl RoomMember {
+    pub fn new(room_member: SdkRoomMember) -> Self {
+        RoomMember { inner: room_member }
+    }
+}
+
+impl std::ops::Deref for RoomMember {
+    type Target = SdkRoomMember;
+    fn deref(&self) -> &SdkRoomMember {
+        &self.inner
+    }
+}
