1c2964f3f313ee6970c38dbdc6812bcebedcea6e
Damir Jelić <poljar@termina.org.uk>
2023-04-05 11:52:25 +0200

    Update crates/matrix-sdk-crypto/src/types/events/room_key_withheld.rs
    Co-authored-by: Jonas Platte <jplatte@matrix.org>

--- crates/matrix-sdk-crypto/src/types/events/room_key_withheld.rs
+++ crates/matrix-sdk-crypto/src/types/events/room_key_withheld.rs
@@ -131,7 +131,7 @@ impl RoomKeyWithheldContent {
                     from_device
                 )
             }
-            _ => unreachable!("Unsupported algorithm {}", algorithm),
+            _ => unreachable!("Unsupported algorithm {algorithm}"),
         }
     }
 
