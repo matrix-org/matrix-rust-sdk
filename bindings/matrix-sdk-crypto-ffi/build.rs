5ba9ba8d304d3c42b7697a68faac68633c8825f9
Damir Jelić <poljar@termina.org.uk>
2023-05-16 17:27:09 +0200

    Expose the git description and commit hash in the crypto-ffi bindings

--- bindings/matrix-sdk-crypto-ffi/build.rs
+++ bindings/matrix-sdk-crypto-ffi/build.rs
@@ -1,3 +1,11 @@
-fn main() {
-    uniffi::generate_scaffolding("./src/olm.udl").unwrap();
+use std::error::Error;
+
+use vergen::EmitBuilder;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    uniffi::generate_scaffolding("./src/olm.udl")?;
+
+    EmitBuilder::builder().git_sha(true).git_describe(true, false, None).emit()?;
+
+    Ok(())
 }
