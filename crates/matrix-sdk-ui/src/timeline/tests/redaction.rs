0b5a341633e24e8f3474c8787f3e63c4b9947cbd
Jonas Platte <jplatte@matrix.org>
2023-06-07 17:27:14 +0200

    ui: Filter virtual timeline items for some tests

--- crates/matrix-sdk-ui/src/timeline/tests/redaction.rs
+++ crates/matrix-sdk-ui/src/timeline/tests/redaction.rs
@@ -28,33 +28,28 @@ use super::{TestTimeline, ALICE, BOB};
 #[async_test]
 async fn reaction_redaction() {
     let timeline = TestTimeline::new();
-    let mut stream = timeline.subscribe().await;
+    let mut stream = timeline.subscribe_events().await;
 
     timeline.handle_live_message_event(&ALICE, RoomMessageEventContent::text_plain("hi!")).await;
-    let _day_divider =
-        assert_matches!(stream.next().await, Some(VectorDiff::PushBack { value }) => value);
     let item = assert_matches!(stream.next().await, Some(VectorDiff::PushBack { value }) => value);
-    let event = item.as_event().unwrap();
-    assert_eq!(event.reactions().len(), 0);
+    assert_eq!(item.reactions().len(), 0);
 
-    let msg_event_id = event.event_id().unwrap();
+    let msg_event_id = item.event_id().unwrap();
 
     let rel = Annotation::new(msg_event_id.to_owned(), "+1".to_owned());
     timeline.handle_live_message_event(&BOB, ReactionEventContent::new(rel)).await;
     let item =
-        assert_matches!(stream.next().await, Some(VectorDiff::Set { index: 1, value }) => value);
-    let event = item.as_event().unwrap();
-    assert_eq!(event.reactions().len(), 1);
+        assert_matches!(stream.next().await, Some(VectorDiff::Set { index: 0, value }) => value);
+    assert_eq!(item.reactions().len(), 1);
 
     // TODO: After adding raw timeline items, check for one here
 
-    let reaction_event_id = event.event_id().unwrap();
+    let reaction_event_id = item.event_id().unwrap();
 
     timeline.handle_live_redaction(&BOB, reaction_event_id).await;
     let item =
-        assert_matches!(stream.next().await, Some(VectorDiff::Set { index: 1, value }) => value);
-    let event = item.as_event().unwrap();
-    assert_eq!(event.reactions().len(), 0);
+        assert_matches!(stream.next().await, Some(VectorDiff::Set { index: 0, value }) => value);
+    assert_eq!(item.reactions().len(), 0);
 }
 
 #[async_test]
