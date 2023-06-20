e7a7b9591620acf5c20be10eb535d1f8a62929a7
Alfonso Grillo <alfogrillo@gmail.com>
2023-06-20 17:29:52 +0200

    Add location message type

--- bindings/matrix-sdk-ffi/src/timeline.rs
+++ bindings/matrix-sdk-ffi/src/timeline.rs
@@ -12,6 +12,7 @@ use matrix_sdk::{
             FileMessageEventContent as RumaFileMessageEventContent,
             FormattedBody as RumaFormattedBody,
             ImageMessageEventContent as RumaImageMessageEventContent,
+            LocationMessageEventContent as RumaLocationMessageEventContent,
             MessageType as RumaMessageType,
             NoticeMessageEventContent as RumaNoticeMessageEventContent, RoomMessageEventContent,
             TextMessageEventContent as RumaTextMessageEventContent, VideoInfo as RumaVideoInfo,
@@ -515,6 +516,7 @@ pub enum MessageType {
     File { content: FileMessageContent },
     Notice { content: NoticeMessageContent },
     Text { content: TextMessageContent },
+    Location { content: LocationContent },
 }
 
 impl From<MessageType> for RumaMessageType {
@@ -552,6 +554,9 @@ impl From<MessageType> for RumaMessageType {
                     formatted: content.formatted.map(Into::into),
                 }))
             }
+            MessageType::Location { content } => Self::Location(
+                RumaLocationMessageEventContent::new(content.body.clone(), content.geo_uri.clone()),
+            ),
         }
     }
 }
@@ -607,6 +612,9 @@ impl TryFrom<RumaMessageType> for MessageType {
                     body: c.body.clone(),
                     formatted: c.formatted.as_ref().map(Into::into),
                 },
+            },
+            RumaMessageType::Location(c) => MessageType::Location {
+                content: LocationContent { body: c.body.clone(), geo_uri: c.geo_uri.clone() },
             },
             _ => bail!("Unsupported type"),
         };
@@ -848,6 +856,12 @@ pub struct NoticeMessageContent {
 pub struct TextMessageContent {
     pub body: String,
     pub formatted: Option<FormattedBody>,
+}
+
+#[derive(Clone, uniffi::Record)]
+pub struct LocationContent {
+    pub body: String,
+    pub geo_uri: String,
 }
 
 #[derive(Clone, uniffi::Record)]
