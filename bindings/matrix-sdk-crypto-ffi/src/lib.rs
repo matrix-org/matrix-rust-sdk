1369c1954c15069411f20f3743765d696cf709ab
valere <valeref@matrix.org>
2023-06-22 09:33:11 +0200

    bindings: Ignore silently malformed userId in migration of tracked users

--- bindings/matrix-sdk-crypto-ffi/src/lib.rs
+++ bindings/matrix-sdk-crypto-ffi/src/lib.rs
@@ -286,7 +286,8 @@ async fn migrate_data(
     let tracked_users: Vec<_> = data
         .tracked_users
         .into_iter()
-        .map(|u| Ok(((parse_user_id(&u)?), true)))
+        .flat_map(|s| parse_user_id(&s))
+        .map(|u| Ok((u, true)))
         .collect::<anyhow::Result<_>>()?;
 
     let tracked_users: Vec<_> = tracked_users.iter().map(|(u, d)| (&**u, *d)).collect();
@@ -967,7 +968,8 @@ mod test {
                "@ganfra146:matrix.org",
                "@this-is-me:matrix.org",
                "@Amandine:matrix.org",
-               "@ganfra:matrix.org"
+               "@ganfra:matrix.org",
+               "NotAUser%ID"
             ],
             "room_settings": {
                 "!AZkqtjvtwPAuyNOXEt:matrix.org": {
@@ -1035,6 +1037,11 @@ mod test {
 
         let settings3 = machine.get_room_settings("!XYZ:matrix.org".into())?;
         assert!(settings3.is_none());
+
+        assert!(machine.is_user_tracked("@ganfra146:matrix.org".into()).unwrap());
+        assert!(machine.is_user_tracked("@Amandine:matrix.org".into()).unwrap());
+        assert!(machine.is_user_tracked("@this-is-me:matrix.org".into()).unwrap());
+        assert!(machine.is_user_tracked("@ganfra:matrix.org".into()).unwrap());
 
         Ok(())
     }
