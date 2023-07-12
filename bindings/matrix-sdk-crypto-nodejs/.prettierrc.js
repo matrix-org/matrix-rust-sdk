6e10eb9efb595f2f463022454d0af7d265dfb16b
Jonas Platte <jplatte@matrix.org>
2023-07-12 12:46:31 +0200

    Remove matrix-sdk-crypto-js
    It now lives in its own repository at
    https://github.com/matrix-org/matrix-rust-sdk-crypto-nodejs

--- bindings/matrix-sdk-crypto-nodejs/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-// prettier configuration: the same as the conventions used throughout Matrix.org
-// see: https://github.com/matrix-org/eslint-plugin-matrix-org/blob/main/.prettierrc.js
-
-module.exports = {
-    printWidth: 120,
-    tabWidth: 4,
-    quoteProps: "consistent",
-    trailingComma: "all",
-};
