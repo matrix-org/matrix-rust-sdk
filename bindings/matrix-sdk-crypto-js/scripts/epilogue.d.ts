5abc781e1f0470cdf8d0b0c63f41f61d31640f4c
Jonas Platte <jplatte@matrix.org>
2023-07-13 10:21:10 +0200

    Remove matrix-sdk-crypto-js
    It now lives in its own repository at
    https://github.com/matrix-org/matrix-rust-sdk-crypto-web

--- bindings/matrix-sdk-crypto-js/scripts/epilogue.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Load the WebAssembly module in the background, if it has not already been loaded.
- *
- * Returns a promise which will resolve once the other methods are ready.
- *
- * @returns {Promise<void>}
- */
-export function initAsync(): Promise<void>;
