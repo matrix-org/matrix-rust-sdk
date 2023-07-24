matrix-org/matrix-rust-sdk

commit fbf3431287f752a985cfe565bf581d1bd9eec1fd

Rename proposals (#3)
* rename

Signed-off-by: Timo K <toger5@hotmail.de>

* no more action wording

Signed-off-by: Timo K <toger5@hotmail.de>

* Change from widget message to use Request again

Signed-off-by: Timo K <toger5@hotmail.de>

---------

Signed-off-by: Timo K <toger5@hotmail.de>

crates/matrix-sdk/src/widget_api/capabilities.rs
@@ -2,16 +2,17 @@ use serde::{Deserialize, Serialize};
 use url::Url;
 
 #[derive(Debug, Default, Deserialize, Serialize)]
-pub struct Options {
+pub struct Capabilities {
     navigate: bool,
 }
 
-pub struct Capabilities {
+/// A wrapper for the matrix client that only exposes what is available through the capabilities.
+pub struct ClientCapabilities {
     pub navigate: Option<Box<dyn Fn(Url) + Send + Sync + 'static>>,
 }
 
-impl<'t> From<&'t Capabilities> for Options {
-    fn from(capabilities: &'t Capabilities) -> Self {
+impl<'t> From<&'t ClientCapabilities> for Capabilities {
+    fn from(capabilities: &'t ClientCapabilities) -> Self {
         Self { navigate: capabilities.navigate.is_some() }
     }
 }

crates/matrix-sdk/src/widget_api/messages/from_widget/mod.rs
@@ -1,15 +1,15 @@
-pub mod messages;
+pub mod message_types;
 pub mod reply;
 
 pub use self::{
-    messages::{ApiVersion, Incoming, SupportedVersions},
+    message_types::{ApiVersion, FromWidgetMessage, SupportedVersions},
     reply::Reply,
 };
 pub use super::super::Error;
 
-pub struct Request<C, R> {
-    pub content: C,
-    reply: Reply<C, R>,
+pub struct Request<ReqBody, ResBody> {
+    pub content: ReqBody,
+    reply: Reply<ReqBody, ResBody>,
 }
 
 impl<C, R> Request<C, R> {

crates/matrix-sdk/src/widget_api/messages/from_widget/reply.rs
@@ -1,19 +1,19 @@
 use tokio::sync::oneshot::Sender;
 
-use super::super::message::{Message, Response};
+use super::super::message::{MessageBody, Response};
 
 pub struct Reply<Req, Resp> {
-    request: Message<Req, ()>,
-    response: Sender<Message<Req, Resp>>,
+    request: MessageBody<Req, ()>,
+    response: Sender<MessageBody<Req, Resp>>,
 }
 
 impl<Req, Resp> Reply<Req, Resp> {
-    pub fn new(request: Message<Req, ()>, response: Sender<Message<Req, Resp>>) -> Self {
+    pub fn new(request: MessageBody<Req, ()>, response: Sender<MessageBody<Req, Resp>>) -> Self {
         Self { request, response }
     }
 
     pub fn reply(self, response: Resp) -> Result<(), Resp> {
-        let message = Message {
+        let message = MessageBody {
             widget_id: self.request.widget_id,
             request_id: self.request.request_id,
             request: self.request.request,

crates/matrix-sdk/src/widget_api/messages/message.rs
@@ -1,7 +1,18 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Message<Req, Resp> {
+use super::{FromWidgetMessage, ToWidgetMessage};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "api")]
+pub enum Message {
+    #[serde(rename = "fromWidget")]
+    FromWidget(FromWidgetMessage),
+    #[serde(rename = "toWidget")]
+    ToWidget(ToWidgetMessage),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MessageBody<Req, Resp> {
     pub request_id: String,
     pub widget_id: String,
     #[serde(rename = "data")]
@@ -9,19 +20,19 @@ pub struct Message<Req, Resp> {
     pub response: Option<Response<Resp>>,
 }
 
-#[derive(Debug, Serialize, Deserialize )]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(untagged)]
 pub enum Response<Resp> {
     Error(WidgetError),
     Response(Resp),
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct WidgetError {
     pub error: WidgetErrorMessage,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct WidgetErrorMessage {
     pub message: String,
 }

crates/matrix-sdk/src/widget_api/messages/mod.rs
@@ -1,8 +1,8 @@
-mod incoming;
-mod message;
-mod outgoing;
+mod from_widget;
+pub mod message;
+mod to_widget;
 
 pub use self::{
-    incoming::{ApiVersion, Incoming, SupportedVersions},
-    outgoing::{CapabilitiesUpdated, Outgoing, SendMeCapabilities},
+    from_widget::{ApiVersion, FromWidgetMessage, SupportedVersions},
+    to_widget::{CapabilitiesUpdated, ToWidgetMessage, ToWidget, SendMeCapabilities},
 };

crates/matrix-sdk/src/widget_api/messages/to_widget/message_types.rs
@@ -1,0 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "action")]
+pub enum ToWidgetMessage {
+    #[serde(rename = "capabilities")]
+    SendMeCapabilities,
+    #[serde(rename = "notify_capabilities")]
+    CapabilitiesUpdated
+}

crates/matrix-sdk/src/widget_api/messages/to_widget/mod.rs
@@ -1,0 +1,18 @@
+use super::super::capabilities::Capabilities as CapabilityRequest;
+
+pub mod message_types;
+
+pub use self::message_types::ToWidgetMessage;
+pub trait ToWidget {
+    type Response;
+}
+
+pub struct SendMeCapabilities;
+impl ToWidget for SendMeCapabilities {
+    type Response = CapabilityRequest;
+}
+
+pub type CapabilitiesUpdated = CapabilityRequest;
+impl ToWidget for CapabilitiesUpdated {
+    type Response = ();
+}

crates/matrix-sdk/src/widget_api/messages/from_widget/message_types.rs
@@ -1,0 +1,27 @@
+use serde::{Deserialize, Serialize};
+
+use url::Url;
+
+use crate::widget_api::messages::message::MessageBody;
+
+use super::Request;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "action")]
+pub enum FromWidgetMessage {
+    #[serde(rename = "supported_api_versions")]
+    GetSupportedApiVersion(Request<(), SupportedVersions>),
+    #[serde(rename = "content_loaded")]
+    ContentLoaded(Request<(), ()>),
+    #[serde(rename = "org.matrix.msc2931.navigate")]
+    Navigate(Request<Url, Result<(), &'static str>>),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SupportedVersions {
+    pub versions: Vec<ApiVersion>,
+}
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum ApiVersion {
+    PreRelease,
+}

commit 871238984ef09a203c51d9cd403192660db8cc25

widget_api: fix and re-structure errors caused by
https://github.com/toger5/matrix-rust-sdk/pull/3

crates/matrix-sdk/src/lib.rs
@@ -42,7 +42,6 @@ pub mod media;
 pub mod notification_settings;
 pub mod room;
 pub mod sync;
-pub mod widget_api;
 
 #[cfg(feature = "experimental-sliding-sync")]
 pub mod sliding_sync;

crates/matrix-sdk/src/widget_api/capabilities.rs
@@ -1,18 +1,18 @@
 use serde::{Deserialize, Serialize};
 use url::Url;
 
-#[derive(Debug, Default, Deserialize, Serialize)]
-pub struct Capabilities {
+#[derive(Debug, Default, Deserialize, Serialize, Clone)]
+pub struct Options {
     navigate: bool,
 }
 
-/// A wrapper for the matrix client that only exposes what is available through the capabilities.
-pub struct ClientCapabilities {
+#[allow(missing_debug_implementations)]
+pub struct Capabilities {
     pub navigate: Option<Box<dyn Fn(Url) + Send + Sync + 'static>>,
 }
 
-impl<'t> From<&'t ClientCapabilities> for Capabilities {
-    fn from(capabilities: &'t ClientCapabilities) -> Self {
+impl<'t> From<&'t Capabilities> for Options {
+    fn from(capabilities: &'t Capabilities) -> Self {
         Self { navigate: capabilities.navigate.is_some() }
     }
 }

crates/matrix-sdk/src/widget_api/handler/incoming.rs
@@ -1,19 +1,12 @@
 use serde::{Deserialize, Serialize};
-
 use url::Url;
 
-use crate::widget_api::messages::message::MessageBody;
+pub use super::{Error, Request};
 
-use super::Request;
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(tag = "action")]
-pub enum FromWidgetMessage {
-    #[serde(rename = "supported_api_versions")]
+#[allow(missing_debug_implementations)]
+pub enum Message {
     GetSupportedApiVersion(Request<(), SupportedVersions>),
-    #[serde(rename = "content_loaded")]
     ContentLoaded(Request<(), ()>),
-    #[serde(rename = "org.matrix.msc2931.navigate")]
     Navigate(Request<Url, Result<(), &'static str>>),
 }
 

crates/matrix-sdk/src/widget_api/handler/mod.rs
@@ -1,17 +1,22 @@
 use async_trait::async_trait;
 
-use super::{
-    capabilities::{Capabilities, Options as CapabilitiesReq},
-    messages::{
-        ApiVersion, CapabilitiesUpdated, Incoming, Outgoing, SendMeCapabilities, SupportedVersions,
-    },
-    Error, Result,
+use super::capabilities::{Capabilities, Options as CapabilitiesReq};
+
+pub use self::{
+    incoming::{ApiVersion, Message as Incoming, SupportedVersions},
+    outgoing::Message as Outgoing,
+    request::Request,
 };
+pub use super::{Error, Result};
+
+mod incoming;
+mod outgoing;
+mod request;
 
 #[async_trait]
 pub trait Driver {
     async fn initialise(&mut self, req: CapabilitiesReq) -> Result<Capabilities>;
-    async fn send<T: Outgoing>(&mut self, req: T) -> Result<T::Response>;
+    async fn send(&mut self, message: Outgoing) -> Result<()>;
 }
 
 #[allow(missing_debug_implementations)]
@@ -33,12 +38,17 @@ impl<T: Driver> MessageHandler<T> {
                     return Err(Error::WidgetError("Content loaded twice".to_string()));
                 }
 
-                let requested = self.driver.send(SendMeCapabilities).await?;
-                let capabilities = self.driver.initialise(requested).await?;
+                let (req, resp) = Request::new(());
+                self.driver.send(Outgoing::SendMeCapabilities(req)).await?;
+                let options = resp.await.map_err(|_| Error::WidgetDied)?;
+
+                let capabilities = self.driver.initialise(options).await?;
                 self.capabilities = Some(capabilities);
 
-                let approved: CapabilitiesUpdated = self.capabilities.as_ref().unwrap().into();
-                self.driver.send(approved).await?;
+                let approved: CapabilitiesReq = self.capabilities.as_ref().unwrap().into();
+                let (req, resp) = Request::new(approved);
+                self.driver.send(Outgoing::CapabilitiesUpdated(req)).await?;
+                resp.await.map_err(|_| Error::WidgetDied)?;
             }
 
             Incoming::GetSupportedApiVersion(r) => {
@@ -48,7 +58,7 @@ impl<T: Driver> MessageHandler<T> {
             Incoming::Navigate(r) => {
                 match self.capabilities.as_ref().and_then(|c| c.navigate.as_ref()) {
                     Some(navigate) => {
-                        navigate(r.content.clone());
+                        navigate(r.clone());
                         r.reply(Ok(()))?;
                     }
                     None => {

crates/matrix-sdk/src/widget_api/handler/outgoing.rs
@@ -1,0 +1,7 @@
+use super::{super::capabilities::Options, Request};
+
+#[allow(missing_debug_implementations)]
+pub enum Message {
+    SendMeCapabilities(Request<(), Options>),
+    CapabilitiesUpdated(Request<Options, ()>),
+}

crates/matrix-sdk/src/widget_api/handler/request.rs
@@ -1,0 +1,30 @@
+use std::ops::Deref;
+
+use tokio::sync::oneshot::{Receiver, Sender};
+
+use super::{Error, Result};
+
+#[allow(missing_debug_implementations)]
+pub struct Request<Req, Resp> {
+    content: Req,
+    reply: Sender<Resp>,
+}
+
+impl<C, R> Request<C, R> {
+    pub fn new(content: C) -> (Self, Receiver<R>) {
+        let (reply, response) = tokio::sync::oneshot::channel();
+        (Self { content, reply }, response)
+    }
+
+    pub fn reply(self, response: R) -> Result<()> {
+        self.reply.send(response).map_err(|_| Error::WidgetDied)
+    }
+}
+
+impl<Req, Resp> Deref for Request<Req, Resp> {
+    type Target = Req;
+
+    fn deref(&self) -> &Self::Target {
+        &self.content
+    }
+}

crates/matrix-sdk/src/widget_api/messages.rs
@@ -1,0 +1,90 @@
+use serde::{Deserialize, Serialize};
+
+use super::{capabilities::Options, handler::ApiVersion};
+
+// #[derive(Serialize, Deserialize, Debug)]
+// #[serde(tag = "api")]
+// pub enum JsonMessage {
+//     #[serde(rename = "fromWidget")]
+//     FromWidget(FromWidget),
+//     #[serde(rename = "toWidget")]
+//     ToWidget(ToWidget),
+// }
+
+#[derive(Debug, Serialize, Clone)]
+#[serde(tag = "action")]
+pub enum ToWidget {
+    // Requests.
+    #[serde(rename = "capabilities")]
+    SendMeCapabilities,
+    #[serde(rename = "notify_capabilities")]
+    CapabilitiesUpdated,
+
+    // Responses.
+    #[serde(rename = "supported_api_versions")]
+    SupportedApiVersions(Message<Vec<ApiVersion>, ()>),
+    #[serde(rename = "content_loaded")]
+    ContentLoaded(Message<(), ()>),
+    #[serde(rename = "org.matrix.msc2931.navigate")]
+    Navigate(Message<String, ()>),
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(tag = "action")]
+pub enum FromWidget {
+    // Requests.
+    #[serde(rename = "supported_api_versions")]
+    SupportedApiVersions(Message<(), ()>),
+    #[serde(rename = "content_loaded")]
+    ContentLoaded(Message<(), ()>),
+    #[serde(rename = "org.matrix.msc2931.navigate")]
+    Navigate(Message<String, ()>),
+
+    // Responses.
+    #[serde(rename = "capabilities")]
+    Capabilities(Message<(), Options>),
+    #[serde(rename = "notify_capabilities")]
+    CapabilitiesConfirmed(Message<(), ()>),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Message<Req, Resp> {
+    #[serde(flatten)]
+    pub header: Header,
+    #[serde(rename = "data")]
+    pub request: Req,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub response: Option<Response<Resp>>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Header {
+    pub request_id: String,
+    pub widget_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(untagged)]
+pub enum Response<Resp> {
+    Error(WidgetError),
+    Response(Resp),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct WidgetError {
+    pub error: WidgetErrorMessage,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct WidgetErrorMessage {
+    pub message: String,
+}
+
+impl<Resp> Into<Result<Resp, WidgetError>> for Response<Resp> {
+    fn into(self) -> Result<Resp, WidgetError> {
+        match self {
+            Response::Error(err) => Err(err),
+            Response::Response(resp) => Ok(resp),
+        }
+    }
+}

crates/matrix-sdk/src/widget_api/messages/from_widget/mod.rs
@@ -1,19 +1,0 @@
-pub mod message_types;
-pub mod reply;
-
-pub use self::{
-    message_types::{ApiVersion, FromWidgetMessage, SupportedVersions},
-    reply::Reply,
-};
-pub use super::super::Error;
-
-pub struct Request<ReqBody, ResBody> {
-    pub content: ReqBody,
-    reply: Reply<ReqBody, ResBody>,
-}
-
-impl<C, R> Request<C, R> {
-    pub fn reply(self, response: R) -> Result<(), Error> {
-        self.reply.reply(response).map_err(|_| Error::WidgetDied)
-    }
-}

crates/matrix-sdk/src/widget_api/messages/from_widget/reply.rs
@@ -1,29 +1,0 @@
-use tokio::sync::oneshot::Sender;
-
-use super::super::message::{MessageBody, Response};
-
-pub struct Reply<Req, Resp> {
-    request: MessageBody<Req, ()>,
-    response: Sender<MessageBody<Req, Resp>>,
-}
-
-impl<Req, Resp> Reply<Req, Resp> {
-    pub fn new(request: MessageBody<Req, ()>, response: Sender<MessageBody<Req, Resp>>) -> Self {
-        Self { request, response }
-    }
-
-    pub fn reply(self, response: Resp) -> Result<(), Resp> {
-        let message = MessageBody {
-            widget_id: self.request.widget_id,
-            request_id: self.request.request_id,
-            request: self.request.request,
-            response: Some(Response::Response(response)),
-        };
-
-        self.response.send(message).map_err(|r| {
-            // Safe to unwrap here, because the `response` is always `Some()` (see above).
-            let result: Result<Resp, _> = r.response.unwrap().into();
-            result.unwrap()
-        })
-    }
-}

crates/matrix-sdk/src/widget_api/messages/message.rs
@@ -1,47 +1,0 @@
-use serde::{Deserialize, Serialize};
-
-use super::{FromWidgetMessage, ToWidgetMessage};
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(tag = "api")]
-pub enum Message {
-    #[serde(rename = "fromWidget")]
-    FromWidget(FromWidgetMessage),
-    #[serde(rename = "toWidget")]
-    ToWidget(ToWidgetMessage),
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct MessageBody<Req, Resp> {
-    pub request_id: String,
-    pub widget_id: String,
-    #[serde(rename = "data")]
-    pub request: Req,
-    pub response: Option<Response<Resp>>,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(untagged)]
-pub enum Response<Resp> {
-    Error(WidgetError),
-    Response(Resp),
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct WidgetError {
-    pub error: WidgetErrorMessage,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct WidgetErrorMessage {
-    pub message: String,
-}
-
-impl<Resp> Into<Result<Resp, WidgetError>> for Response<Resp> {
-    fn into(self) -> Result<Resp, WidgetError> {
-        match self {
-            Response::Error(err) => Err(err),
-            Response::Response(resp) => Ok(resp),
-        }
-    }
-}

crates/matrix-sdk/src/widget_api/messages/mod.rs
@@ -1,8 +1,0 @@
-mod from_widget;
-pub mod message;
-mod to_widget;
-
-pub use self::{
-    from_widget::{ApiVersion, FromWidgetMessage, SupportedVersions},
-    to_widget::{CapabilitiesUpdated, ToWidgetMessage, ToWidget, SendMeCapabilities},
-};

crates/matrix-sdk/src/widget_api/messages/to_widget/message_types.rs
@@ -1,10 +1,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(tag = "action")]
-pub enum ToWidgetMessage {
-    #[serde(rename = "capabilities")]
-    SendMeCapabilities,
-    #[serde(rename = "notify_capabilities")]
-    CapabilitiesUpdated
-}

crates/matrix-sdk/src/widget_api/messages/to_widget/mod.rs
@@ -1,18 +1,0 @@
-use super::super::capabilities::Capabilities as CapabilityRequest;
-
-pub mod message_types;
-
-pub use self::message_types::ToWidgetMessage;
-pub trait ToWidget {
-    type Response;
-}
-
-pub struct SendMeCapabilities;
-impl ToWidget for SendMeCapabilities {
-    type Response = CapabilityRequest;
-}
-
-pub type CapabilitiesUpdated = CapabilityRequest;
-impl ToWidget for CapabilitiesUpdated {
-    type Response = ();
-}