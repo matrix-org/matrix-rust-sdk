a952cbe4ce41efb85d26f8d48dcceb9499aa46cd
Benjamin Bouvier <public@benj.me>
2023-08-03 11:25:18 +0200

    feat(sync service): split the encryption and room list sliding syncs in different tasks

--- crates/matrix-sdk-ui/tests/integration/sync_service.rs
+++ crates/matrix-sdk-ui/tests/integration/sync_service.rs
@@ -1,10 +1,21 @@
-use std::{
-    sync::{Arc, Mutex},
-    time::Duration,
-};
+// Copyright 2023 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for that specific language governing permissions and
+// limitations under the License.
+
+use std::{sync::Mutex, time::Duration};
 
 use matrix_sdk_test::async_test;
-use matrix_sdk_ui::sync_service::{SyncService, SyncServiceState};
+use matrix_sdk_ui::sync_service::{State, SyncService};
 use serde_json::json;
 use wiremock::{Match as _, Mock, MockGuard, MockServer, Request, ResponseTemplate};
 
@@ -14,7 +25,7 @@ use crate::{
 };
 
 /// Sets up a sliding sync server that use different `pos` values for the
-/// encrptyion and the room sync. Will respond after 50 milliseconds.
+/// encrptyion and the room sync.
 async fn setup_mocking_sliding_sync_server(server: &MockServer) -> MockGuard {
     let encryption_pos = Mutex::new(0);
     let room_pos = Mutex::new(0);
@@ -53,32 +64,23 @@ async fn test_sync_service_state() -> anyhow::Result<()> {
         SyncService::builder(client).with_encryption_sync(false, None).build().await.unwrap();
 
     // At first, the sync service is sleeping.
-    assert_eq!(sync_service.state().get(), SyncServiceState::Idle);
+    assert_eq!(sync_service.state(), State::Idle);
     assert!(server.received_requests().await.unwrap().is_empty());
 
     // After starting, the sync service is, well, running.
     sync_service.start().await?;
-    assert_eq!(sync_service.state().get(), SyncServiceState::Running);
+    assert_eq!(sync_service.state(), State::BothRunning);
 
     // Restarting while started doesn't change the current state.
     sync_service.start().await?;
-    assert_eq!(sync_service.state().get(), SyncServiceState::Running);
+    assert_eq!(sync_service.state(), State::BothRunning);
 
     // Let the server respond a few times.
     tokio::time::sleep(Duration::from_millis(300)).await;
 
     // Pausing will stop both syncs, after a bit of delay.
     sync_service.pause()?;
-
-    // Since this is racy: either it's already terminated, or we wait a bit and then
-    // it'll be terminated.
-    let current_state = sync_service.state().get();
-    if !matches!(current_state, SyncServiceState::Terminated) {
-        assert_eq!(current_state, SyncServiceState::Running);
-
-        tokio::time::sleep(Duration::from_millis(100)).await;
-        assert_eq!(sync_service.state().get(), SyncServiceState::Terminated);
-    }
+    assert_eq!(sync_service.state(), State::Idle);
 
     let mut num_encryption_sync_requests: i32 = 0;
     let mut num_room_list_requests = 0;
@@ -112,7 +114,10 @@ async fn test_sync_service_state() -> anyhow::Result<()> {
 
     assert!(num_encryption_sync_requests > 0);
     assert!(num_room_list_requests > 0);
-    assert!((num_encryption_sync_requests - num_room_list_requests).abs() <= 1);
+    assert!(
+        (num_encryption_sync_requests - num_room_list_requests).abs() <= 1,
+        "encryption:{num_encryption_sync_requests} / room_list:{num_room_list_requests}"
+    );
     assert!(latest_room_list_pos.is_some());
 
     // Now reset the server, wait for a bit that it doesn't receive extra requests.
@@ -127,7 +132,7 @@ async fn test_sync_service_state() -> anyhow::Result<()> {
     // When restarting and waiting a bit, the server gets new requests, starting at
     // the same position than just before being stopped.
     sync_service.start().await?;
-    assert_eq!(sync_service.state().get(), SyncServiceState::Running);
+    assert_eq!(sync_service.state(), State::BothRunning);
 
     tokio::time::sleep(Duration::from_millis(100)).await;
 
@@ -170,44 +175,62 @@ async fn test_sync_service_state() -> anyhow::Result<()> {
 
     assert!(num_encryption_sync_requests > 0);
     assert!(num_room_list_requests > 0);
-    assert!((num_encryption_sync_requests - num_room_list_requests).abs() <= 1);
+    assert!(
+        (num_encryption_sync_requests - num_room_list_requests).abs() <= 1,
+        "encryption:{num_encryption_sync_requests} / room_list:{num_room_list_requests}"
+    );
 
     Ok(())
 }
 
 #[async_test]
 async fn test_sync_service_no_encryption_state() -> anyhow::Result<()> {
-    let (client, _server) = logged_in_client().await;
+    let (client, server) = logged_in_client().await;
 
     let sync_service = SyncService::builder(client).build().await.unwrap();
-    let states = Arc::new(Mutex::new(Vec::new()));
-
-    let mut state = sync_service.state();
-
-    let task_states = states.clone();
-    let handle = tokio::spawn(async move {
-        while let Some(state) = state.next().await {
-            task_states.lock().unwrap().push(state);
-        }
-    });
 
     // At first, the sync service is sleeping.
-    assert_eq!(sync_service.state().get(), SyncServiceState::Idle);
+    assert_eq!(sync_service.state(), State::Idle);
 
     // After starting, the sync service will be running for a very short while, then
     // getting into the error state.
     sync_service.start().await?;
+    assert_eq!(sync_service.state(), State::BothRunning);
 
     // Wait a bit for the underlying sync to fail (server isn't mocked here, so will
     // 404).
     tokio::time::sleep(Duration::from_millis(200)).await;
 
-    // Drop the service so the stream spawned in the task is aborted too.
-    drop(sync_service);
-    handle.await?;
+    // The sync service is internally marked as "not running".
+    assert_eq!(sync_service.state(), State::OnlyEncryptionSync);
 
-    let states = states.lock().unwrap();
-    assert_eq!(*states, &[SyncServiceState::Running, SyncServiceState::Error,]);
+    // Now, start mocking the endpoint, and make sure the room sliding sync will
+    // restart properly.
+    let _guard = setup_mocking_sliding_sync_server(&server).await;
+
+    sync_service.start().await?;
+
+    // Wait for the first response to arrive.
+    tokio::task::yield_now().await;
+
+    let mut has_room_list_requests = false;
+    for request in &server.received_requests().await.expect("Request recording has been disabled") {
+        if !SlidingSyncMatcher.matches(request) {
+            continue;
+        }
+        let mut json_value = serde_json::from_slice::<serde_json::Value>(&request.body).unwrap();
+        if let Some(root) = json_value.as_object_mut() {
+            if let Some(conn_id) = root.get("conn_id").and_then(|obj| obj.as_str()) {
+                if conn_id == "room-list" {
+                    has_room_list_requests = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    assert!(has_room_list_requests);
+    assert_eq!(sync_service.state(), State::BothRunning);
 
     Ok(())
 }
