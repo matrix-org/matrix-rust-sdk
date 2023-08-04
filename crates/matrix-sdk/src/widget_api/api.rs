b2dc0145a54c9dc33e2bdcaab5f8886b9aaf3c3f
Daniel Abramov <daniel.abramov@element.io>
2023-08-04 23:17:56 +0200

    widget_api(wip): simplify incoming process macro

--- crates/matrix-sdk/src/widget_api/api.rs
+++ crates/matrix-sdk/src/widget_api/api.rs
@@ -3,21 +3,24 @@ use std::sync::{Arc, Mutex};
 
 use async_trait::async_trait;
 use serde_json::from_str as from_json;
-use tokio::sync::{mpsc::{UnboundedReceiver as Receiver, UnboundedSender as Sender}, oneshot};
+use tokio::sync::{
+    mpsc::{UnboundedReceiver as Receiver, UnboundedSender as Sender},
+    oneshot,
+};
 use uuid::Uuid;
 
 use super::{
-    handler::{Driver, Incoming, MessageHandler, Request, OutgoingMessage},
+    handler::{Driver, Incoming, MessageHandler, OutgoingMessage, Request},
     messages::{
         from_widget::{
-            FromWidgetMessage as FromWidgetAction, ReadEventRequest, SendEventRequest,
-            SendToDeviceRequest, ReadEventResponse,
+            FromWidgetMessage as FromWidgetAction, ReadEventRequest, ReadEventResponse,
+            SendEventRequest, SendToDeviceRequest,
         },
+        openid,
         to_widget::{
             CapabilitiesUpdatedRequest, SendMeCapabilitiesResponse,
             ToWidgetMessage as ToWidgetAction,
         },
-        openid,
         Header, Message as WidgetMessage, MessageBody, Response as ResponseBody,
     },
     Error, Result,
@@ -43,10 +46,8 @@ impl Transport {
 
 macro_rules! handle_incoming {
     {
-        from_widget = { $action:expr }
-        env = { $handler:expr, $outgoing:expr },
-        identifiers = { $msg:ident, $resp:ident },
-        $($req_type:ident => {$incoming_type:ident { $req_expr:expr } -> { $resp_expr:expr }}),* ,
+        chain = { $action:ident($msg:ident) -> $handler:ident -> $resp:ident },
+        $($req_type:ident ($req_expr:expr) -> $incoming_type:ident ($resp_expr:expr) ),* ,
     } => {
         match $action {
             $(
@@ -63,9 +64,7 @@ macro_rules! handle_incoming {
                     };
 
                     $msg.response = Some(response);
-                    $outgoing
-                        .send(WidgetMessage::FromWidget(FromWidgetAction::$req_type($msg)))
-                        .map_err(|_| Error::WidgetDied)?;
+                    FromWidgetAction::$req_type($msg)
                 }
             )*
         }
@@ -97,44 +96,38 @@ async fn process<T: Driver>(
 
     match decoded {
         WidgetMessage::ToWidget(to_widget) => {
-            let pending = map.lock().unwrap().remove(to_widget.id()).ok_or(Error::UnexpectedResponse)?;
+            let pending =
+                map.lock().unwrap().remove(to_widget.id()).ok_or(Error::UnexpectedResponse)?;
             pending.send(to_widget).map_err(|_| Error::WidgetDied).expect("Handler died");
         }
 
         WidgetMessage::FromWidget(from_widget) => {
-            handle_incoming! {
-                from_widget = { from_widget }
-                env = {  handler, outgoing },
-                identifiers = { msg, resp },
+            let response = handle_incoming! {
+                chain = { from_widget(msg) -> handler -> resp },
 
-                GetSupportedApiVersion => {
-                    GetSupportedApiVersion { msg.request } -> { resp }
-                },
+                GetSupportedApiVersion (msg.request)
+                    -> GetSupportedApiVersion (resp),
 
-                ContentLoaded => {
-                    ContentLoaded { msg.request } -> { resp }
-                },
+                ContentLoaded (msg.request)
+                    -> ContentLoaded (resp),
 
-                GetOpenId => {
-                    GetOpenID { openid::Request{ id: msg.header.request_id.clone() } } -> { resp }
-                },
+                GetOpenId (openid::Request{ id: msg.header.request_id.clone() })
+                    -> GetOpenID (resp),
 
-                SendToDevice => {
-                    SendToDeviceRequest { msg.request.clone() } -> { resp }
-                },
+                SendToDevice (msg.request.clone())
+                    -> SendToDeviceRequest (resp),
 
-                SendEvent => {
-                    SendEvent { msg.request.clone() } -> { resp }
-                },
+                SendEvent (msg.request.clone())
+                    -> SendEvent (resp),
 
-                ReadEvent => {
-                    ReadEvents { msg.request.clone().into() } -> { ReadEventResponse { events: resp }}
-                },
+                ReadEvent (msg.request.clone().into())
+                    -> ReadEvents (ReadEventResponse { events: resp }),
 
-                ReadRelations => {
-                    ReadEvents { msg.request.clone().into() } -> { ReadEventResponse { events: resp }}
-                },
-            }
+                ReadRelations (msg.request.clone().into())
+                    -> ReadEvents (ReadEventResponse { events: resp }),
+            };
+
+            outgoing.send(WidgetMessage::FromWidget(response)).map_err(|_| Error::WidgetDied)?;
         }
     }
 
