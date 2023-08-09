1b8768becc0c6582b3c280a503498a1aa09c8c45
Jonas Platte <jplatte@matrix.org>
2023-08-09 12:09:05 +0200

    Use AsyncCell instead of mpsc channel

--- crates/matrix-sdk-ui/src/room_list_service/room_list.rs
+++ crates/matrix-sdk-ui/src/room_list_service/room_list.rs
@@ -12,9 +12,11 @@
 // See the License for that specific language governing permissions and
 // limitations under the License.
 
-use std::future::ready;
+use std::{future::ready, sync::Arc};
 
+use async_cell::sync::AsyncCell;
 use async_rx::StreamExt as _;
+use async_stream::stream;
 use eyeball::{SharedObservable, Subscriber};
 use eyeball_im::{Vector, VectorDiff};
 use futures_util::{pin_mut, stream, Stream, StreamExt as _};
@@ -22,8 +24,6 @@ use matrix_sdk::{
     executor::{spawn, JoinHandle},
     RoomListEntry, SlidingSync, SlidingSyncList,
 };
-use tokio::sync::mpsc;
-use tokio_stream::wrappers::ReceiverStream;
 
 use super::{Error, State};
 
@@ -142,15 +142,17 @@ impl RoomList {
     pub fn entries_with_dynamic_filter(
         &self,
     ) -> (impl Stream<Item = Vec<VectorDiff<RoomListEntry>>>, DynamicRoomListFilter) {
-        // FIXME: Should ideally be a single-consumer watch channel / observable.
-        let (tx, rx) = mpsc::channel(1);
+        let filter_fn_cell = AsyncCell::shared();
+        let dynamic_filter = DynamicRoomListFilter::new(filter_fn_cell.clone());
 
         let list = self.sliding_sync_list.clone();
         let room_list_service_state = self.room_list_service_state.clone();
-        let stream = ReceiverStream::new(rx)
-            .map(move |filter_fn| {
+        let stream = stream! {
+            loop {
+                let filter_fn = filter_fn_cell.take().await;
                 let (items, stream) = list.room_list_filtered_stream(filter_fn);
-                stream::once(
+
+                yield stream::once(
                     // Reset the stream with all its items.
                     ready(vec![VectorDiff::Reset { values: items }]),
                 )
@@ -159,12 +161,11 @@ impl RoomList {
                     // `room_list_service_state` is changed.
                     stream.batch_with(room_list_service_state.clone().map(|_| ())),
                 )
-            })
-            .switch();
+            }
+        }
+        .switch();
 
-        let filter = DynamicRoomListFilter::new(tx);
-
-        (stream, filter)
+        (stream, dynamic_filter)
     }
 }
 
@@ -211,27 +212,31 @@ pub enum RoomListLoadingState {
 
 type BoxedFilterFn = Box<dyn Fn(&RoomListEntry) -> bool + Send + Sync>;
 
+/// Dynamic filter for the [`RoomList`] entries.
+///
+/// To get one value of this type, use [`RoomList::entries_with_dynamic_filter`]
 pub struct DynamicRoomListFilter {
-    tx: mpsc::Sender<BoxedFilterFn>,
+    inner: Arc<AsyncCell<BoxedFilterFn>>,
 }
 
 impl DynamicRoomListFilter {
-    fn new(tx: mpsc::Sender<BoxedFilterFn>) -> Self {
-        Self { tx }
+    fn new(inner: Arc<AsyncCell<BoxedFilterFn>>) -> Self {
+        Self { inner }
     }
 
     /// Set the filter.
     ///
     /// If the associated stream has been dropped, returns `false` to indicate
     /// the operation didn't have an effect.
-    ///
-    /// The future returned by this should usually resolve immediately. The only
-    /// case where it doesn't is when the associated stream is polled less
-    /// frequently than the filter is being set.
-    pub async fn set(
-        &self,
-        filter: impl Fn(&RoomListEntry) -> bool + Send + Sync + 'static,
-    ) -> bool {
-        self.tx.send(Box::new(filter)).await.is_ok()
+    pub fn set(&self, filter: impl Fn(&RoomListEntry) -> bool + Send + Sync + 'static) -> bool {
+        if Arc::strong_count(&self.inner) == 1 {
+            // there is no other reference to the boxed filter fn, setting it
+            // would be pointless (no new references can be created from self,
+            // either)
+            false
+        } else {
+            self.inner.set(Box::new(filter));
+            true
+        }
     }
 }
