ca3e53b1737414990a36ee2dd7927fb3018beda8
Daniel Abramov <daniel.abramov@element.io>
2023-08-16 18:36:24 +0200

    widget_api: rename driver -> matrix mod

--- crates/matrix-sdk/src/widget_api/mod.rs
+++ crates/matrix-sdk/src/widget_api/mod.rs
@@ -2,9 +2,13 @@
 #![allow(missing_docs)]
 
 pub mod api;
-pub mod driver;
 pub mod error;
 pub mod handler;
+pub mod matrix;
 pub mod messages;
 
 pub use self::error::{Error, Result};
+
+pub fn run_client_widget_api() {
+    println!("Hello, world!");
+}
