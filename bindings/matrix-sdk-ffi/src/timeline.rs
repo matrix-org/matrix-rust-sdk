ede3cac615ef250e11652c8f48908c608d99b7de
Matthew Hodgson <matthew@matrix.org>
2023-10-02 17:32:02 +0100

    switch to separate methods per review feedback

--- bindings/matrix-sdk-ffi/src/timeline.rs
+++ bindings/matrix-sdk-ffi/src/timeline.rs
@@ -56,30 +56,35 @@ pub fn message_event_content_new(
 #[uniffi::export]
 pub fn message_event_content_from_markdown(
     md: String,
-    emote: bool,
 ) -> Arc<RoomMessageEventContentWithoutRelation> {
-    if emote {
-        Arc::new(RoomMessageEventContentWithoutRelation::new(RumaMessageType::emote_markdown(md)))
-    } else {
-        Arc::new(RoomMessageEventContentWithoutRelation::new(RumaMessageType::text_markdown(md)))
-    }
+    Arc::new(RoomMessageEventContentWithoutRelation::new(RumaMessageType::text_markdown(md)))
+}
+
+#[uniffi::export]
+pub fn message_event_content_from_markdown_as_emote(
+    md: String,
+) -> Arc<RoomMessageEventContentWithoutRelation> {
+    Arc::new(RoomMessageEventContentWithoutRelation::new(RumaMessageType::emote_markdown(md)))
 }
 
 #[uniffi::export]
 pub fn message_event_content_from_html(
     body: String,
     html_body: String,
-    emote: bool,
 ) -> Arc<RoomMessageEventContentWithoutRelation> {
-    if emote {
-        Arc::new(RoomMessageEventContentWithoutRelation::new(RumaMessageType::emote_html(
-            body, html_body,
-        )))
-    } else {
-        Arc::new(RoomMessageEventContentWithoutRelation::new(RumaMessageType::text_html(
-            body, html_body,
-        )))
-    }
+    Arc::new(RoomMessageEventContentWithoutRelation::new(RumaMessageType::text_html(
+        body, html_body,
+    )))
+}
+
+#[uniffi::export]
+pub fn message_event_content_from_html_as_emote(
+    body: String,
+    html_body: String,
+) -> Arc<RoomMessageEventContentWithoutRelation> {
+    Arc::new(RoomMessageEventContentWithoutRelation::new(RumaMessageType::emote_html(
+        body, html_body,
+    )))
 }
 
 #[uniffi::export(callback_interface)]
