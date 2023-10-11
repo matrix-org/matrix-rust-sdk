5b1178d3b638095c7c5c27ca6b6d07edec87d455
Alfonso Grillo <alfogrillo@gmail.com>
2023-10-11 11:01:51 +0200

    Update ruma-events dependency

--- crates/matrix-sdk/src/encryption/mod.rs
+++ crates/matrix-sdk/src/encryption/mod.rs
@@ -197,9 +197,10 @@ impl Client {
                     thumbnail_source,
                     thumbnail_info
                 });
-                let content = assign!(ImageMessageEventContent::encrypted(body.to_owned(), file), {
-                    info: Some(Box::new(info))
-                });
+                let content =
+                    assign!(ImageMessageEventContent::encrypted(body.to_owned(), file), {
+                        info: Some(Box::new(info))
+                    });
                 MessageType::Image(content)
             }
             mime::AUDIO => {
@@ -228,9 +229,10 @@ impl Client {
                     thumbnail_source,
                     thumbnail_info
                 });
-                let content = assign!(VideoMessageEventContent::encrypted(body.to_owned(), file), {
-                    info: Some(Box::new(info))
-                });
+                let content =
+                    assign!(VideoMessageEventContent::encrypted(body.to_owned(), file), {
+                        info: Some(Box::new(info))
+                    });
                 MessageType::Video(content)
             }
             _ => {
