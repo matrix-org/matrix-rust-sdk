6b543cd81d6c3f1957d10a6c2f50d8dc190ada9a
Timo K <toger5@hotmail.de>
2023-10-18 18:57:59 +0200

    add call related functions to ffi room
    Signed-off-by: Timo K <toger5@hotmail.de>

--- bindings/matrix-sdk-ffi/src/room.rs
+++ bindings/matrix-sdk-ffi/src/room.rs
@@ -145,6 +145,18 @@ impl Room {
         self.inner.state().into()
     }
 
+    /// Is there a non expired membership with application "m.call" and scope "m.room" in this room
+    pub fn has_room_call(&self) -> bool {
+        self.inner.has_room_call()
+    }
+
+    /// Returns a Vec of userId's that participate in the room call. (matrix_rtc memberships with application
+    /// "m.call" and scope "m.room" are considered). A user can occur twice if they join with two devices.
+    /// (convert to a set depending if the different users are required or the amount of sessions).
+    pub fn room_call_participants(&self) -> Vec<OwnedUserId> {
+        self.inner.room_call_participants()
+    }
+
     pub fn inviter(&self) -> Option<Arc<RoomMember>> {
         if self.inner.state() == RoomState::Invited {
             RUNTIME.block_on(async move {
