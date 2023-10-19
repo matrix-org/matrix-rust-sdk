aff3170729d9a778d765ab8f732e901c0e5a4a99
Jonas Platte <jplatte@matrix.org>
2023-10-19 09:57:56 +0200

    widget: Add Deserialize impl for StateKeySelector

--- crates/matrix-sdk/src/widget/mod.rs
+++ crates/matrix-sdk/src/widget/mod.rs
@@ -14,7 +14,10 @@
 
 //! Widget API implementation.
 
+use std::fmt;
+
 use async_channel::{Receiver, Sender};
+use serde::de::{self, Deserialize, Deserializer, Visitor};
 use tokio::sync::mpsc::unbounded_channel;
 use tokio_util::sync::{CancellationToken, DropGuard};
 
@@ -227,9 +230,84 @@ impl WidgetDriver {
 }
 
 // TODO: Decide which module this type should live in
-#[allow(dead_code)]
 #[derive(Clone, Debug)]
 pub(crate) enum StateKeySelector {
     Key(String),
     Any,
+}
+
+impl<'de> Deserialize<'de> for StateKeySelector {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct StateKeySelectorVisitor;
+
+        impl<'de> Visitor<'de> for StateKeySelectorVisitor {
+            type Value = StateKeySelector;
+
+            fn expecting(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(f, "a string or `true`")
+            }
+
+            fn visit_bool<E>(self, v: bool) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                if v {
+                    Ok(StateKeySelector::Any)
+                } else {
+                    Err(E::invalid_value(de::Unexpected::Bool(v), &self))
+                }
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                self.visit_string(v.to_owned())
+            }
+
+            fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(StateKeySelector::Key(v))
+            }
+        }
+
+        deserializer.deserialize_any(StateKeySelectorVisitor)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_matches::assert_matches;
+    use serde_json::json;
+
+    use super::StateKeySelector;
+
+    #[test]
+    fn state_key_selector_from_true() {
+        let state_key = serde_json::from_value(json!(true)).unwrap();
+        assert_matches!(state_key, StateKeySelector::Any);
+    }
+
+    #[test]
+    fn state_key_selector_from_string() {
+        let state_key = serde_json::from_value(json!("test")).unwrap();
+        assert_matches!(state_key, StateKeySelector::Key(k) if k == "test");
+    }
+
+    #[test]
+    fn state_key_selector_from_false() {
+        let result = serde_json::from_value::<StateKeySelector>(json!(false));
+        assert_matches!(result, Err(e) if e.is_data());
+    }
+
+    #[test]
+    fn state_key_selector_from_number() {
+        let result = serde_json::from_value::<StateKeySelector>(json!(5));
+        assert_matches!(result, Err(e) if e.is_data());
+    }
 }
