4f70108632f50e1e1a6ad60ab8621cf7b1daf541
Benjamin Bouvier <public@benj.me>
2023-10-31 18:35:17 +0100

    fix: change the event_type after encrypting
    The event_type passed to `encrypt_room_event_raw` must be the one of the cleartext event, not `m.room.encrypted`. The returned event has the expected type.

--- crates/matrix-sdk/src/room/futures.rs
+++ crates/matrix-sdk/src/room/futures.rs
@@ -182,11 +182,11 @@ impl<'a> IntoFuture for SendRawMessageLikeEvent<'a> {
                     let olm = room.client.olm_machine().await;
                     let olm = olm.as_ref().expect("Olm machine wasn't started");
 
-                    event_type = "m.room.encrypted";
                     content = olm
                         .encrypt_room_event_raw(room.room_id(), event_type, &content)
                         .await?
                         .cast();
+                    event_type = "m.room.encrypted";
                 }
             } else {
                 tracing::Span::current().record("encrypted", false);
