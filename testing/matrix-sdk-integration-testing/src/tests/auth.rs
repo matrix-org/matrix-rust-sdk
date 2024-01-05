6fa807d69c5bb2b528c5c160b45e0fd9baffa76c
Benjamin Kampmann <ben@gnunicorn.org>
2024-01-05 11:25:52 +0000

    remove comment from test

--- testing/matrix-sdk-integration-testing/src/tests/auth.rs
+++ testing/matrix-sdk-integration-testing/src/tests/auth.rs
@@ -6,7 +6,6 @@ use crate::helpers::TestClientBuilder;
 async fn register_login_is_one_device() -> Result<()> {
     // registers a fresh client and makes sure we are logged in
     let client = TestClientBuilder::new("alice").randomize_username().use_sqlite().build().await?;
-    // should leave us with exactly one device logged in, right?!?
     let devices = client.devices().await?.devices;
     assert_eq!(devices.len(), 1);
     Ok(())
