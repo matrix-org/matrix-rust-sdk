fb4b5ea48fe94a1938f0c65b755674e1988811db
Mauro <34335419+Velin92@users.noreply.github.com>
2024-01-12 17:01:05 +0100

    ffi bindings: Expose `members_no_sync` (#3004)
    As it is useful to allow fetching members from the store in offline contexts.

--- bindings/matrix-sdk-ffi/src/room.rs
+++ bindings/matrix-sdk-ffi/src/room.rs
@@ -171,6 +171,12 @@ impl Room {
         Ok(Arc::new(RoomMembersIterator::new(self.inner.members(RoomMemberships::empty()).await?)))
     }
 
+    pub async fn members_no_sync(&self) -> Result<Arc<RoomMembersIterator>, ClientError> {
+        Ok(Arc::new(RoomMembersIterator::new(
+            self.inner.members_no_sync(RoomMemberships::empty()).await?,
+        )))
+    }
+
     pub async fn member(&self, user_id: String) -> Result<Arc<RoomMember>, ClientError> {
         let user_id = UserId::parse(&*user_id).context("Invalid user id.")?;
         let member = self.inner.get_member(&user_id).await?.context("No user found")?;
