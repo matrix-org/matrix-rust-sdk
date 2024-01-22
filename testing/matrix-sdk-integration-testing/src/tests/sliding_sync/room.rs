348af60853f659aa13339b07ad7862fcc2d62a19
Benjamin Bouvier <public@benj.me>
2024-01-22 12:16:31 +0100

    fixup! test: reenable the unread count test in code coverage

--- testing/matrix-sdk-integration-testing/src/tests/sliding_sync/room.rs
+++ testing/matrix-sdk-integration-testing/src/tests/sliding_sync/room.rs
@@ -431,7 +431,19 @@ async fn test_room_notification_count() -> Result<()> {
 
     bob_room.send(RoomMessageEventContent::text_plain("hello world")).await?;
 
-    assert!(info_updates.next().await.is_some());
+    for _ in 0..1 {
+        assert!(info_updates.next().await.is_some());
+        if !alice_room
+            .members_no_sync(RoomMemberships::JOIN)
+            .await?
+            .iter()
+            .any(|member| member.user_id() == alice.user_id().unwrap())
+        {
+            // The proxy may send another update that contains the membership information
+            // soonish.
+            continue;
+        }
+    }
 
     assert_eq!(alice_room.num_unread_messages(), 1);
     assert_eq!(alice_room.num_unread_notifications(), 1);
