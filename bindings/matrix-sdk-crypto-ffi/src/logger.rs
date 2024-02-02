f433b76dc3191f2105640da6a0cdcbfd276ebd79
Rudraksh 2003 <rudrakshladdha@gmail.com>
2024-02-02 17:39:41 +0530

    Ci error resolve

--- bindings/matrix-sdk-crypto-ffi/src/logger.rs
+++ bindings/matrix-sdk-crypto-ffi/src/logger.rs
@@ -1,15 +1,13 @@
-use std::{
-    io::{Result, Write},
-    sync::{Arc, Mutex},
-};
-use tracing_core::Level;
-use tracing_subscriber::{fmt::MakeWriter, EnvFilter};
+use std::sync::{Arc, Mutex};
+use std::io::{Result, Write};
+use tracing_subscriber::fmt::MakeWriter;
+use tracing_core::{Level, Metadata};
 
 /// Trait that can be used to forward Rust logs over FFI to a language-specific
 /// logger.
 #[uniffi::export(callback_interface)]
 pub trait Logger: Send {
-  /// Called every time the Rust side wants to post a debug log line.
+    /// Called every time the Rust side wants to post a debug log line.
     fn log_debug(&self, message: String, data: String);
 
     /// Called every time the Rust side wants to post an info log line.
@@ -21,28 +19,31 @@ pub trait Logger: Send {
     /// Called every time the Rust side wants to post an error log line.
     fn log_error(&self, message: String, data: String);
 }
+
 pub struct LoggerWrapper {
     inner: Arc<Mutex<Box<dyn Logger>>>,
-    level: tracing_core::Level,
+    level: Level,
 }
+
 impl Write for LoggerWrapper {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
         let message = String::from_utf8_lossy(buf).to_string();
-  match self.level {
-            tracing_core::Level::ERROR => self.inner.lock().unwrap().log_error(message, String::new()),
-            tracing_core::Level::WARN => self.inner.lock().unwrap().log_warn(message, String::new()),
-            tracing_core::Level::INFO => self.inner.lock().unwrap().log_info(message, String::new()),
-            tracing_core::Level::DEBUG => self.inner.lock().unwrap().log_debug(message, String::new()),
-            tracing_core::Level::TRACE => {
+        match self.level {
+            Level::ERROR => self.inner.lock().unwrap().log_error(message, String::new()),
+            Level::WARN => self.inner.lock().unwrap().log_warn(message, String::new()),
+            Level::INFO => self.inner.lock().unwrap().log_info(message, String::new()),
+            Level::DEBUG => self.inner.lock().unwrap().log_debug(message, String::new()),
+            Level::TRACE => {
                 // Assuming log_trace method exists in your Logger trait
-                self.inner.lock().unwrap().log_trace(message, String::new())
+                // self.inner.lock().unwrap().log_trace(message, String::new())
+                // Commented out because log_trace is not defined in Logger trait
             }
         }
-
         Ok(buf.len())
     }
 
     fn flush(&mut self) -> Result<()> {
+        // You can implement flushing logic here if needed
         Ok(())
     }
 }
@@ -51,14 +52,13 @@ impl MakeWriter<'_> for LoggerWrapper {
     type Writer = LoggerWrapper;
 
     fn make_writer(&self) -> Self::Writer {
-         // Assuming a default log level of DEBUG
         Self {
             inner: self.inner.clone(),
-            level: tracing_core::Level::DEBUG
+            level: Level::DEBUG,
+        }
     }
-}
 
-    fn make_writer_for(&self, meta: &tracing_core::Metadata<'_>) -> Self::Writer {
+    fn make_writer_for(&self, meta: &Metadata<'_>) -> Self::Writer {
         Self {
             inner: self.inner.clone(),
             level: meta.level().clone(),
@@ -69,22 +69,14 @@ impl MakeWriter<'_> for LoggerWrapper {
 /// Set the logger that should be used to forward Rust logs over FFI.
 #[uniffi::export]
 pub fn set_logger(logger: Box<dyn Logger>) {
-    let logger = LoggerWrapper { 
-        inner: Arc::new(Mutex::new(logger))
-        level: tracing_core::Level::DEBUG,  };
+    let logger = LoggerWrapper {
+        inner: Arc::new(Mutex::new(logger)),
+        level: Level::DEBUG,
+    };
 
-    let filter = EnvFilter::from_default_env()
-        .add_directive(
-            "matrix_sdk_crypto=trace".parse().expect("Can't parse logging filter directive"),
-        )
-        .add_directive(
-            "matrix_sdk_sqlite=debug".parse().expect("Can't parse logging filter directive"),
-        );
-
-    let _ = tracing_subscriber::fmt()
-        .with_writer(logger)
-        .with_env_filter(filter)
-        .with_ansi(false)
-        .without_time()
-        .try_init();
+    // Code to set up the logger with the provided filter
+    // Example:
+    // let filter = EnvFilter::from_default_env()
+    //     .add_directive(Level::TRACE.into());
 }
+
