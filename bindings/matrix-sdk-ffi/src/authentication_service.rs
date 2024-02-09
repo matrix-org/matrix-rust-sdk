c20e6aeca7d93bdf41b6f890bb5d2d7d1a8f2b59
Benjamin Bouvier <public@benj.me>
2024-02-09 17:47:27 +0100

    ffi: configure encryption settings only when it's needed
    It's wrong that the first client, used only to determine how to log in and find the user id, try to run the encryption initialization tasks.
    In particular, it should not even try to bootstrap the account, as this may send OTKs to the server, which the client will forget about as soon
    as it's respawned as a database-backed client.

--- bindings/matrix-sdk-ffi/src/authentication_service.rs
+++ bindings/matrix-sdk-ffi/src/authentication_service.rs
@@ -591,6 +591,12 @@ impl AuthenticationService {
             .passphrase(self.passphrase.clone())
             .homeserver_url(homeserver_url)
             .sliding_sync_proxy(sliding_sync_proxy)
+            .with_encryption_settings(matrix_sdk::encryption::EncryptionSettings {
+                auto_enable_cross_signing: true,
+                backup_download_strategy:
+                    matrix_sdk::encryption::BackupDownloadStrategy::AfterDecryptionFailure,
+                auto_enable_backups: true,
+            })
             .username(user_id.to_string());
 
         if let Some(id) = &self.cross_process_refresh_lock_id {
