11074d8f4dabd9294e49e6ad8562a970eca1df46
Benjamin Bouvier <public@benj.me>
2024-02-12 18:52:19 +0100

    event cache: listen to all the room updates at once!

--- crates/matrix-sdk-ui/src/timeline/mod.rs
+++ crates/matrix-sdk-ui/src/timeline/mod.rs
@@ -59,7 +59,7 @@ use tokio::sync::{mpsc::Sender, Mutex, Notify};
 use tracing::{debug, error, info, instrument, trace, warn};
 
 use self::futures::SendAttachment;
-use crate::event_cache::RoomEventCache;
+use crate::event_cache::EventCacheDropHandles;
 
 mod builder;
 mod error;
@@ -816,7 +816,7 @@ struct TimelineDropHandle {
     room_update_join_handle: JoinHandle<()>,
     ignore_user_list_update_join_handle: JoinHandle<()>,
     room_key_from_backups_join_handle: JoinHandle<()>,
-    _event_cache: RoomEventCache,
+    _event_cache_drop_handle: Arc<EventCacheDropHandles>,
 }
 
 impl Drop for TimelineDropHandle {
