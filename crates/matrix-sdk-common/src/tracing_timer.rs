5f9a90ff3bc442b3b9cea0959eb6ea4f90b5d9e5
maan2003 <manmeetmann2003@gmail.com>
2024-02-13 10:06:15 +0530

    fix(wasm): don't use std::time::Instant

--- crates/matrix-sdk-common/src/tracing_timer.rs
+++ crates/matrix-sdk-common/src/tracing_timer.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::time::Instant;
+use crate::instant::Instant;
 
 use tracing::{callsite::DefaultCallsite, Callsite as _};
 
