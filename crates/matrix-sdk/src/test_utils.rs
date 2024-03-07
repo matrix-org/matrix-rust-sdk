89ce74920f78c3b689b15196592f0b1ee412ce14
Jorge Martín <jorgem@element.io>
2024-03-07 17:48:08 +0100

    sdk: remove unused import

--- crates/matrix-sdk/src/test_utils.rs
+++ crates/matrix-sdk/src/test_utils.rs
@@ -7,7 +7,7 @@ use ruma::{api::MatrixVersion, device_id, user_id};
 use url::Url;
 
 use crate::{
-    config::{RequestConfig, SyncSettings},
+    config::RequestConfig,
     matrix_auth::{MatrixSession, MatrixSessionTokens},
     Client, ClientBuilder,
 };
