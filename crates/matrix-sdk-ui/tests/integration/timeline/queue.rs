2c9a088a36ed0f686f4bdb394b16a1b7ac6dbcf7
Benjamin Bouvier <public@benj.me>
2024-03-21 19:07:34 +0100

    day divider: add test for duplicate date header after matching two local echoes
    I checked that the test failed on main, by causing a final state of [DD First DD Second].
    
    Fixes #2590.

--- crates/matrix-sdk-ui/tests/integration/timeline/queue.rs
+++ crates/matrix-sdk-ui/tests/integration/timeline/queue.rs
@@ -21,7 +21,9 @@ use futures_util::StreamExt;
 use matrix_sdk::{config::SyncSettings, test_utils::logged_in_client_with_server};
 use matrix_sdk_test::{async_test, EventBuilder, JoinedRoomBuilder, SyncResponseBuilder, ALICE};
 use matrix_sdk_ui::timeline::{EventItemOrigin, EventSendState, RoomExt};
-use ruma::{events::room::message::RoomMessageEventContent, room_id};
+use ruma::{
+    event_id, events::room::message::RoomMessageEventContent, room_id, MilliSecondsSinceUnixEpoch,
+};
 use serde_json::json;
 use stream_assert::{assert_next_matches, assert_pending};
 use tokio::time::sleep;
@@ -38,10 +40,10 @@ async fn test_message_order() {
     let (client, server) = logged_in_client_with_server().await;
     let sync_settings = SyncSettings::new().timeout(Duration::from_millis(3000));
 
-    let mut ev_builder = SyncResponseBuilder::new();
-    ev_builder.add_joined_room(JoinedRoomBuilder::new(room_id));
+    let mut sync_response_builder = SyncResponseBuilder::new();
+    sync_response_builder.add_joined_room(JoinedRoomBuilder::new(room_id));
 
-    mock_sync(&server, ev_builder.build_json_sync_response(), None).await;
+    mock_sync(&server, sync_response_builder.build_json_sync_response(), None).await;
     let _response = client.sync_once(sync_settings.clone()).await.unwrap();
     server.reset().await;
 
@@ -110,10 +112,10 @@ async fn test_retry_order() {
     let (client, server) = logged_in_client_with_server().await;
     let sync_settings = SyncSettings::new().timeout(Duration::from_millis(3000));
 
-    let mut ev_builder = SyncResponseBuilder::new();
-    ev_builder.add_joined_room(JoinedRoomBuilder::new(room_id));
+    let mut sync_response_builder = SyncResponseBuilder::new();
+    sync_response_builder.add_joined_room(JoinedRoomBuilder::new(room_id));
 
-    mock_sync(&server, ev_builder.build_json_sync_response(), None).await;
+    mock_sync(&server, sync_response_builder.build_json_sync_response(), None).await;
     let _response = client.sync_once(sync_settings.clone()).await.unwrap();
     server.reset().await;
 
@@ -288,4 +290,139 @@ async fn test_clear_with_echoes() {
     assert_eq!(event_items.len(), 2);
     assert_matches!(event_items[0].send_state(), Some(EventSendState::SendingFailed { .. }));
     assert_matches!(event_items[1].send_state(), Some(EventSendState::NotSentYet));
+}
+
+#[async_test]
+async fn test_no_duplicate_day_divider() {
+    let room_id = room_id!("!a98sd12bjh:example.org");
+    let (client, server) = logged_in_client_with_server().await;
+    let sync_settings = SyncSettings::new().timeout(Duration::from_millis(3000));
+
+    let mut sync_response_builder = SyncResponseBuilder::new();
+    sync_response_builder.add_joined_room(JoinedRoomBuilder::new(room_id));
+
+    mock_sync(&server, sync_response_builder.build_json_sync_response(), None).await;
+    let _response = client.sync_once(sync_settings.clone()).await.unwrap();
+    server.reset().await;
+
+    mock_encryption_state(&server, false).await;
+
+    let room = client.get_room(room_id).unwrap();
+    let timeline = Arc::new(room.timeline().await.unwrap());
+    let (_, mut timeline_stream) = timeline.subscribe().await;
+
+    // Response for first message takes 200ms to respond.
+    Mock::given(method("PUT"))
+        .and(path_regex(r"^/_matrix/client/r0/rooms/.*/send/.*"))
+        .and(body_string_contains("First!"))
+        .respond_with(
+            ResponseTemplate::new(200)
+                .set_body_json(&json!({
+                    "event_id": "$PyHxV5mYzjetBUT3qZq7V95GOzxb02EP",
+                }))
+                .set_delay(Duration::from_millis(200)),
+        )
+        .mount(&server)
+        .await;
+
+    // Response for second message only takes 100ms to respond, so should come
+    // back first if we don't serialize requests.
+    Mock::given(method("PUT"))
+        .and(path_regex(r"^/_matrix/client/r0/rooms/.*/send/.*"))
+        .and(body_string_contains("Second."))
+        .respond_with(
+            ResponseTemplate::new(200)
+                .set_body_json(&json!({
+                    "event_id": "$5E2kLK/Sg342bgBU9ceEIEPYpbFaqJpZ",
+                }))
+                .set_delay(Duration::from_millis(100)),
+        )
+        .mount(&server)
+        .await;
+
+    timeline.send(RoomMessageEventContent::text_plain("First!").into()).await;
+    timeline.send(RoomMessageEventContent::text_plain("Second.").into()).await;
+
+    // Local echoes are available as soon as `timeline.send` returns.
+    assert_next_matches!(timeline_stream, VectorDiff::PushBack { value } => {
+        assert_eq!(value.as_event().unwrap().content().as_message().unwrap().body(), "First!");
+    });
+
+    assert_next_matches!(timeline_stream, VectorDiff::PushFront { value } => {
+        assert!(value.is_day_divider());
+    });
+
+    assert_next_matches!(timeline_stream, VectorDiff::PushBack { value } => {
+        assert_eq!(value.as_event().unwrap().content().as_message().unwrap().body(), "Second.");
+    });
+
+    // Wait 200ms for the first msg, 100ms for the second, 200ms for overhead.
+    sleep(Duration::from_millis(500)).await;
+
+    // The first item should be updated first.
+    assert_next_matches!(timeline_stream, VectorDiff::Set { index: 1, value } => {
+        let value = value.as_event().unwrap();
+        assert_eq!(value.content().as_message().unwrap().body(), "First!");
+        assert_eq!(value.event_id().unwrap(), "$PyHxV5mYzjetBUT3qZq7V95GOzxb02EP");
+    });
+
+    // Then the second one.
+    assert_next_matches!(timeline_stream, VectorDiff::Set { index: 2, value } => {
+        let value = value.as_event().unwrap();
+        assert_eq!(value.content().as_message().unwrap().body(), "Second.");
+        assert_eq!(value.event_id().unwrap(), "$5E2kLK/Sg342bgBU9ceEIEPYpbFaqJpZ");
+    });
+
+    assert_pending!(timeline_stream);
+
+    // Now have the sync return both events with more data.
+    let ev_builder = EventBuilder::new();
+
+    let now = MilliSecondsSinceUnixEpoch::now();
+    ev_builder.set_next_ts(now.0.into());
+
+    sync_response_builder.add_joined_room(
+        JoinedRoomBuilder::new(room_id)
+            .add_timeline_event(ev_builder.make_sync_message_event_with_id(
+                client.user_id().unwrap(),
+                event_id!("$PyHxV5mYzjetBUT3qZq7V95GOzxb02EP"),
+                RoomMessageEventContent::text_plain("First!"),
+            ))
+            .add_timeline_event(ev_builder.make_sync_message_event_with_id(
+                client.user_id().unwrap(),
+                event_id!("$5E2kLK/Sg342bgBU9ceEIEPYpbFaqJpZ"),
+                RoomMessageEventContent::text_plain("Second."),
+            )),
+    );
+
+    mock_sync(&server, sync_response_builder.build_json_sync_response(), None).await;
+    let _response = client.sync_once(sync_settings.clone()).await.unwrap();
+    server.reset().await;
+
+    // The first message is removed -> [DD Second]
+    assert_next_matches!(timeline_stream, VectorDiff::Remove { index: 1 });
+
+    // The first message is reinserted -> [First DD Second]
+    assert_next_matches!(timeline_stream, VectorDiff::PushFront { value } => {
+        let value = value.as_event().unwrap();
+        assert_eq!(value.content().as_message().unwrap().body(), "First!");
+        assert_eq!(value.event_id().unwrap(), "$PyHxV5mYzjetBUT3qZq7V95GOzxb02EP");
+    });
+
+    // The second message is replaced -> [First DD Second]
+    assert_next_matches!(timeline_stream, VectorDiff::Set { index: 2, value } => {
+        let value = value.as_event().unwrap();
+        assert_eq!(value.content().as_message().unwrap().body(), "Second.");
+        assert_eq!(value.event_id().unwrap(), "$5E2kLK/Sg342bgBU9ceEIEPYpbFaqJpZ");
+    });
+
+    // A new day divider is inserted -> [DD First DD Second]
+    assert_next_matches!(timeline_stream, VectorDiff::PushFront { value } => {
+        assert!(value.is_day_divider());
+    });
+
+    // The useless day divider is removed. -> [DD First Second]
+    assert_next_matches!(timeline_stream, VectorDiff::Remove { index: 2 });
+
+    assert_pending!(timeline_stream);
 }
