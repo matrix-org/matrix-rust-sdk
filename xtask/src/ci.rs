adff571c6840657328c17b0a9d8ac84cf57468b2
Benjamin Bouvier <public@benj.me>
2024-06-03 17:23:25 +0200

    xtask: force --target-applies-to-host when running Clippy check
    Without this, the configs from `.cargo/config.toml` were not read in CI
    tasks, causing false positives when running Clippy on CI (i.e. there
    were issues observed when compiling locally that were not found when
    compiling remotely).
    
    Not entirely sure why it's needed, because I'm seeing the issues when
    I'm using `cargo xtask ci clippy` locally, with nothing changed.

--- xtask/src/ci.rs
+++ xtask/src/ci.rs
@@ -179,7 +179,10 @@ fn check_typos() -> Result<()> {
 
 fn check_clippy() -> Result<()> {
     cmd!("rustup run {NIGHTLY} cargo clippy --all-targets --features testing -- -D warnings")
+        // Work around https://github.com/rust-lang/cargo/issues/10744
+        .env("CARGO_TARGET_APPLIES_TO_HOST", "true")
         .run()?;
+
     cmd!(
         "rustup run {NIGHTLY} cargo clippy --workspace --all-targets
             --exclude matrix-sdk-crypto --exclude xtask
@@ -187,12 +190,16 @@ fn check_clippy() -> Result<()> {
             --features native-tls,experimental-sliding-sync,sso-login,testing
             -- -D warnings"
     )
+    .env("CARGO_TARGET_APPLIES_TO_HOST", "true")
     .run()?;
+
     cmd!(
         "rustup run {NIGHTLY} cargo clippy --all-targets -p matrix-sdk-crypto
             --no-default-features -- -D warnings"
     )
+    .env("CARGO_TARGET_APPLIES_TO_HOST", "true")
     .run()?;
+
     Ok(())
 }
 
