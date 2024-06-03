96824055f854963e3a2eb708f347564339424043
Benjamin Bouvier <public@benj.me>
2024-06-03 12:20:06 +0200

    ffi: add new method to retrieve a timeline item by transaction id

--- bindings/matrix-sdk-ffi/src/timeline/mod.rs
+++ bindings/matrix-sdk-ffi/src/timeline/mod.rs
@@ -45,7 +45,7 @@ use ruma::{
         },
         AnyMessageLikeEventContent,
     },
-    EventId,
+    EventId, OwnedTransactionId,
 };
 use tokio::{
     sync::Mutex,
@@ -530,6 +530,14 @@ impl Timeline {
         Ok(())
     }
 
+    /// Get the current timeline item for the given event ID, if any.
+    ///
+    /// Will return a remote event, *or* a local echo that has been sent but not
+    /// yet replaced by a remote echo.
+    ///
+    /// It's preferable to store the timeline items in the model for your UI, if
+    /// possible, instead of just storing IDs and coming back to the timeline
+    /// object to look up items.
     pub async fn get_event_timeline_item_by_event_id(
         &self,
         event_id: String,
@@ -540,6 +548,26 @@ impl Timeline {
             .item_by_event_id(&event_id)
             .await
             .context("Item with given event ID not found")?;
+        Ok(Arc::new(EventTimelineItem(item)))
+    }
+
+    /// Get the current timeline item for the given transaction ID, if any.
+    ///
+    /// This will always return a local echo, if found.
+    ///
+    /// It's preferable to store the timeline items in the model for your UI, if
+    /// possible, instead of just storing IDs and coming back to the timeline
+    /// object to look up items.
+    pub async fn get_event_timeline_item_by_transaction_id(
+        &self,
+        transaction_id: String,
+    ) -> Result<Arc<EventTimelineItem>, ClientError> {
+        let transaction_id: OwnedTransactionId = transaction_id.into();
+        let item = self
+            .inner
+            .item_by_transaction_id(&transaction_id)
+            .await
+            .context("Item with given transaction ID not found")?;
         Ok(Arc::new(EventTimelineItem(item)))
     }
 
