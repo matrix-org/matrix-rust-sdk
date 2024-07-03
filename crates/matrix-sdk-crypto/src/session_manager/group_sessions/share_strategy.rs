78036173beb84ca9bfbd6ef6eeae335e6aa25ca5
Valere <bill.carson@valrsoft.com>
2024-07-03 11:24:11 +0200

    crypto: refactor extract detection of removed device from outbound

--- crates/matrix-sdk-crypto/src/session_manager/group_sessions/share_strategy.rs
+++ crates/matrix-sdk-crypto/src/session_manager/group_sessions/share_strategy.rs
@@ -162,33 +162,7 @@ pub(crate) async fn collect_session_recipients(
         // of the devices in the session got deleted or blacklisted in the
         // meantime. If so, we should also rotate the session.
         if !should_rotate {
-            // Device IDs that should receive this session
-            let recipient_device_ids: BTreeSet<&DeviceId> =
-                recipients.iter().map(|d| d.device_id()).collect();
-
-            if let Some(shared) = outbound.shared_with_set.read().unwrap().get(user_id) {
-                // Devices that received this session
-                let shared: BTreeSet<OwnedDeviceId> = shared.keys().cloned().collect();
-                let shared: BTreeSet<&DeviceId> = shared.iter().map(|d| d.as_ref()).collect();
-
-                // The set difference between
-                //
-                // 1. Devices that had previously received the session, and
-                // 2. Devices that would now receive the session
-                //
-                // Represents newly deleted or blacklisted devices. If this
-                // set is non-empty, we must rotate.
-                let newly_deleted_or_blacklisted =
-                    shared.difference(&recipient_device_ids).collect::<BTreeSet<_>>();
-
-                should_rotate = !newly_deleted_or_blacklisted.is_empty();
-                if should_rotate {
-                    debug!(
-                        "Rotating a room key due to these devices being deleted/blacklisted {:?}",
-                        newly_deleted_or_blacklisted,
-                    );
-                }
-            };
+            should_rotate = should_rotate_due_to_left_device(user_id, &recipients, outbound);
         }
 
         devices.entry(user_id.to_owned()).or_default().extend(recipients);
@@ -207,6 +181,45 @@ pub(crate) async fn collect_session_recipients(
     trace!(should_rotate, "Done calculating group session recipients");
 
     Ok(CollectRecipientsResult { should_rotate, devices, withheld_devices })
+}
+
+// Check whether any of the devices currently in the discussion (outbound
+// session stores the devices it already got shared with) got deleted or
+// blacklisted in the meantime. If it's the case the session should be rotated.
+fn should_rotate_due_to_left_device(
+    user_id: &UserId,
+    future_recipients: &Vec<ReadOnlyDevice>,
+    outbound: &OutboundGroupSession,
+) -> bool {
+    // Device IDs that should receive this session
+    let recipient_device_ids: BTreeSet<&DeviceId> =
+        future_recipients.iter().map(|d| d.device_id()).collect();
+
+    if let Some(shared) = outbound.shared_with_set.read().unwrap().get(user_id) {
+        // Devices that received this session
+        let shared: BTreeSet<OwnedDeviceId> = shared.keys().cloned().collect();
+        let shared: BTreeSet<&DeviceId> = shared.iter().map(|d| d.as_ref()).collect();
+
+        // The set difference between
+        //
+        // 1. Devices that had previously received the session, and
+        // 2. Devices that would now receive the session
+        //
+        // Represents newly deleted or blacklisted devices. If this
+        // set is non-empty, we must rotate.
+        let newly_deleted_or_blacklisted =
+            shared.difference(&recipient_device_ids).collect::<BTreeSet<_>>();
+
+        let should_rotate = !newly_deleted_or_blacklisted.is_empty();
+        if should_rotate {
+            debug!(
+                "Rotating a room key due to these devices being deleted/blacklisted {:?}",
+                newly_deleted_or_blacklisted,
+            );
+        }
+        return should_rotate;
+    };
+    false
 }
 
 struct RecipientDevices {
