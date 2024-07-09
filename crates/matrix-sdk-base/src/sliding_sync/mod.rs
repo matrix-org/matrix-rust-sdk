5a4bf780fbab9ba0e08f2f2c6918c6891f42f745
Ivan Enderlin <ivan@mnt.io>
2024-07-09 10:39:05 +0200

    feat(base): Create an `http` module for `sliding_sync`.
    This patch creates an `http` module containing all the sliding sync types
    types (from Simplified MSC3575 or simply MSC3575).

--- crates/matrix-sdk-base/src/sliding_sync/mod.rs
+++ crates/matrix-sdk-base/src/sliding_sync/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod http;
+
 use std::collections::BTreeMap;
 #[cfg(feature = "e2e-encryption")]
 use std::ops::Deref;
