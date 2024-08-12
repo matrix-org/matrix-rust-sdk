1c8e77119b58619fc84d24f2fec0bdb88881cb0a
Benjamin Bouvier <benjamin@bouvier.cc>
2024-08-12 15:32:32 +0200

    event cache(refactor): use a map keyed by event id for retrieving events
    And add unit tests.

--- crates/matrix-sdk/src/room/mod.rs
+++ crates/matrix-sdk/src/room/mod.rs
@@ -398,8 +398,16 @@ impl Room {
     ) -> Result<TimelineEvent> {
         let request =
             get_room_event::v3::Request::new(self.room_id().to_owned(), event_id.to_owned());
-        let event = self.client.send(request, request_config).await?.event;
-        self.try_decrypt_event(event).await
+
+        let raw_event = self.client.send(request, request_config).await?.event;
+        let event = self.try_decrypt_event(raw_event).await?;
+
+        // Save the event into the event cache, if it's set up.
+        if let Ok((cache, _handles)) = self.event_cache().await {
+            cache.save_event(event.clone().into()).await;
+        }
+
+        Ok(event)
     }
 
     /// Fetch the event with the given `EventId` in this room, using the
@@ -2357,12 +2365,6 @@ impl Room {
     /// The call may fail if there is an error in getting the power levels.
     pub async fn can_user_trigger_room_notification(&self, user_id: &UserId) -> Result<bool> {
         Ok(self.room_power_levels().await?.user_can_trigger_room_notification(user_id))
-    }
-
-    /// Removes the cached pinned events associated with this room.
-    pub async fn clear_pinned_events(&self) {
-        let pinned_event_ids = self.pinned_event_ids();
-        self.client.inner.pinned_event_cache.remove_bulk(&pinned_event_ids).await;
     }
 
     /// Get a list of servers that should know this room.
