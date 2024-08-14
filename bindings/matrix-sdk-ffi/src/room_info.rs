d7aea23f8e5fe7ec0ef01977179716e046ded0ea
Ivan Enderlin <ivan@mnt.io>
2024-08-14 11:05:31 +0200

    feat(ffi): Add `RoomInfo::cached_user_defined_notification_mode`.
    This patch replaces `RoomInfo::user_defined_notification_mode` by
    its cached variant: `cached_user_defined_notification_mode`, and
    call `Room::cached_user_defined_notification_mode` which will boost
    performance when computing a new `RoomInfo`.

--- bindings/matrix-sdk-ffi/src/room_info.rs
+++ bindings/matrix-sdk-ffi/src/room_info.rs
@@ -39,7 +39,7 @@ pub struct RoomInfo {
     user_power_levels: HashMap<String, i64>,
     highlight_count: u64,
     notification_count: u64,
-    user_defined_notification_mode: Option<RoomNotificationMode>,
+    cached_user_defined_notification_mode: Option<RoomNotificationMode>,
     has_room_call: bool,
     active_room_call_participants: Vec<String>,
     /// Whether this room has been explicitly marked as unread
@@ -98,9 +98,8 @@ impl RoomInfo {
             user_power_levels,
             highlight_count: unread_notification_counts.highlight_count,
             notification_count: unread_notification_counts.notification_count,
-            user_defined_notification_mode: room
-                .user_defined_notification_mode()
-                .await
+            cached_user_defined_notification_mode: room
+                .cached_user_defined_notification_mode()
                 .map(Into::into),
             has_room_call: room.has_active_room_call(),
             active_room_call_participants: room
