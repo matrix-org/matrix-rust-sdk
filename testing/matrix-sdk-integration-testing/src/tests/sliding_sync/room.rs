84e4552da71b2c1eaec3e1e6259bbe01d3214526
Kévin Commaille <zecakeh@tedomum.fr>
2024-08-31 14:33:45 +0200

    Revert "feat(sdk): Remove `NotificationSettings::subscribe_to_changes`."
    This reverts commit 4e291205d5fa944d9cd53b4eab48e238bfe2a3e8.

--- testing/matrix-sdk-integration-testing/src/tests/sliding_sync/room.rs
+++ testing/matrix-sdk-integration-testing/src/tests/sliding_sync/room.rs
@@ -581,6 +581,8 @@ async fn test_room_notification_count() -> Result<()> {
     // Now Alice is only interesting in mentions of their name.
     let settings = alice.notification_settings().await;
 
+    let mut settings_changes = settings.subscribe_to_changes();
+
     tracing::warn!("Updating room notification mode to mentions and keywords only...");
     settings
         .set_room_notification_mode(
@@ -591,7 +593,7 @@ async fn test_room_notification_count() -> Result<()> {
     tracing::warn!("Done!");
 
     // Wait for remote echo.
-    tokio::time::sleep(Duration::from_secs(3))
+    timeout(Duration::from_secs(3), settings_changes.recv())
         .await
         .expect("timeout when waiting for settings update")
         .expect("should've received echo after updating settings");
