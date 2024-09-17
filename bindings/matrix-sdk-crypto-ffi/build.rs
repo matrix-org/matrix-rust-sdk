decdd6f47ee2aceb7086a83e0bd35b66a9d854cc
Jorge Martín <jorgem@element.io>
2024-09-17 16:07:17 +0200

    crypto-ffi: update the x86-64 Android workaround to match `matrix-sdk-ffi`
    This workaround was applied to `matrix-sdk-ffi` and it should be used here too

--- bindings/matrix-sdk-crypto-ffi/build.rs
+++ bindings/matrix-sdk-crypto-ffi/build.rs
@@ -5,6 +5,9 @@ use vergen::EmitBuilder;
 /// Adds a temporary workaround for an issue with the Rust compiler and Android
 /// in x86_64 devices: https://github.com/rust-lang/rust/issues/109717.
 /// The workaround comes from: https://github.com/mozilla/application-services/pull/5442
+///
+/// IMPORTANT: if you modify this, make sure to modify
+/// [../matrix-sdk-ffi/build.rs] too!
 fn setup_x86_64_android_workaround() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set");
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH not set");
@@ -18,11 +21,11 @@ fn setup_x86_64_android_workaround() {
                 "Unsupported OS. You must use either Linux, MacOS or Windows to build the crate."
             ),
         };
-        const DEFAULT_CLANG_VERSION: &str = "14.0.7";
+        const DEFAULT_CLANG_VERSION: &str = "18";
         let clang_version =
             env::var("NDK_CLANG_VERSION").unwrap_or_else(|_| DEFAULT_CLANG_VERSION.to_owned());
         let linux_x86_64_lib_dir = format!(
-            "toolchains/llvm/prebuilt/{build_os}-x86_64/lib64/clang/{clang_version}/lib/linux/"
+            "toolchains/llvm/prebuilt/{build_os}-x86_64/lib/clang/{clang_version}/lib/linux/"
         );
         println!("cargo:rustc-link-search={android_ndk_home}/{linux_x86_64_lib_dir}");
         println!("cargo:rustc-link-lib=static=clang_rt.builtins-x86_64-android");
