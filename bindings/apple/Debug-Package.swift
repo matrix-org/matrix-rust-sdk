38a15afc9c9e26df57eb4815810cb16bfb819d00
Mauro Romito <mauro.romito@element.io>
2024-11-22 17:17:43 +0100

    build (apple): add dynamic type to debug package

--- bindings/apple/Debug-Package.swift
+++ bindings/apple/Debug-Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     ],
     products: [
         .library(name: "MatrixRustSDK",
+                 type: .dynamic,
                  targets: ["MatrixRustSDK"]),
     ],
     targets: [
