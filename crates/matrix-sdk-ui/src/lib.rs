ba88edb100de7dce5ee6dbc9cfae1c019093aa68
Damir Jelić <poljar@termina.org.uk>
2024-11-27 12:17:23 +0100

    chore: Silence unexpected cfg warnings
    These are all coming from macro invocations of macros that are defined
    in other crates. It's likely a clippy issue. We should try to revert
    this the next time we bump the nightly version we're using.

--- crates/matrix-sdk-ui/src/lib.rs
+++ crates/matrix-sdk-ui/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![cfg_attr(test, allow(unexpected_cfgs))] // Triggered by the init_tracing_for_tests!() invocation.
+
 use ruma::html::HtmlSanitizerMode;
 
 mod events;
