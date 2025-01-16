88a7137ceb61ad35373409ecb53885ad2182d661
Jonas Platte <jplatte@matrix.org>
2025-01-16 23:57:52 +0100

    chore(ui): Copy some attributes from matrix-sdk
    The others should likely be copied at some point as well, but including
    the readme as crate documentation is not currently useful since the
    readme for the ui crate is empty, and warning about missing docs or
    debug implementations would make CI fail without substantial extra work.

--- crates/matrix-sdk-ui/src/lib.rs
+++ crates/matrix-sdk-ui/src/lib.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![cfg_attr(target_arch = "wasm32", allow(clippy::arc_with_non_send_sync))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
 use ruma::html::HtmlSanitizerMode;
 
 mod events;
