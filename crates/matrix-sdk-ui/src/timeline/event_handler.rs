matrix-org/matrix-rust-sdk

commit f0d98602a9e65cf26a607cf10a0715b92da12d4a

timeline: use the `EncryptionInfo` provided by the replacement event when processing edits - this prevents issues where spoofing the sender field is enough to spoof and edit and display wrong decorations in the app - fixes matrix-org/internal-config/issues/1549

crates/matrix-sdk-ui/src/timeline/event_handler.rs
@@ -543,7 +543,21 @@ impl<'a, 'o> TimelineEventHandler<'a, 'o> {
             };
 
             trace!("Applying edit");
-            Some(event_item.with_content(new_content, edit_json))
+
+            if let EventTimelineItemKind::Remote(remote_event) = &event_item.kind {
+                let new_encryption_info = match &this.ctx.flow {
+                    Flow::Local { .. } => None,
+                    Flow::Remote { encryption_info, .. } => encryption_info.clone(),
+                };
+
+                Some(event_item.with_content(new_content, edit_json).with_kind(
+                    EventTimelineItemKind::Remote(
+                        remote_event.with_encryption_info(new_encryption_info),
+                    ),
+                ))
+            } else {
+                Some(event_item.with_content(new_content, edit_json))
+            }
         });
 
         if !found {

crates/matrix-sdk-ui/src/timeline/event_item/remote.rs
@@ -73,6 +73,11 @@ pub(in crate::timeline) struct RemoteEventTimelineItem {
 }
 
 impl RemoteEventTimelineItem {
+    /// Clone the current event item, and update its `encryption_info`.
+    pub fn with_encryption_info(&self, encryption_info: Option<EncryptionInfo>) -> Self {
+        Self { encryption_info, ..self.clone() }
+    }
+
     /// Clone the current event item, and update its `reactions`.
     pub fn with_reactions(&self, reactions: ReactionsByKeyBySender) -> Self {
         Self { reactions, ..self.clone() }

crates/matrix-sdk-ui/src/timeline/tests/edit.rs
@@ -12,10 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::BTreeMap;
+
 use assert_matches2::assert_let;
 use eyeball_im::VectorDiff;
+use matrix_sdk::deserialized_responses::{
+    AlgorithmInfo, EncryptionInfo, VerificationLevel, VerificationState,
+};
 use matrix_sdk_test::{async_test, sync_timeline_event, ALICE};
 use ruma::{
+    event_id,
     events::room::message::{MessageType, RedactedRoomMessageEventContent},
     server_name, EventId,
 };
@@ -152,4 +158,67 @@ async fn test_aggregated_sanitized() {
 
     let day_divider = assert_next_matches!(stream, VectorDiff::PushFront { value } => value);
     assert!(day_divider.is_day_divider());
+}
+
+#[async_test]
+async fn test_edit_updates_encryption_info() {
+    let timeline = TestTimeline::new();
+    let event_factory = &timeline.factory;
+
+    let original_event_id = event_id!("$original_event");
+
+    let mut original_event = event_factory
+        .text_msg("**original** message")
+        .sender(*ALICE)
+        .event_id(original_event_id)
+        .into_sync();
+
+    let mut encryption_info = EncryptionInfo {
+        sender: (*ALICE).into(),
+        sender_device: None,
+        algorithm_info: AlgorithmInfo::MegolmV1AesSha2 {
+            curve25519_key: "123".to_owned(),
+            sender_claimed_keys: BTreeMap::new(),
+        },
+        verification_state: VerificationState::Verified,
+    };
+
+    original_event.encryption_info = Some(encryption_info.clone());
+
+    timeline.handle_live_event(original_event).await;
+
+    let items = timeline.inner.items().await;
+    let first_event = items[1].as_event().unwrap();
+
+    assert_eq!(
+        first_event.encryption_info().unwrap().verification_state,
+        VerificationState::Verified
+    );
+
+    assert_let!(TimelineItemContent::Message(message) = first_event.content());
+    assert_let!(MessageType::Text(text) = message.msgtype());
+    assert_eq!(text.body, "**original** message");
+
+    let mut edit_event = event_factory
+        .text_msg(" * !!edited!! **better** message")
+        .sender(*ALICE)
+        .edit(original_event_id, MessageType::text_plain("!!edited!! **better** message").into())
+        .into_sync();
+    encryption_info.verification_state =
+        VerificationState::Unverified(VerificationLevel::UnverifiedIdentity);
+    edit_event.encryption_info = Some(encryption_info);
+
+    timeline.handle_live_event(edit_event).await;
+
+    let items = timeline.inner.items().await;
+    let first_event = items[1].as_event().unwrap();
+
+    assert_eq!(
+        first_event.encryption_info().unwrap().verification_state,
+        VerificationState::Unverified(VerificationLevel::UnverifiedIdentity)
+    );
+
+    assert_let!(TimelineItemContent::Message(message) = first_event.content());
+    assert_let!(MessageType::Text(text) = message.msgtype());
+    assert_eq!(text.body, "!!edited!! **better** message");
 }

commit 9b351e4e504e84c90f855440c30bd17a4d70cf7b

fix(ui): Fix latest_edit_json for live edits
This was a regression introduced in
f0d98602a9e65cf26a607cf10a0715b92da12d4a.

`latest_edit_json` was first set by the call to
`EventTimelineItem::with_content()`.
It was overwritten in the next section because of the
`if let EventTimelineItemKind::Remote(remote_event)`
that uses `item` instead of `new_item`.
It means that the updated `RemoteEventTimelineItem`
inside`new_item` was replaced by the outdated one inside `item`,
so `latest_edit_json` goes back to its previous value.

I believe that part of why that went unnoticed is that the code looks
more
complicated due to the need to set an inner field, so I decided to
change the API and
move `with_encryption_info` to `EventTimelineItem`, which makes the code
look cleaner.

Signed-off-by: Kévin Commaille <zecakeh@tedomum.fr>

crates/matrix-sdk-ui/src/timeline/event_handler.rs
@@ -687,12 +687,8 @@ impl<'a, 'o> TimelineEventHandler<'a, 'o> {
 
         let mut new_item = item.with_content(TimelineItemContent::Message(new_msg), edit_json);
 
-        if let EventTimelineItemKind::Remote(remote_event) = &item.kind {
-            if let Flow::Remote { encryption_info, .. } = &self.ctx.flow {
-                new_item = new_item.with_kind(EventTimelineItemKind::Remote(
-                    remote_event.with_encryption_info(encryption_info.clone()),
-                ));
-            }
+        if let Flow::Remote { encryption_info, .. } = &self.ctx.flow {
+            new_item = new_item.with_encryption_info(encryption_info.clone());
         }
 
         Some(new_item)

crates/matrix-sdk-ui/src/timeline/event_item/mod.rs
@@ -528,6 +528,16 @@ impl EventTimelineItem {
         Self { sender_profile, ..self.clone() }
     }
 
+    /// Clone the current event item, and update its `encryption_info`.
+    pub(super) fn with_encryption_info(&self, encryption_info: Option<EncryptionInfo>) -> Self {
+        let mut new = self.clone();
+        if let EventTimelineItemKind::Remote(r) = &mut new.kind {
+            r.encryption_info = encryption_info;
+        }
+
+        new
+    }
+
     /// Create a clone of the current item, with content that's been redacted.
     pub(super) fn redact(&self, room_version: &RoomVersionId) -> Self {
         let content = self.content.redact(room_version);

crates/matrix-sdk-ui/src/timeline/event_item/remote.rs
@@ -68,11 +68,6 @@ pub(in crate::timeline) struct RemoteEventTimelineItem {
 }
 
 impl RemoteEventTimelineItem {
-    /// Clone the current event item, and update its `encryption_info`.
-    pub fn with_encryption_info(&self, encryption_info: Option<EncryptionInfo>) -> Self {
-        Self { encryption_info, ..self.clone() }
-    }
-
     /// Clone the current event item, and redacts its fields.
     pub fn redact(&self) -> Self {
         Self { original_json: None, latest_edit_json: None, ..self.clone() }

crates/matrix-sdk-ui/src/timeline/tests/encryption.rs
@@ -348,6 +348,7 @@ async fn test_retry_edit_decryption() {
     let item = items[1].as_event().unwrap();
 
     assert_matches!(item.encryption_info(), Some(_));
+    assert_matches!(item.latest_edit_json(), Some(_));
     assert_let!(TimelineItemContent::Message(msg) = item.content());
     assert!(msg.is_edited());
     assert_eq!(msg.body(), "This is Error");
@@ -452,10 +453,9 @@ async fn test_retry_edit_and_more() {
     let timeline_items = timeline.controller.items().await;
     assert_eq!(timeline_items.len(), 3);
     assert!(timeline_items[0].is_date_divider());
-    assert_eq!(
-        timeline_items[1].as_event().unwrap().content().as_message().unwrap().body(),
-        "edited"
-    );
+    let timeline_event = timeline_items[1].as_event().unwrap();
+    assert!(timeline_event.latest_edit_json().is_some());
+    assert_eq!(timeline_event.content().as_message().unwrap().body(), "edited");
     assert_eq!(
         timeline_items[2].as_event().unwrap().content().as_message().unwrap().body(),
         "Another message"

crates/matrix-sdk-ui/tests/integration/timeline/edit.rs
@@ -101,6 +101,7 @@ async fn test_edit() {
     assert_let!(Some(VectorDiff::PushBack { value: first }) = timeline_stream.next().await);
     let item = first.as_event().unwrap();
     assert_eq!(item.read_receipts().len(), 1, "implicit read receipt");
+    assert_matches!(item.latest_edit_json(), None);
     assert_let!(TimelineItemContent::Message(msg) = item.content());
     assert_matches!(msg.msgtype(), MessageType::Text(_));
     assert_matches!(msg.in_reply_to(), None);
@@ -131,6 +132,7 @@ async fn test_edit() {
     assert_eq!(item.read_receipts().len(), 1, "implicit read receipt");
 
     assert_let!(TimelineItemContent::Message(msg) = item.content());
+    assert_matches!(item.latest_edit_json(), None);
     assert_let!(MessageType::Text(TextMessageEventContent { body, .. }) = msg.msgtype());
     assert_eq!(body, "Test");
     assert_matches!(msg.in_reply_to(), None);
@@ -140,6 +142,7 @@ async fn test_edit() {
     // something after the second event.
     assert_let!(Some(VectorDiff::Set { index: 1, value: item }) = timeline_stream.next().await);
     let item = item.as_event().unwrap();
+    assert_matches!(item.latest_edit_json(), None);
     assert_let!(TimelineItemContent::Message(msg) = item.content());
     assert_let!(MessageType::Text(text) = msg.msgtype());
     assert_eq!(text.body, "hello");
@@ -158,6 +161,7 @@ async fn test_edit() {
     // The text changes in Alice's message.
     assert_let!(Some(VectorDiff::Set { index: 1, value: edit }) = timeline_stream.next().await);
     let item = edit.as_event().unwrap();
+    assert_matches!(item.latest_edit_json(), Some(_));
     assert_let!(TimelineItemContent::Message(edited) = item.content());
     assert_let!(MessageType::Text(text) = edited.msgtype());
     assert_eq!(text.body, "hi");

crates/matrix-sdk-ui/tests/integration/timeline/subscribe.rs
@@ -129,6 +129,7 @@ async fn test_event_filter() {
     let first_event = first.as_event().unwrap();
     assert_eq!(first_event.event_id(), Some(first_event_id));
     assert_eq!(first_event.read_receipts().len(), 1, "implicit read receipt");
+    assert_matches!(first_event.latest_edit_json(), None);
     assert_let!(TimelineItemContent::Message(msg) = first_event.content());
     assert_matches!(msg.msgtype(), MessageType::Text(_));
     assert!(!msg.is_edited());
@@ -190,6 +191,7 @@ async fn test_event_filter() {
     assert_let!(Some(VectorDiff::Set { index: 1, value: first }) = timeline_stream.next().await);
     let first_event = first.as_event().unwrap();
     assert!(first_event.read_receipts().is_empty());
+    assert_matches!(first_event.latest_edit_json(), Some(_));
     assert_let!(TimelineItemContent::Message(msg) = first_event.content());
     assert_let!(MessageType::Text(text) = msg.msgtype());
     assert_eq!(text.body, "hi");