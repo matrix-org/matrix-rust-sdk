f19516905adf220c0b99d4e392a7cb8a567e94ce
Damir Jelić <poljar@termina.org.uk>
2025-03-10 12:23:38 +0100

    fixup! refactor(multiverse): Move the set_status_message under the Status widget

--- labs/multiverse/src/status.rs
+++ labs/multiverse/src/status.rs
@@ -1,6 +1,11 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
-use tokio::task::JoinHandle;
+use tokio::{spawn, task::JoinHandle, time::sleep};
+
+const MESSAGE_DURATION: Duration = Duration::from_secs(4);
 
 pub struct Status {
     /// Content of the latest status message, if set.
@@ -14,5 +19,24 @@ impl Status {
     /// Create a new empty [`Status`] widget.
     pub fn new() -> Self {
         Self { last_status_message: Default::default(), clear_status_message: Default::default() }
+    }
+
+    /// Set the current status message (displayed at the bottom), for a few
+    /// seconds.
+    pub fn set_message(&mut self, status: String) {
+        if let Some(handle) = self.clear_status_message.take() {
+            // Cancel the previous task to clear the status message.
+            handle.abort();
+        }
+
+        *self.last_status_message.lock().unwrap() = Some(status);
+
+        let message = self.last_status_message.clone();
+        self.clear_status_message = Some(spawn(async move {
+            // Clear the status message in 4 seconds.
+            sleep(MESSAGE_DURATION).await;
+
+            *message.lock().unwrap() = None;
+        }));
     }
 }
