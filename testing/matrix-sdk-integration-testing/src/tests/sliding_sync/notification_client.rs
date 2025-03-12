82add9709b3393596602b0ed909f638a52d91e3c
Ivan Enderlin <ivan@mnt.io>
2025-03-12 13:54:24 +0100

    test: Fix a test in `NotificationClient`.
    With the previous patch, `NotificationClient` with sliding sync now
    knows immediately when the room is encrypted or not.

--- testing/matrix-sdk-integration-testing/src/tests/sliding_sync/notification_client.rs
+++ testing/matrix-sdk-integration-testing/src/tests/sliding_sync/notification_client.rs
@@ -100,7 +100,7 @@ async fn test_notification() -> Result<()> {
         assert_matches!(notification.event, NotificationEvent::Invite(_));
         assert_eq!(notification.event.sender(), alice.user_id().unwrap());
         assert_eq!(notification.joined_members_count, 0);
-        assert_eq!(notification.is_room_encrypted, None);
+        assert_eq!(notification.is_room_encrypted, Some(false));
         assert!(notification.is_direct_message_room);
 
         assert_matches!(notification.event, NotificationEvent::Invite(observed_invite) => {
