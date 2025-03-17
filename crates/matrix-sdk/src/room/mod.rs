7f8b3cb0ad7b824a0a562601cdabce1607ab807d
Richard van der Hoff <richard@matrix.org>
2025-03-17 12:54:19 +0000

    sdk: add `Room::share_history`
    The next step in our work on sharing encrypted room history. Add a method to
    `matrix_sdk::room::Room` which will upload an encrypted key bundle.

--- crates/matrix-sdk/src/room/mod.rs
+++ crates/matrix-sdk/src/room/mod.rs
@@ -140,6 +140,7 @@ use crate::{
     attachment::{AttachmentConfig, AttachmentInfo},
     client::WeakClient,
     config::RequestConfig,
+    encryption::futures::ShareRoomHistory,
     error::{BeaconError, WrongRoomState},
     event_cache::{self, EventCacheDropHandles, RoomEventCache},
     event_handler::{EventHandler, EventHandlerDropGuard, EventHandlerHandle, SyncEvent},
@@ -1749,6 +1750,20 @@ impl Room {
         }
 
         Ok(())
+    }
+
+    /// Share any shareable E2EE history in this room with the given recipient,
+    /// as per [MSC4268].
+    ///
+    /// # Panics
+    ///
+    /// Panics if the OlmMachine hasn't been set up on the client.
+    ///
+    /// [MSC4268]: https://github.com/matrix-org/matrix-spec-proposals/pull/4268
+    #[cfg(feature = "e2e-encryption")]
+    #[instrument(skip_all, fields(room_id = ?self.room_id(), ?user_id))]
+    pub(crate) fn share_history<'a>(&'a self, user_id: &UserId) -> ShareRoomHistory<'a> {
+        ShareRoomHistory::new(self, user_id.to_owned())
     }
 
     /// Wait for the room to be fully synced.
