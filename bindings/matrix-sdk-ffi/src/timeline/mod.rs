9a2946b4ed3e1bf02dd6b1d4f44c9a32bb31a7d8
Benjamin Bouvier <benjamin@bouvier.cc>
2025-03-19 16:09:37 +0100

    feat(timeline): add a timeline start virtual item

--- bindings/matrix-sdk-ffi/src/timeline/mod.rs
+++ bindings/matrix-sdk-ffi/src/timeline/mod.rs
@@ -974,6 +974,7 @@ impl TimelineItem {
         match self.0.as_virtual()? {
             VItem::DateDivider(ts) => Some(VirtualTimelineItem::DateDivider { ts: (*ts).into() }),
             VItem::ReadMarker => Some(VirtualTimelineItem::ReadMarker),
+            VItem::TimelineStart => Some(VirtualTimelineItem::TimelineStart),
         }
     }
 
@@ -1254,6 +1255,9 @@ pub enum VirtualTimelineItem {
 
     /// The user's own read marker.
     ReadMarker,
+
+    /// The timeline start, that is, the *oldest* event in time for that room.
+    TimelineStart,
 }
 
 /// A [`TimelineItem`](super::TimelineItem) that doesn't correspond to an event.
