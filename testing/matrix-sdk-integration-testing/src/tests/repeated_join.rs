bd6b7c2ce182f989d5e99597140d569297d66088
Ivan Enderlin <ivan@mnt.io>
2025-03-26 17:10:03 +0100

    refactor(base): Rename `BaseClient::store` to `state_store`.
    This patch renames `BaseClient::store` to `state_store`, and
    inevitably `Client::store` to `state_store` too.

--- testing/matrix-sdk-integration-testing/src/tests/repeated_join.rs
+++ testing/matrix-sdk-integration-testing/src/tests/repeated_join.rs
@@ -97,15 +97,15 @@ async fn test_repeated_join_leave() -> Result<()> {
 
     // Now check the underlying state store that it also has the correct information
     // (for when the client restarts).
-    let invited = karl.store().get_user_ids(room_id, RoomMemberships::INVITE).await?;
+    let invited = karl.state_store().get_user_ids(room_id, RoomMemberships::INVITE).await?;
     assert_eq!(invited.len(), 1);
     assert_eq!(invited[0], karl_id);
 
-    let joined = karl.store().get_user_ids(room_id, RoomMemberships::JOIN).await?;
+    let joined = karl.state_store().get_user_ids(room_id, RoomMemberships::JOIN).await?;
     assert!(!joined.contains(&karl_id));
 
     let event = karl
-        .store()
+        .state_store()
         .get_member_event(room_id, &karl_id)
         .await?
         .expect("member event should exist")
