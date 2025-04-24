67ee57271cf8585567b1d6272d345e6b5cd113d8
Timo <toger5@hotmail.de>
2025-04-24 17:01:03 +0200

    WidgetDriver: Add module documentation

--- crates/matrix-sdk/src/widget/mod.rs
+++ crates/matrix-sdk/src/widget/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Widget API implementation.
+#![doc = include_str!("README.md")]
 
 use std::{fmt, time::Duration};
 
