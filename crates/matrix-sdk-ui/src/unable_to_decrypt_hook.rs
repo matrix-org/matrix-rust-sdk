37c4458c1f4b175c7161e71eaeab9269707a709a
Damir Jelić <poljar@termina.org.uk>
2025-04-25 12:28:55 +0200

    Apply suggestions from code review
    Co-authored-by: Benjamin Bouvier <benjamin@bouvier.cc>
    Signed-off-by: Damir Jelić <poljar@termina.org.uk>

--- crates/matrix-sdk-ui/src/unable_to_decrypt_hook.rs
+++ crates/matrix-sdk-ui/src/unable_to_decrypt_hook.rs
@@ -234,7 +234,7 @@ impl UtdHookManager {
         event_timestamp: MilliSecondsSinceUnixEpoch,
         sender_user_id: &UserId,
     ) {
-        trace!("On UTD for event {}", event_id);
+        trace!(%event_id, "UtdHookManager: Observed UTD");
         // Hold the lock on `reported_utds` throughout, to avoid races with other
         // threads.
         let mut reported_utds_lock = self.reported_utds.lock().await;
@@ -332,7 +332,7 @@ impl UtdHookManager {
     /// Note: if this is called for an event that was never marked as a UTD
     /// before, it has no effect.
     pub(crate) async fn on_late_decrypt(&self, event_id: &EventId) {
-        trace!("On late decrypt for event {}", event_id);
+        trace!(%event_id, "UtdHookManager: On late decrypt");
         // Hold the lock on `reported_utds` throughout, to avoid races with other
         // threads.
         let mut reported_utds_lock = self.reported_utds.lock().await;
@@ -341,7 +341,7 @@ impl UtdHookManager {
         // a pending UTD. If so, remove the event from the pending list —
         // doing so will cause the reporting task to no-op if it runs.
         let Some(pending_utd_report) = self.pending_delayed.lock().unwrap().remove(event_id) else {
-            trace!("Received a late decrypt report for an unknown utd, eventId:{}", event_id);
+            trace!(%event_id, "UtdHookManager: received a late decrypt report for an unknown utd");
             return;
         };
 
