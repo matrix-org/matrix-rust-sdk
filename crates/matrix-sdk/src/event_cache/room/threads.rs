531d5dc60afba290bf2fb82546c153c6d3565c03
Benjamin Bouvier <benjamin@bouvier.cc>
2025-05-14 17:14:30 +0200

    feat(thread): process a single event from sync based on its thread info

--- crates/matrix-sdk/src/event_cache/room/threads.rs
+++ crates/matrix-sdk/src/event_cache/room/threads.rs
@@ -12,12 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use as_variant::as_variant;
-use ruma::{events::AnySyncTimelineEvent, serde::Raw, OwnedEventId};
+use std::collections::BTreeMap;
+
+use ruma::{events::AnySyncTimelineEvent, serde::Raw, OwnedEventId, OwnedRoomId};
 use thiserror::Error;
 
 mod serde_helpers {
-    use ruma::events::relation::BundledThread;
+    use ruma::{events::relation::BundledThread, OwnedEventId};
     use serde::Deserialize;
 
     #[derive(Deserialize)]
@@ -30,6 +31,115 @@ mod serde_helpers {
     pub struct Unsigned {
         #[serde(rename = "m.relations")]
         pub relations: Option<Relations>,
+    }
+
+    #[derive(Deserialize)]
+    enum RelationsType {
+        #[serde(rename = "m.thread")]
+        Thread,
+    }
+
+    #[derive(Deserialize)]
+    struct RelatesTo {
+        #[serde(rename = "rel_type")]
+        rel_type: RelationsType,
+        #[serde(rename = "event_id")]
+        event_id: Option<OwnedEventId>,
+    }
+
+    #[derive(Deserialize)]
+    pub struct SimplifiedContent {
+        #[serde(rename = "m.relates_to")]
+        relates_to: Option<RelatesTo>,
+    }
+
+    impl SimplifiedContent {
+        pub fn thread_root(self) -> Option<OwnedEventId> {
+            self.relates_to?.event_id
+        }
+    }
+}
+
+/// All the information and events related to a single thread.
+#[derive(Debug)]
+pub struct ThreadEventCache {
+    /// Current, always up-to-date thread summary.
+    summary: ThreadSummary,
+}
+
+/// A model of all the threads in a room.
+pub struct RoomThreads {
+    room_id: OwnedRoomId,
+
+    threads: BTreeMap<OwnedEventId, ThreadEventCache>,
+}
+
+impl RoomThreads {
+    pub fn new(room_id: OwnedRoomId) -> Self {
+        // TODO: persistent storage, reload threads from the database, etc.
+        Self { room_id, threads: BTreeMap::new() }
+    }
+
+    /// Given a new sync event, process it and update the thread cache.
+    ///
+    /// If the sync event was the root of a thread we didn't know about, or part
+    /// of a thread, this will return a thread summary update (along with
+    /// the thread root id), to be forwarded to observers.
+    fn on_new_sync_event(
+        &mut self,
+        raw: &Raw<AnySyncTimelineEvent>,
+    ) -> Result<Option<(OwnedEventId, ThreadSummary)>, EventCacheThreadError> {
+        let Some(event_id) = raw.get_field::<OwnedEventId>("event_id").ok().flatten() else {
+            // No event id, nothing to do.
+            return Ok(None);
+        };
+
+        // Try to look if it's a new thread root. Since we only create a thread summary
+        // if we didn't know about the thread, check for that first, to avoid
+        // deserializing for nothing.
+        if !self.threads.contains_key(&event_id) {
+            if let Some(summary) = ThreadSummary::extract_from_bundled(raw)? {
+                self.threads
+                    .insert(event_id.clone(), ThreadEventCache { summary: summary.clone() });
+                return Ok(Some((event_id, summary)));
+            }
+        }
+
+        // Alternatively, try to see if this is event is part of a thread itself.
+        if let Some(thread_root) = raw
+            .get_field::<serde_helpers::SimplifiedContent>("content")
+            .ok()
+            .flatten()
+            .and_then(|content| content.thread_root())
+        {
+            if let Some(thread_cache) = self.threads.get_mut(&thread_root) {
+                let known_summary = &mut thread_cache.summary;
+                known_summary.count += 1;
+                known_summary.latest_event = event_id;
+                // TODO: we can update the current_user_participated field too,
+                // based on the sender.
+                //summary.current_user_participated = maybe?
+                return Ok(Some((thread_root, known_summary.clone())));
+            }
+
+            // The thread was unknown; add a stub summary.
+            let summary = ThreadSummary {
+                // The root + this event.
+                count: 2,
+                // This is the final event.
+                latest_event: event_id.clone(),
+                // TODO: we can do better than that, based on the sender.
+                current_user_participated: false,
+                // This thread is likely outdated.
+                maybe_outdated: true,
+            };
+
+            self.threads.insert(thread_root.clone(), ThreadEventCache { summary: summary.clone() });
+
+            Ok(Some((thread_root, summary)))
+        } else {
+            Ok(None)
+        }
     }
 }
 
@@ -126,7 +236,7 @@ mod tests {
         room_id, uint,
     };
 
-    use super::ThreadSummary;
+    use super::{RoomThreads, ThreadSummary};
     use crate::event_cache::room::threads::EventCacheThreadError;
 
     #[test]
@@ -170,5 +280,93 @@ mod tests {
 
         let err = ThreadSummary::extract_from_bundled(&root).expect_err("extracting didn't work");
         assert_matches!(err, EventCacheThreadError::SummaryDeserialization(_));
+    }
+
+    #[test]
+    fn test_thread_summary_updated_on_new_thread_root() {
+        let room_id = room_id!("!room:example.org");
+        let f = EventFactory::new().sender(&ALICE).room(room_id);
+
+        let latest_event_id = event_id!("$latest");
+        let latest_event = f.text_msg("hello to you too!").event_id(latest_event_id).into_raw();
+        let bundled_thread = BundledThread::new(latest_event, uint!(3), false);
+        let mut relations = BundledMessageLikeRelations::new();
+        relations.thread = Some(Box::new(bundled_thread));
+
+        let root_event_id = event_id!("$root");
+        let root = f
+            .text_msg("hello world!")
+            .bundled_relations(relations)
+            .event_id(root_event_id)
+            .into_raw();
+
+        let mut room_threads = RoomThreads::new(room_id.to_owned());
+
+        // We find a new summary the first time we process this event.
+        let (thread_root, summary) = room_threads
+            .on_new_sync_event(&root)
+            .expect("event processing works")
+            .expect("summary found");
+
+        assert_eq!(thread_root, root_event_id);
+
+        assert_eq!(summary.count, 3);
+        assert!(summary.current_user_participated.not());
+        assert_eq!(summary.latest_event, latest_event_id);
+        assert!(summary.maybe_outdated);
+
+        // But if we already knew about this thread, we won't emit a new thread summary
+        // update.
+        let summary_update = room_threads.on_new_sync_event(&root).expect("event processing works");
+        assert_matches!(summary_update, None);
+    }
+
+    #[test]
+    fn test_thread_summary_updated_on_new_thread_response() {
+        let room_id = room_id!("!room:example.org");
+        let f = EventFactory::new().sender(&ALICE).room(room_id);
+
+        let root_event_id = event_id!("$root");
+        let thread_event_id = event_id!("$thread");
+        let event = f
+            .text_msg("hello to you too!")
+            .in_thread(root_event_id, thread_event_id)
+            .event_id(thread_event_id)
+            .into_raw();
+
+        let mut room_threads = RoomThreads::new(room_id.to_owned());
+
+        // We get a summary for the thread root the first time we process this event.
+        let (found_root, summary) = room_threads
+            .on_new_sync_event(&event)
+            .expect("event processing works")
+            .expect("summary found");
+
+        assert_eq!(found_root, root_event_id);
+
+        assert_eq!(summary.count, 2);
+        assert!(summary.current_user_participated.not());
+        assert_eq!(&summary.latest_event, thread_event_id);
+        assert!(summary.maybe_outdated);
+
+        // On subsequent events, we will update the thread summary accordingly.
+        let thread_event_id2 = event_id!("$thread2");
+        let event = f
+            .text_msg("bonjour !")
+            .in_thread(root_event_id, thread_event_id2)
+            .event_id(thread_event_id2)
+            .into_raw();
+
+        let (found_root, summary) = room_threads
+            .on_new_sync_event(&event)
+            .expect("event processing works")
+            .expect("summary found");
+
+        assert_eq!(found_root, root_event_id);
+
+        assert_eq!(summary.count, 3);
+        assert!(summary.current_user_participated.not());
+        assert_eq!(&summary.latest_event, thread_event_id2);
+        assert!(summary.maybe_outdated);
     }
 }
