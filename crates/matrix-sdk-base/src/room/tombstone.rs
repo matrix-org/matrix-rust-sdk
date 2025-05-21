669e9fa735ff9cbcd275fd5c8f90b5c62c955520
Ivan Enderlin <ivan@mnt.io>
2025-05-21 14:28:42 +0200

    chore(base): Move the `Room::*tombstone*` methods in new `tombstone` module.
    This patch moves the `Room::is_tombstoned` and `Room::tombstone` methods
    in the new `tombstone` module.

---
+++ crates/matrix-sdk-base/src/room/tombstone.rs
@@ -0,0 +1,29 @@
+// Copyright 2025 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use ruma::events::room::tombstone::RoomTombstoneEventContent;
+
+use super::Room;
+
+impl Room {
+    /// Has the room been tombstoned.
+    pub fn is_tombstoned(&self) -> bool {
+        self.inner.read().base_info.tombstone.is_some()
+    }
+
+    /// Get the `m.room.tombstone` content of this room if there is one.
+    pub fn tombstone(&self) -> Option<RoomTombstoneEventContent> {
+        self.inner.read().tombstone().cloned()
+    }
+}
