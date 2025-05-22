0c4a895298fd461aa60e90ba387274344e580f6b
Valere <bill.carson@valrsoft.com>
2025-05-22 15:41:15 +0200

    fixup: coverage, test deserialize new format

--- crates/matrix-sdk-crypto/src/machine/tests/send_encrypted_to_device.rs
+++ crates/matrix-sdk-crypto/src/machine/tests/send_encrypted_to_device.rs
@@ -554,7 +554,7 @@ async fn test_migration_old_olm_decrypted_processed_event() {
     );
 
     assert_matches!(
-        processed_event,
+        processed_event.clone(),
         ProcessedToDeviceEvent::Decrypted {
             raw: _,
             encryption_info: EncryptionInfo {
@@ -564,5 +564,13 @@ async fn test_migration_old_olm_decrypted_processed_event() {
                 ..
             }
         }
+    );
+
+    // Serialize to new format and deserialize again
+    let json = serde_json::to_value(processed_event).unwrap();
+    let deserialized: ProcessedToDeviceEvent = serde_json::from_value(json).unwrap();
+    assert_eq!(
+        deserialized.to_raw().deserialize().unwrap().sender().as_str(),
+        "@alice:example.org"
     );
 }
