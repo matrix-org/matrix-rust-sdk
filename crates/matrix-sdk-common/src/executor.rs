5942f0e06f918d1cf7b46c19950a0e15d841e080
Daniel Salinas <danielsalinas@Daniels-MacBook-Pro-2.local>
2025-05-23 12:04:57 -0400

    Improve wasm join handle to implement more tokio methods
    This change adds support for the abort/abort_handle/is_finished
    methods onto the JoinHandle shim, by introducing a dependency
    on n0-future, a wasm-compatible library.
    
    This library strives to implement the methods of tokio, but does not
    do them in exactly the same fashion.  In order to minimize the
    amount of code that needs to be Wasm aware, the JoinHandle
    wrapper is maintained.

--- crates/matrix-sdk-common/src/executor.rs
+++ crates/matrix-sdk-common/src/executor.rs
@@ -12,87 +12,80 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Abstraction over an executor so we can spawn tasks under WASM the same way
+//! Abstraction over an executor so we can spawn tasks under Wasm the same way
 //! we do usually.
 
-#[cfg(target_arch = "wasm32")]
-use std::{
-    future::Future,
-    pin::Pin,
-    task::{Context, Poll},
-};
+//! On non Wasm platforms, this re-exports parts of tokio directly.  For Wasm,
+//! we wrap the n0-future package's AbortHandle + spawn method in order to
+//! provide an interface that is similar to tokio's.
 
-#[cfg(target_arch = "wasm32")]
-pub use futures_util::future::Aborted as JoinError;
-#[cfg(target_arch = "wasm32")]
-use futures_util::{
-    future::{AbortHandle, Abortable, RemoteHandle},
-    FutureExt,
-};
-#[cfg(not(target_arch = "wasm32"))]
-pub use tokio::task::{spawn, JoinError, JoinHandle};
+#[cfg(not(target_family = "wasm"))]
+mod sys {
+    pub use tokio::task::{spawn, AbortHandle, JoinError, JoinHandle};
 
-#[cfg(target_arch = "wasm32")]
-pub fn spawn<F, T>(future: F) -> JoinHandle<T>
-where
-    F: Future<Output = T> + 'static,
-{
-    let (future, remote_handle) = future.remote_handle();
-    let (abort_handle, abort_registration) = AbortHandle::new_pair();
-    let future = Abortable::new(future, abort_registration);
-
-    wasm_bindgen_futures::spawn_local(async {
-        // Poll the future, and ignore the result (either it's `Ok(())`, or it's
-        // `Err(Aborted)`).
-        let _ = future.await;
-    });
-
-    JoinHandle { remote_handle: Some(remote_handle), abort_handle }
+    /// A handle to a runtime for executing async tasks and futures.
+    pub type Handle = tokio::runtime::Handle;
+    pub type Runtime = tokio::runtime::Runtime;
 }
 
-#[cfg(target_arch = "wasm32")]
-#[derive(Debug)]
-pub struct JoinHandle<T> {
-    remote_handle: Option<RemoteHandle<T>>,
-    abort_handle: AbortHandle,
-}
+#[cfg(target_family = "wasm")]
+mod sys {
+    use std::{
+        future::Future,
+        pin::Pin,
+        task::{Context, Poll},
+    };
 
-#[cfg(target_arch = "wasm32")]
-impl<T> JoinHandle<T> {
-    pub fn abort(&self) {
-        self.abort_handle.abort();
+    pub use futures_util::future::AbortHandle;
+    pub use n0_future::task::{spawn as n0_spawn, JoinError};
+
+    /// Wrapper around n0-future's JoinHandle to provide tokio-compatible API
+    /// The wrapper adds methods the abort, abort_handle, and is_finished
+    /// methods, in order to have better conformity with the tokio equivalents.
+    #[derive(Debug)]
+    pub struct JoinHandle<T> {
+        inner: n0_future::task::JoinHandle<Result<T, futures_util::future::Aborted>>,
+        abort_handle: AbortHandle,
     }
 
-    pub fn is_finished(&self) -> bool {
-        self.abort_handle.is_aborted()
-    }
-}
+    impl<T> JoinHandle<T> {
+        pub fn abort(&self) {
+            self.abort_handle.abort();
+        }
 
-#[cfg(target_arch = "wasm32")]
-impl<T> Drop for JoinHandle<T> {
-    fn drop(&mut self) {
-        // don't abort the spawned future
-        if let Some(h) = self.remote_handle.take() {
-            h.forget();
+        pub fn abort_handle(&self) -> AbortHandle {
+            self.abort_handle.clone()
+        }
+
+        pub fn is_finished(&self) -> bool {
+            self.abort_handle.is_aborted()
         }
     }
-}
 
-#[cfg(target_arch = "wasm32")]
-impl<T: 'static> Future for JoinHandle<T> {
-    type Output = Result<T, JoinError>;
+    impl<T> Future for JoinHandle<T> {
+        type Output = Result<T, JoinError>;
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        if self.abort_handle.is_aborted() {
-            // The future has been aborted. It is not possible to poll it again.
-            Poll::Ready(Err(JoinError))
-        } else if let Some(handle) = self.remote_handle.as_mut() {
-            Pin::new(handle).poll(cx).map(Ok)
-        } else {
-            Poll::Ready(Err(JoinError))
+        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+            match Pin::new(&mut self.inner).poll(cx) {
+                Poll::Ready(Ok(Ok(value))) => Poll::Ready(Ok(value)),
+                Poll::Ready(Ok(Err(_aborted))) => Poll::Ready(Err(JoinError::Cancelled)),
+                Poll::Ready(Err(join_err)) => Poll::Ready(Err(join_err)),
+                Poll::Pending => Poll::Pending,
+            }
         }
     }
+
+    pub fn spawn<F, T: 'static>(future: F) -> JoinHandle<T>
+    where
+        F: Future<Output = T> + 'static,
+    {
+        let (abortable_future, abort_handle) = futures_util::future::abortable(future);
+        let inner = n0_spawn(abortable_future);
+        JoinHandle { inner, abort_handle }
+    }
 }
+
+pub use sys::*;
 
 #[cfg(test)]
 mod tests {
