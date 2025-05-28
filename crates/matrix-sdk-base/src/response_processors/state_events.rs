65b1200d171049bb17460e27d970cc4ec15af3c0
Ivan Enderlin <ivan@mnt.io>
2025-05-28 17:47:29 +0200

    feat(base): Detecting invalid states in room upgrades.
    This patch adds the `check_tombstone` response processor to
    detect invalid state with room upgrades. One can check the new
    `InconsistentTombstonedRooms` error to learn more about the detected
    patterns.

--- crates/matrix-sdk-base/src/response_processors/state_events.rs
+++ crates/matrix-sdk-base/src/response_processors/state_events.rs
@@ -12,11 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::{BTreeMap, BTreeSet};
+
 use ruma::{events::AnySyncStateEvent, serde::Raw};
 use serde::Deserialize;
 use tracing::warn;
 
 use super::Context;
+use crate::{store::BaseStateStore, sync::RoomUpdates, Error, Result};
 
 /// Collect [`AnySyncStateEvent`].
 pub mod sync {
@@ -248,15 +251,492 @@ where
         .unzip()
 }
 
+/// Check room upgrades (`m.room.tombstone` and `m.room.create`) aren't creating
+/// a loop or a merger.
+///
+/// Please see [`Error::InconsistentTombstonedRooms`] to learn more about the
+/// kind of invalid states it can detect.
+pub fn check_tombstone(
+    context: &mut Context,
+    room_updates: &RoomUpdates,
+    state_store: &BaseStateStore,
+) -> Result<()> {
+    let mut room_updates = room_updates
+        .iter_all_room_ids()
+        // Ignore rooms with no `RoomInfo`s.
+        .filter_map(|room_id| context.state_changes.room_infos.get(room_id))
+        .map(|room_info| {
+            (
+                room_info.room_id.clone(),
+                room_info.base_info.tombstone.as_ref().and_then(|tombstone_event| {
+                    Some(tombstone_event.as_original()?.content.replacement_room.clone())
+                }),
+            )
+        })
+        .collect::<BTreeMap<_, _>>();
+
+    let mut already_seen = BTreeSet::new();
+
+    while let Some((mut room_id, mut maybe_replacement_room_id)) = room_updates.pop_first() {
+        loop {
+            if already_seen.contains(&room_id) {
+                return Err(Error::InconsistentTombstonedRooms { room_in_path: room_id });
+            }
+
+            // First time we see this room: let's remember it.
+            already_seen.insert(room_id);
+
+            let Some(replacement_room_id) = maybe_replacement_room_id else {
+                // No replacement room. Stop looking for one.
+                break;
+            };
+
+            // Look up for the replacement room in the `room_updates`.
+            let (next_room_id, next_replacement_room_id) = if let Some(next_replacement_room_id) =
+                room_updates.remove(&replacement_room_id)
+            {
+                (replacement_room_id, next_replacement_room_id)
+            }
+            // Look for for the replacement room in the known rooms.
+            else if let Some(replacement_room) = state_store.room(&replacement_room_id) {
+                (
+                    replacement_room_id,
+                    replacement_room.successor_room().map(|successor_room| successor_room.room_id),
+                )
+            } else {
+                break;
+            };
+
+            room_id = next_room_id;
+            maybe_replacement_room_id = next_replacement_room_id;
+        }
+    }
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
+    use assert_matches::assert_matches;
     use matrix_sdk_test::{
         async_test, event_factory::EventFactory, JoinedRoomBuilder, StateTestEvent,
         SyncResponseBuilder, DEFAULT_TEST_ROOM_ID,
     };
-    use ruma::{event_id, user_id};
+    use ruma::{event_id, room_id, user_id, RoomVersionId};
 
-    use crate::test_utils::logged_in_base_client;
+    use crate::{test_utils::logged_in_base_client, Error};
+
+    #[async_test]
+    async fn test_check_tombstone_no_newly_tombstoned_rooms() {
+        let client = logged_in_base_client(None).await;
+
+        // Create a new room, no tombstone, no anything.
+        {
+            let response = SyncResponseBuilder::new()
+                .add_joined_room(JoinedRoomBuilder::new(room_id!("!r0")))
+                .build_sync_response();
+
+            assert!(client.receive_sync_response(response).await.is_ok());
+        }
+    }
+
+    /// This test is ensuring [`check_tombstone`] is not rejecting normal room
+    /// upgrades not forming a loop or a merger.
+    #[async_test]
+    async fn test_check_tombstone_no_error() {
+        let sender = user_id!("@mnt_io:matrix.org");
+        let event_factory = EventFactory::new().sender(sender);
+        let mut response_builder = SyncResponseBuilder::new();
+        let room_id_0 = room_id!("!r0");
+        let room_id_1 = room_id!("!r1");
+        let room_id_2 = room_id!("!r2");
+
+        let client = logged_in_base_client(None).await;
+
+        // Create room 0.
+        {
+            let response = response_builder
+                .add_joined_room(JoinedRoomBuilder::new(room_id_0))
+                .build_sync_response();
+
+            assert!(client.receive_sync_response(response).await.is_ok());
+
+            let room_0 = client.get_room(room_id_0).unwrap();
+
+            assert!(room_0.predecessor_room().is_none());
+            assert!(room_0.successor_room().is_none());
+        }
+
+        // Room 0 is being tombstoned by room 1.
+        {
+            let tombstone_event_id = event_id!("$ev0");
+            let response = response_builder
+                // tombstoning room 0 with room 1
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_0).add_timeline_event(
+                        event_factory
+                            .room_tombstone("hello", room_id_1)
+                            .room(room_id_0)
+                            .event_id(tombstone_event_id),
+                    ),
+                )
+                // creating room 1, tombstoning room 0
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_1).add_timeline_event(
+                        event_factory
+                            .create(sender, RoomVersionId::try_from("42").unwrap())
+                            .predecessor(room_id_0, tombstone_event_id),
+                    ),
+                )
+                .build_sync_response();
+
+            assert!(client.receive_sync_response(response).await.is_ok());
+
+            let room_0 = client.get_room(room_id_0).unwrap();
+
+            assert!(room_0.predecessor_room().is_none(), "room 0 must not have a predecessor");
+            assert_eq!(
+                room_0.successor_room().expect("room 0 must have a successor").room_id,
+                room_id_1,
+                "room 0 does not have the expected successor",
+            );
+
+            let room_1 = client.get_room(room_id_1).unwrap();
+
+            assert_eq!(
+                room_1.predecessor_room().expect("room 1 must have a predecessor").room_id,
+                room_id_0,
+                "room 1 does not have the expected predecessor",
+            );
+            assert!(room_1.successor_room().is_none(), "room 1 must not have a successor");
+        }
+
+        // Room 1 is being tombstoned by room 2.
+        {
+            let tombstone_event_id = event_id!("$ev1");
+            let response = response_builder
+                // tombstoning room 1 with room 2
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_1).add_timeline_event(
+                        event_factory
+                            .room_tombstone("hello", room_id_2)
+                            .room(room_id_1)
+                            .event_id(tombstone_event_id),
+                    ),
+                )
+                // creating room 2, tombstoning room 1
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_2).add_timeline_event(
+                        event_factory
+                            .create(sender, RoomVersionId::try_from("43").unwrap())
+                            .predecessor(room_id_1, tombstone_event_id),
+                    ),
+                )
+                .build_sync_response();
+
+            assert!(client.receive_sync_response(response).await.is_ok());
+
+            let room_1 = client.get_room(room_id_1).unwrap();
+
+            assert_eq!(
+                room_1.predecessor_room().expect("room 1 must have a predecessor").room_id,
+                room_id_0,
+                "room 1 does not have the expected predecessor",
+            );
+            assert_eq!(
+                room_1.successor_room().expect("room 1 must have a successor").room_id,
+                room_id_2,
+                "room 1 does not have the expected successor",
+            );
+
+            let room_2 = client.get_room(room_id_2).unwrap();
+
+            assert_eq!(
+                room_2.predecessor_room().expect("room 2 must have a predecessor").room_id,
+                room_id_1,
+                "room 2 does not have the expected predecessor",
+            );
+            assert!(room_2.successor_room().is_none(), "room 2 must not have a successor");
+        }
+    }
+
+    /// This test is ensuring [`check_tombstone`] detects the smallest loop
+    /// possible in tombstone: room 0 is tombstoning itself and is replacing
+    /// itself.
+    ///
+    /// ```text
+    /// m.room.tombstone
+    /// replaced by room 0
+    /// ┌──────────────┐
+    /// │              │
+    /// │  ┌────────┐  │
+    /// └──┤ room 0 ◄──┘
+    ///    └────────┘
+    /// ```
+    #[async_test]
+    async fn test_check_tombstone_shortest_loop() {
+        let sender = user_id!("@mnt_io:matrix.org");
+        let event_factory = EventFactory::new().sender(sender);
+        let mut response_builder = SyncResponseBuilder::new();
+        let room_id_0 = room_id!("!r0");
+
+        let client = logged_in_base_client(None).await;
+
+        // Create room 0.
+        {
+            let response = response_builder
+                .add_joined_room(JoinedRoomBuilder::new(room_id_0))
+                .build_sync_response();
+
+            assert!(client.receive_sync_response(response).await.is_ok());
+
+            let room_0 = client.get_room(room_id_0).unwrap();
+
+            assert!(room_0.predecessor_room().is_none());
+            assert!(room_0.successor_room().is_none());
+        }
+
+        // Room 0 is being tombstoned by room 0.
+        {
+            let tombstone_event_id = event_id!("$ev0");
+            let response = response_builder
+                // tombstoning room 0 with room 0
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_0).add_timeline_event(
+                        event_factory
+                            .room_tombstone("hello", room_id_0)
+                            .room(room_id_0)
+                            .event_id(tombstone_event_id),
+                    ),
+                )
+                .build_sync_response();
+
+            assert_matches!(
+                client.receive_sync_response(response).await,
+                Err(Error::InconsistentTombstonedRooms { room_in_path }) => {
+                    assert_eq!(room_in_path, room_id_0);
+                }
+            );
+        }
+    }
+
+    /// This test is ensuring [`check_tombstone`] detects loops involving the
+    /// state store and the room updates.
+    ///
+    /// ```text
+    ///     m.room.tombstone     m.room.tombstone
+    ///     replaced by room 1   replaced by room 2
+    ///     ┌─────────────────┐  ┌─────────────────┐
+    ///     │                 │  │                 │
+    /// ┌────────┐         ┌──▼──┴──┐         ┌────▼───┐
+    /// │ room 0 │         │ room 1 │         │ room 2 │
+    /// └───▲────┘         └────────┘         └────┬───┘
+    ///     │                                      │
+    ///     └──────────────────────────────────────┘
+    ///                 m.room.tomsbone
+    ///                 replaced by room 0
+    /// ```
+    #[async_test]
+    async fn test_check_tombstone_loop() {
+        let sender = user_id!("@mnt_io:matrix.org");
+        let event_factory = EventFactory::new().sender(sender);
+        let mut response_builder = SyncResponseBuilder::new();
+        let room_id_0 = room_id!("!r0");
+        let room_id_1 = room_id!("!r1");
+        let room_id_2 = room_id!("!r2");
+
+        let client = logged_in_base_client(None).await;
+
+        // Create room 0.
+        {
+            let response = response_builder
+                .add_joined_room(JoinedRoomBuilder::new(room_id_0))
+                .build_sync_response();
+
+            assert!(client.receive_sync_response(response).await.is_ok());
+
+            let room_0 = client.get_room(room_id_0).unwrap();
+
+            assert!(room_0.predecessor_room().is_none());
+            assert!(room_0.successor_room().is_none());
+        }
+
+        // Room 0 is being tombstoned by room 1.
+        {
+            let tombstone_event_id = event_id!("$ev0");
+            let response = response_builder
+                // tombstoning room 0 with room 1
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_0).add_timeline_event(
+                        event_factory
+                            .room_tombstone("hello", room_id_1)
+                            .room(room_id_0)
+                            .event_id(tombstone_event_id),
+                    ),
+                )
+                // creating room 1, tombstoning room 0
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_1).add_timeline_event(
+                        event_factory
+                            .create(sender, RoomVersionId::try_from("42").unwrap())
+                            .predecessor(room_id_0, tombstone_event_id),
+                    ),
+                )
+                .build_sync_response();
+
+            assert!(client.receive_sync_response(response).await.is_ok());
+
+            let room_0 = client.get_room(room_id_0).unwrap();
+
+            assert!(room_0.predecessor_room().is_none(), "room 0 must not have a predecessor");
+            assert_eq!(
+                room_0.successor_room().expect("room 0 must have a successor").room_id,
+                room_id_1,
+                "room 0 does not have the expected successor",
+            );
+
+            let room_1 = client.get_room(room_id_1).unwrap();
+
+            assert_eq!(
+                room_1.predecessor_room().expect("room 1 must have a predecessor").room_id,
+                room_id_0,
+                "room 1 does not have the expected predecessor",
+            );
+            assert!(room_1.successor_room().is_none(), "room 1 must not have a successor");
+        }
+
+        // Room 1 is being tombstoned by room 2, but room 2 is tombstoned by room 0!
+        {
+            let tombstone_event_id = event_id!("$ev1");
+            let response = response_builder
+                // tombstoning room 1 with room 2
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_1).add_timeline_event(
+                        event_factory
+                            .room_tombstone("hello", room_id_2)
+                            .room(room_id_1)
+                            .event_id(tombstone_event_id),
+                    ),
+                )
+                // creating room 2, tombstoning room 1, but also tombstoned by room 0
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_2)
+                        .add_timeline_event(
+                            event_factory
+                                .create(sender, RoomVersionId::try_from("43").unwrap())
+                                .predecessor(room_id_1, tombstone_event_id),
+                        )
+                        .add_timeline_event(
+                            event_factory
+                                .room_tombstone("hehe", room_id_0)
+                                .room(room_id_2)
+                                .event_id(event_id!("$ev_foo")),
+                        ),
+                )
+                .build_sync_response();
+
+            assert_matches!(
+                client.receive_sync_response(response).await,
+                Err(Error::InconsistentTombstonedRooms { room_in_path }) => {
+                    assert_eq!(room_in_path, room_id_1);
+                }
+            );
+        }
+    }
+
+    /// This test is ensuring [`check_tombstone`] detects a merger pattern in
+    /// tombstoned room, i.e. when two rooms are being replaced by the same
+    /// room.
+    ///
+    /// ```text
+    ///      m.room.tombstone
+    ///      replaced by room 2
+    ///      ┌──────────────┐
+    ///      │              │
+    /// ┌────┴───┐          │
+    /// │ room 0 │          │
+    /// └────────┘     ┌────▼───┐
+    ///                │ room 2 │
+    /// ┌────────┐     └────▲───┘
+    /// │ room 1 │          │
+    /// └────┬───┘          │
+    ///      │              │
+    ///      └──────────────┘
+    ///      m.room.tombstone
+    ///      replaced by room 2
+    /// ```
+    #[async_test]
+    async fn test_check_tombstone_merger() {
+        let sender = user_id!("@mnt_io:matrix.org");
+        let event_factory = EventFactory::new().sender(sender);
+        let mut response_builder = SyncResponseBuilder::new();
+        let room_id_0 = room_id!("!r0");
+        let room_id_1 = room_id!("!r1");
+        let room_id_2 = room_id!("!r2");
+
+        let client = logged_in_base_client(None).await;
+
+        // Create room 0 and room 1.
+        {
+            let response = response_builder
+                .add_joined_room(JoinedRoomBuilder::new(room_id_0))
+                .add_joined_room(JoinedRoomBuilder::new(room_id_1))
+                .build_sync_response();
+
+            assert!(client.receive_sync_response(response).await.is_ok());
+
+            let room_0 = client.get_room(room_id_0).unwrap();
+
+            assert!(room_0.predecessor_room().is_none());
+            assert!(room_0.successor_room().is_none());
+
+            let room_1 = client.get_room(room_id_1).unwrap();
+
+            assert!(room_1.predecessor_room().is_none());
+            assert!(room_1.successor_room().is_none());
+        }
+
+        // Room 0 and room 1 are both being tombstoned by room 2.
+        {
+            let tombstone_event_id_for_room_0 = event_id!("$ev0");
+            let tombstone_event_id_for_room_1 = event_id!("$ev1");
+            let response = response_builder
+                // tombstoning room 0 with room 2
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_0).add_timeline_event(
+                        event_factory
+                            .room_tombstone("hello", room_id_2)
+                            .room(room_id_0)
+                            .event_id(tombstone_event_id_for_room_0),
+                    ),
+                )
+                // tombstoning room 1 with room 2
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_1).add_timeline_event(
+                        event_factory
+                            .room_tombstone("hello", room_id_2)
+                            .room(room_id_1)
+                            .event_id(tombstone_event_id_for_room_1),
+                    ),
+                )
+                // creating room 2, tombstoning room 0 (it's not possible to succeed to 2 rooms)
+                .add_joined_room(
+                    JoinedRoomBuilder::new(room_id_2).add_timeline_event(
+                        event_factory
+                            .create(sender, RoomVersionId::try_from("42").unwrap())
+                            .predecessor(room_id_0, tombstone_event_id_for_room_0),
+                    ),
+                )
+                .build_sync_response();
+
+            assert_matches!(
+                client.receive_sync_response(response).await,
+                Err(Error::InconsistentTombstonedRooms { room_in_path }) => {
+                    assert_eq!(room_in_path, room_id_2);
+                }
+            );
+        }
+    }
 
     #[async_test]
     async fn test_state_events_after_sync() {
