8a4e6d6029d06c7df313036f380ccc86b5d3f1e7
Daniel Salinas <danielsalinas@daniels-mbp-2.myfiosgateway.com>
2025-06-03 09:29:37 -0400

    Send/Sync bounds

--- crates/matrix-sdk-common/src/deserialized_responses.rs
+++ crates/matrix-sdk-common/src/deserialized_responses.rs
@@ -34,6 +34,8 @@ use crate::{
     debug::{DebugRawEvent, DebugStructExt},
     serde_helpers::extract_bundled_thread_summary,
 };
+#[cfg(feature = "test-send-sync")]
+use crate::{SendOutsideWasm, SyncOutsideWasm};
 
 const AUTHENTICITY_NOT_GUARANTEED: &str =
     "The authenticity of this encrypted message can't be guaranteed on this device.";
