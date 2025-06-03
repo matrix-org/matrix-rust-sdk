977cf0faa21ddfdf04ee110fbc274e57d1fd1cad
Daniel Salinas <danielsalinas@daniels-mbp-2.myfiosgateway.com>
2025-06-03 10:54:01 -0400

    Remove unused dep

--- bindings/matrix-sdk-ffi/src/client.rs
+++ bindings/matrix-sdk-ffi/src/client.rs
@@ -41,7 +41,7 @@ use matrix_sdk::{
     AuthApi, AuthSession, Client as MatrixClient, SessionChange, SessionTokens,
     STATE_STORE_DATABASE_NAME,
 };
-use matrix_sdk_common::{stream::StreamExt, AsyncTraitDeps, SendOutsideWasm, SyncOutsideWasm};
+use matrix_sdk_common::{stream::StreamExt, SendOutsideWasm, SyncOutsideWasm};
 use matrix_sdk_ui::{
     notification_client::{
         NotificationClient as MatrixNotificationClient,
