e076373d849e71d9a4146e16c05832d641e44be2
Michael Goldenberg <m@mgoldenberg.net>
2025-06-04 10:36:02 -0400

    fixup! refactor(indexeddb): add internal types for event cache store
    Signed-off-by: Michael Goldenberg <m@mgoldenberg.net>

--- crates/matrix-sdk-indexeddb/src/event_cache_store/types.rs
+++ crates/matrix-sdk-indexeddb/src/event_cache_store/types.rs
@@ -1,69 +1,66 @@
-use matrix_sdk_base::{
-    deserialized_responses::TimelineEvent,
-    linked_chunk::{ChunkIdentifier, Position},
-};
+use matrix_sdk_base::{deserialized_responses::TimelineEvent, linked_chunk::ChunkIdentifier};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ChunkForCache {
+pub struct Chunk {
     pub identifier: u64,
-    pub previous: Option<Box<ChunkForCache>>,
-    pub next: Option<Box<ChunkForCache>>,
-    pub chunk_type: ChunkTypeForCache,
+    pub previous: Option<Box<Chunk>>,
+    pub next: Option<Box<Chunk>>,
+    pub chunk_type: ChunkType,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
-pub enum ChunkTypeForCache {
+pub enum ChunkType {
     Event,
     Gap,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(untagged)]
-pub enum EventForCache {
-    InBand(InBandEventForCache),
-    OutOfBand(OutOfBandEventForCache),
+pub enum Event {
+    InBand(InBandEvent),
+    OutOfBand(OutOfBandEvent),
 }
 
-impl From<EventForCache> for TimelineEvent {
-    fn from(value: EventForCache) -> Self {
+impl From<Event> for TimelineEvent {
+    fn from(value: Event) -> Self {
         match value {
-            EventForCache::InBand(e) => e.content,
-            EventForCache::OutOfBand(e) => e.content,
+            Event::InBand(e) => e.content,
+            Event::OutOfBand(e) => e.content,
         }
     }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct GenericEventForCache<P> {
+pub struct GenericEvent<P> {
     pub content: TimelineEvent,
     pub room_id: String,
     pub position: P,
 }
 
-pub type InBandEventForCache = GenericEventForCache<PositionForCache>;
-pub type OutOfBandEventForCache = GenericEventForCache<()>;
+pub type InBandEvent = GenericEvent<Position>;
+pub type OutOfBandEvent = GenericEvent<()>;
 
 #[derive(Debug, Default, Copy, Clone, Serialize, Deserialize)]
-pub struct PositionForCache {
+pub struct Position {
     pub chunk_identifier: u64,
     pub index: usize,
 }
 
-impl From<PositionForCache> for Position {
-    fn from(value: PositionForCache) -> Self {
+impl From<Position> for matrix_sdk_base::linked_chunk::Position {
+    fn from(value: Position) -> Self {
         Self::new(ChunkIdentifier::new(value.chunk_identifier), value.index)
     }
 }
 
-impl From<Position> for PositionForCache {
-    fn from(value: Position) -> Self {
+impl From<matrix_sdk_base::linked_chunk::Position> for Position {
+    fn from(value: matrix_sdk_base::linked_chunk::Position) -> Self {
         Self { chunk_identifier: value.chunk_identifier().index(), index: value.index() }
     }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct GapForCache {
+pub struct Gap {
     pub prev_token: String,
 }
