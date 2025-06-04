75ea5b482d965b65165878856a29ed6cfcb21e22
Benjamin Bouvier <benjamin@bouvier.cc>
2025-06-04 17:11:55 +0200

    refactor(event cache): make deduplication entirely stateless
    Having a small data structure to hold the room id and store isn't that
    useful, after all.

--- crates/matrix-sdk/src/event_cache/room/mod.rs
+++ crates/matrix-sdk/src/event_cache/room/mod.rs
@@ -565,14 +565,11 @@ mod private {
     use tracing::{debug, error, instrument, trace, warn};
 
     use super::{
-        super::{
-            deduplicator::{DeduplicationOutcome, Deduplicator},
-            EventCacheError,
-        },
+        super::{deduplicator::DeduplicationOutcome, EventCacheError},
         events::RoomEvents,
         sort_positions_descending, EventLocation, LoadMoreEventsBackwardsOutcome,
     };
-    use crate::event_cache::RoomPaginationStatus;
+    use crate::event_cache::{deduplicator::filter_duplicate_events, RoomPaginationStatus};
 
     /// State for a single room's event cache.
     ///
@@ -590,9 +587,6 @@ mod private {
 
         /// The events of the room.
         events: RoomEvents,
-
-        /// The events deduplicator instance to help finding duplicates.
-        deduplicator: Deduplicator,
 
         /// Have we ever waited for a previous-batch-token to come from sync, in
         /// the context of pagination? We do this at most once per room,
@@ -647,14 +641,12 @@ mod private {
             };
 
             let events = RoomEvents::with_initial_linked_chunk(linked_chunk);
-            let deduplicator = Deduplicator::new(room_id.clone(), store.clone());
 
             Ok(Self {
                 room: room_id,
                 room_version,
                 store,
                 events,
-                deduplicator,
                 waited_for_initial_prev_token: false,
                 listener_count: Default::default(),
                 pagination_status,
@@ -692,7 +684,7 @@ mod private {
             events: Vec<Event>,
         ) -> Result<(DeduplicationOutcome, bool), EventCacheError> {
             let deduplication_outcome =
-                self.deduplicator.filter_duplicate_events(events, &self.events).await?;
+                filter_duplicate_events(&self.room, &self.store, events, &self.events).await?;
 
             let number_of_events = deduplication_outcome.all_events.len();
             let number_of_deduplicated_events =
