42cee8c6fc948d59015aa4f661691a8f3ceaa7c4
Benjamin Bouvier <benjamin@bouvier.cc>
2025-06-04 18:51:29 +0200

    refactor(event cache): only mark that we've waited for an initial previous-batch token after a sync
    It doesn't make sense to do it after a back-pagination, since a
    back-pagination does require a previous-batch token in the first place,
    meaning that if we did paginate, then we did wait for a previous-batch
    token beforehand.

--- crates/matrix-sdk/src/event_cache/room/mod.rs
+++ crates/matrix-sdk/src/event_cache/room/mod.rs
@@ -1098,14 +1098,6 @@ mod private {
                 }
             }
 
-            // If we've never waited for an initial previous-batch token, and we now have at
-            // least one gap in the chunk, no need to wait for a previous-batch token later.
-            if !self.waited_for_initial_prev_token
-                && self.events.chunks().any(|chunk| chunk.is_gap())
-            {
-                self.waited_for_initial_prev_token = true;
-            }
-
             Ok(())
         }
 
@@ -1382,6 +1374,12 @@ mod private {
                 events
             })
             .await?;
+
+            // If we've never waited for an initial previous-batch token, and we've now
+            // inserted a gap, no need to wait for a previous-batch token later.
+            if !self.waited_for_initial_prev_token && prev_batch.is_some() {
+                self.waited_for_initial_prev_token = true;
+            }
 
             if timeline.limited && prev_batch.is_some() {
                 // If there was a previous batch token for a limited timeline, unload the chunks
