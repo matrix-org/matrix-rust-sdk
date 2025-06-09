922ea91603ac7e58bd0c7b1347089fa4a1ea33a0
Michael Goldenberg <m@mgoldenberg.net>
2025-06-09 00:11:04 -0400

    refactor(indexeddb): support querying by next chunk index, even when next chunk does not exist
    Signed-off-by: Michael Goldenberg <m@mgoldenberg.net>

--- crates/matrix-sdk-indexeddb/src/event_cache_store/serializer/types.rs
+++ crates/matrix-sdk-indexeddb/src/event_cache_store/serializer/types.rs
@@ -39,8 +39,9 @@ pub struct IndexedChunk {
     /// The primary key of the object store.
     pub id: IndexedChunkIdKey,
     /// An indexed key on the object store, which represents the
-    /// [`IndexedChunkIdKey`] of the next chunk in the linked list.
-    pub next: IndexedChunkIdKey,
+    /// [`IndexedChunkIdKey`] of the next chunk in the linked list, if it
+    /// exists.
+    pub next: IndexedNextChunkIdKey,
     /// The (possibly) encrypted content of the chunk.
     pub content: IndexedChunkContent,
 }
@@ -58,6 +59,30 @@ pub struct IndexedChunkIdKey(IndexedRoomId, IndexedChunkId);
 pub type IndexedRoomId = String;
 pub type IndexedChunkId = u64;
 pub type IndexedChunkContent = MaybeEncrypted;
+
+/// The value associated with the [`next`](IndexedChunk::next) index of the
+/// [`LINKED_CHUNKS`][1] object store, which is constructed from:
+///
+/// - The (possibly) encrypted Room ID
+/// - The Chunk ID, if there is a next chunk in the list.
+///
+/// Note: it would be more convenient to represent this type with an optional
+/// Chunk ID, but unfortunately, this creates an issue when querying for objects
+/// that don't have a `next` value, because `None` serializes to `null` which
+/// is an invalid value in any part of an IndexedDB query.
+///
+/// Furthermore, each variant must serialize to the same type, so the `None`
+/// variant must contain a non-empty tuple.
+///
+/// [1]: crate::event_cache_store::migrations::v1::create_linked_chunks_object_store
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum IndexedNextChunkIdKey {
+    /// There is no next chunk.
+    None((IndexedRoomId,)),
+    /// The identifier of the next chunk in the list.
+    Some(IndexedChunkIdKey),
+}
 
 /// Represents the [`EVENTS`][1] object store.
 ///
