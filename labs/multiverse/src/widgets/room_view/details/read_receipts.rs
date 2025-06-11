45b6daff110c5cb0ef5d9e92df95f3823e37440e
Damir Jelić <poljar@termina.org.uk>
2025-06-11 17:26:54 +0200

    feat(multiverse): Only render read receipts for a single event if one is selected

--- labs/multiverse/src/widgets/room_view/details/read_receipts.rs
+++ labs/multiverse/src/widgets/room_view/details/read_receipts.rs
@@ -1,3 +1,4 @@
+use matrix_sdk::{ruma::EventId, Room};
 use matrix_sdk_base::read_receipts::RoomReadReceipts;
 use matrix_sdk_ui::timeline::TimelineItem;
 use ratatui::{
@@ -17,62 +18,63 @@ impl<'a> ReadReceipts<'a> {
     }
 }
 
+fn render_room_stats(room: &Room, area: Rect, buf: &mut Buffer) {
+    let RoomReadReceipts { num_unread, num_notifications, num_mentions, .. } = room.read_receipts();
+
+    let content = vec![
+        Line::from(format!("- unread: {num_unread}")),
+        Line::from(format!("- notifications: {num_notifications}")),
+        Line::from(format!("- mentions: {num_mentions}")),
+        Line::from(""),
+        Line::from("---"),
+        Line::from(format!("{:?}", room.read_receipts())),
+        Line::from("#"),
+    ];
+
+    Paragraph::new(content)
+        .fg(TEXT_COLOR)
+        .wrap(Wrap { trim: false })
+        .block(Block::new().borders(Borders::BOTTOM))
+        .render(area, buf);
+}
+
+fn find_selected_event(item: &TimelineItem, selected_event: &EventId) -> bool {
+    match item.as_event() {
+        Some(event) => event.event_id() == Some(selected_event),
+        None => false,
+    }
+}
+
 impl Widget for &mut ReadReceipts<'_> {
     fn render(self, area: Rect, buf: &mut Buffer)
     where
         Self: Sized,
     {
         match self.state.selected_room {
-            Some(room) => {
-                let RoomReadReceipts { num_unread, num_notifications, num_mentions, .. } =
-                    room.read_receipts();
-
-                let vertical = Layout::vertical([Constraint::Length(8), Constraint::Fill(1)]);
-                let [top, bottom] = vertical.areas(area);
-
-                let content = vec![
-                    Line::from(format!("- unread: {num_unread}")),
-                    Line::from(format!("- notifications: {num_notifications}")),
-                    Line::from(format!("- mentions: {num_mentions}")),
-                    Line::from(""),
-                    Line::from("---"),
-                    Line::from(format!("{:?}", room.read_receipts())),
-                    Line::from("#"),
-                ];
-
-                Paragraph::new(content)
-                    .fg(TEXT_COLOR)
-                    .wrap(Wrap { trim: false })
-                    .block(Block::new().borders(Borders::BOTTOM))
-                    .render(top, buf);
-
-                if let Some(items) = self.state.timeline_items {
-                    let mut list_items = Vec::new();
-                    let mut selected_item = None;
-
-                    for (index, item) in items.into_iter().enumerate() {
-                        let Some(event) = item.as_event() else { continue };
-
-                        if event.event_id() == self.state.selected_event.as_deref() {
-                            selected_item = Some(index);
-                        }
-
-                        let Some(list_item) = format_timeline_item(item) else {
-                            continue;
-                        };
-
-                        list_items.push(list_item);
-                    }
-
-                    let list = List::new(list_items)
-                        .highlight_spacing(HighlightSpacing::Always)
-                        .highlight_symbol(">")
-                        .highlight_style(SELECTED_STYLE_FG);
-
-                    let mut state = ListState::default().with_selected(selected_item);
-                    StatefulWidget::render(list, bottom, buf, &mut state);
+            Some(room) => match (self.state.timeline_items, self.state.selected_event.as_deref()) {
+                (None, None) | (None, Some(_)) | (Some(_), None) => {
+                    render_room_stats(room, area, buf)
                 }
-            }
+                (Some(items), Some(selected_event)) => {
+                    let item =
+                        items.into_iter().find(|&item| find_selected_event(item, &selected_event));
+
+                    if let Some(item) = item.map(|i| format_timeline_item(i)).flatten() {
+                        let second_item = ListItem::from(selected_event.to_string());
+
+                        let list = List::new(vec![second_item, item])
+                            .highlight_spacing(HighlightSpacing::Always)
+                            .highlight_symbol(">")
+                            .highlight_style(SELECTED_STYLE_FG);
+
+                        let mut state = ListState::default();
+
+                        StatefulWidget::render(list, area, buf, &mut state);
+                    } else {
+                        render_room_stats(room, area, buf);
+                    }
+                }
+            },
             None => {
                 let content = "(room disappeared in the room list service)";
                 Paragraph::new(content).fg(TEXT_COLOR).wrap(Wrap { trim: false }).render(area, buf);
