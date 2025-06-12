a99df7e1d82e9f740600b786f1fedc802eb6284e
Michael Goldenberg <m@mgoldenberg.net>
2025-06-12 12:26:36 -0400

    refactor(indexeddb): add indexing trait impls for chunk
    Signed-off-by: Michael Goldenberg <m@mgoldenberg.net>

--- crates/matrix-sdk-indexeddb/src/event_cache_store/migrations.rs
+++ crates/matrix-sdk-indexeddb/src/event_cache_store/migrations.rs
@@ -109,6 +109,7 @@ pub mod v1 {
 
     pub mod keys {
         pub const CORE: &str = "core";
+        pub const ROOMS: &str = "rooms";
         pub const LINKED_CHUNKS: &str = "linked_chunks";
         pub const LINKED_CHUNKS_KEY_PATH: &str = "id";
         pub const LINKED_CHUNKS_NEXT: &str = "linked_chunks_next";
