72850849e20ce56b2aaa1dbaf48be9861718d329
Yorusaka Miyabi <23130178+ShadowRZ@users.noreply.github.com>
2025-06-12 21:07:34 +0800

    fixup! feat(ffi): Expose legacy SSO support infomation

--- bindings/matrix-sdk-ffi/src/client.rs
+++ bindings/matrix-sdk-ffi/src/client.rs
@@ -335,16 +335,32 @@ impl Client {
             }
         };
 
-        let supports_password_login = self.supports_password_login().await.ok().unwrap_or(false);
-        let supports_sso_login = self.supports_sso_login().await.ok().unwrap_or(false);
+        let login_types = self.inner.matrix_auth().get_login_types().await.ok();
+        let supports_password_login = login_types
+            .as_ref()
+            .map(|login_types| {
+                login_types.flows.iter().any(|login_type| {
+                    matches!(login_type, get_login_types::v3::LoginType::Password(_))
+                })
+            })
+            .unwrap_or(false);
+        let supports_sso_login = login_types
+            .as_ref()
+            .map(|login_types| {
+                login_types
+                    .flows
+                    .iter()
+                    .any(|login_type| matches!(login_type, get_login_types::v3::LoginType::Sso(_)))
+            })
+            .unwrap_or(false);
         let sliding_sync_version = self.sliding_sync_version();
 
         Arc::new(HomeserverLoginDetails {
             url: self.homeserver(),
             sliding_sync_version,
             supports_oidc_login,
-            supports_sso_login,
             supported_oidc_prompts,
+            supports_sso_login,
             supports_password_login,
         })
     }
@@ -732,29 +748,6 @@ impl Client {
     /// functions makes it possible to force reset it.
     pub async fn reset_server_capabilities(&self) -> Result<(), ClientError> {
         Ok(self.inner.reset_server_capabilities().await?)
-    }
-}
-
-impl Client {
-    /// Whether or not the client's homeserver supports the password login flow.
-    pub(crate) async fn supports_password_login(&self) -> anyhow::Result<bool> {
-        let login_types = self.inner.matrix_auth().get_login_types().await?;
-        let supports_password = login_types
-            .flows
-            .iter()
-            .any(|login_type| matches!(login_type, get_login_types::v3::LoginType::Password(_)));
-        Ok(supports_password)
-    }
-
-    /// Whether or not the client's homeserver supports the legacy SSO login
-    /// flow.
-    pub(crate) async fn supports_sso_login(&self) -> anyhow::Result<bool> {
-        let login_types = self.inner.matrix_auth().get_login_types().await?;
-        let supports_password = login_types
-            .flows
-            .iter()
-            .any(|login_type| matches!(login_type, get_login_types::v3::LoginType::Sso(_)));
-        Ok(supports_password)
     }
 }
 
