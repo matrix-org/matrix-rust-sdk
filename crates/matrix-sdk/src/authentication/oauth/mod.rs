299bae9e946e89c5eac0a968cab106b66ff815ea
fl0lli <github@fl0lli.de>
2025-07-14 17:34:31 +0200

    docs(sdk): improve doc for additional_scopes param for OAuth.login
    Signed-off-by: fl0lli <github@fl0lli.de>

--- crates/matrix-sdk/src/authentication/oauth/mod.rs
+++ crates/matrix-sdk/src/authentication/oauth/mod.rs
@@ -910,8 +910,10 @@ impl OAuth {
     ///   was called previously.
     ///
     /// * `additional_scopes` - Additional scopes to request from the
-    ///   authorization server. If not set, only the scopes for API access and
-    ///   the device ID will be requested.
+    ///   authorization server, e.g. "urn:matrix:client:com.example.msc9999.foo". 
+    ///   The scopes for API access and the device ID according to the
+    ///   [specification](https://spec.matrix.org/v1.15/client-server-api/#allocated-scope-tokens) 
+    ///   are always requested.
     ///
     /// # Example
     ///
