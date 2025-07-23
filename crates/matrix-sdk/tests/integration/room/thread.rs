248f8d847f06b2ae0b721ee7a2a3193ccad79c3f
Benjamin Bouvier <benjamin@bouvier.cc>
2025-07-23 18:55:28 +0200

    test(threads): add test for the thread subscription endpoints

---
+++ crates/matrix-sdk/tests/integration/room/thread.rs
@@ -0,0 +1,57 @@
+use matrix_sdk::test_utils::mocks::MatrixMockServer;
+use matrix_sdk_test::async_test;
+use ruma::{owned_event_id, room_id};
+
+#[async_test]
+async fn test_subscribe_thread() {
+    let server = MatrixMockServer::new().await;
+    let client = server.client_builder().build().await;
+
+    let room_id = room_id!("!test:example.org");
+    let room = server.sync_joined_room(&client, room_id).await;
+
+    let root_id = owned_event_id!("$root");
+
+    server
+        .mock_put_thread_subscription()
+        .match_room_id(room_id.to_owned())
+        .match_thread_id(root_id.clone())
+        .ok()
+        .mock_once()
+        .mount()
+        .await;
+
+    // I can subscribe to a thread.
+    room.subscribe_thread(root_id.clone(), true).await.unwrap();
+
+    server
+        .mock_get_thread_subscription()
+        .match_room_id(room_id.to_owned())
+        .match_thread_id(root_id.clone())
+        .ok(true)
+        .mock_once()
+        .mount()
+        .await;
+
+    // I can get the subscription status for that same thread.
+    let subscription = room.fetch_thread_subscription(root_id.clone()).await.unwrap().unwrap();
+    assert!(subscription.automatic);
+
+    // If I try to get a subscription for a thread event that's unknown, I get no
+    // `ThreadSubscription`, not an error.
+    let subscription =
+        room.fetch_thread_subscription(owned_event_id!("$another_root")).await.unwrap();
+    assert!(subscription.is_none());
+
+    // I can also unsubscribe from a thread.
+    server
+        .mock_delete_thread_subscription()
+        .match_room_id(room_id.to_owned())
+        .match_thread_id(root_id.clone())
+        .ok()
+        .mock_once()
+        .mount()
+        .await;
+
+    room.unsubscribe_thread(root_id).await.unwrap();
+}
