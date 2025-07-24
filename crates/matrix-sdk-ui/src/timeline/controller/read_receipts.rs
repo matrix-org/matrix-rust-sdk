d32c18771f2931ec019849b5a9d32ee4109ddc33
Stefan Ceriu <stefanc@matrix.org>
2025-07-24 13:16:41 +0300

    feat(ui): consider unthreaded read receipts when computing the user's latest on thread aware timeline instances
    This change modifies the existing `latest_user_read_receipt` method to retrieve and use unthreaded read receipts and chosing the most recent one.
    We do this in order improve compatibility with non-threaded clients and correctly show what the other party has read on flattened out timelines.

--- crates/matrix-sdk-ui/src/timeline/controller/read_receipts.rs
+++ crates/matrix-sdk-ui/src/timeline/controller/read_receipts.rs
@@ -710,7 +710,7 @@ impl<P: RoomDataProvider> TimelineState<P> {
     ) -> Option<(OwnedEventId, Receipt)> {
         let all_remote_events = self.items.all_remote_events();
 
-        let public_read_receipt = self
+        let threaded_public_read_receipt = self
             .meta
             .user_receipt(
                 user_id,
@@ -721,16 +721,66 @@ impl<P: RoomDataProvider> TimelineState<P> {
             )
             .await;
 
-        let private_read_receipt = self
+        // If the current client is thread aware, we maintain maximal
+        // compatibility with other clients using either unthreaded or
+        // main-thread read receipts by allowing both here and taking the most
+        // recent one.
+        let unthreaded_public_read_receipt = self
             .meta
             .user_receipt(
                 user_id,
-                ReceiptType::ReadPrivate,
-                receipt_thread,
+                ReceiptType::Read,
+                ReceiptThread::Unthreaded,
                 room_data_provider,
                 all_remote_events,
             )
             .await;
+
+        let public_read_receipt = match self.meta.compare_optional_receipts(
+            threaded_public_read_receipt.as_ref(),
+            unthreaded_public_read_receipt.as_ref(),
+            all_remote_events,
+        ) {
+            Ordering::Greater => threaded_public_read_receipt,
+            Ordering::Less => unthreaded_public_read_receipt,
+            _ => unreachable!(),
+        };
+
+        let threaded_private_read_receipt = self
+            .meta
+            .user_receipt(
+                user_id,
+                ReceiptType::ReadPrivate,
+                receipt_thread.clone(),
+                room_data_provider,
+                all_remote_events,
+            )
+            .await;
+
+        // If the current client is thread aware, we maintain maximal
+        // compatibility with other clients using either unthreaded or
+        // main-thread read receipts by allowing both here and taking the most
+        // recent one.
+        let unthreaded_private_read_receipt = self
+            .meta
+            .user_receipt(
+                user_id,
+                ReceiptType::ReadPrivate,
+                ReceiptThread::Unthreaded,
+                room_data_provider,
+                all_remote_events,
+            )
+            .await;
+
+        let private_read_receipt = match self.meta.compare_optional_receipts(
+            threaded_private_read_receipt.as_ref(),
+            unthreaded_private_read_receipt.as_ref(),
+            all_remote_events,
+        ) {
+            Ordering::Greater => threaded_private_read_receipt,
+            Ordering::Less => unthreaded_private_read_receipt,
+            _ => unreachable!(),
+        };
 
         // Let's assume that a private read receipt should be more recent than a public
         // read receipt (otherwise there's no point in the private read receipt),
