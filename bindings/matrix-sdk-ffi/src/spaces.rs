9ae579cf74e7c9d263ee169b1160ae599dd4e1e5
Stefan Ceriu <stefanc@matrix.org>
2025-08-04 17:07:30 +0300

    chore(spaces): introduce a `SpaceServiceRoom`

--- bindings/matrix-sdk-ffi/src/spaces.rs
+++ bindings/matrix-sdk-ffi/src/spaces.rs
@@ -12,10 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use matrix_sdk_ui::spaces::SpaceService as UISpaceService;
+use matrix_sdk_ui::spaces::{
+    SpaceService as UISpaceService, SpaceServiceRoom as UISpaceServiceRoom,
+};
 use ruma::RoomId;
 
-use crate::error::ClientError;
+use crate::{
+    client::JoinRule,
+    error::ClientError,
+    room::{Membership, RoomHero},
+    room_preview::RoomType,
+};
 
 #[derive(uniffi::Object)]
 pub struct SpaceService {
@@ -32,17 +39,53 @@ impl SpaceService {
 #[matrix_sdk_ffi_macros::export]
 impl SpaceService {
     /// Get the list of joined spaces.
-    pub fn joined_spaces(&self) -> Vec<String> {
-        self.inner.joined_spaces().into_iter().map(|id| id.to_string()).collect()
+    pub fn joined_spaces(&self) -> Vec<SpaceServiceRoom> {
+        self.inner.joined_spaces().into_iter().map(Into::into).collect()
     }
 
     /// Get the top-level children for a given space.
     pub async fn top_level_children_for(
         &self,
         space_id: String,
-    ) -> Result<Vec<String>, ClientError> {
+    ) -> Result<Vec<SpaceServiceRoom>, ClientError> {
         let space_id = RoomId::parse(space_id)?;
         let children = self.inner.top_level_children_for(space_id).await?;
-        Ok(children.into_iter().map(|id| id.to_string()).collect())
+        Ok(children.into_iter().map(Into::into).collect())
+    }
+}
+
+#[derive(uniffi::Record)]
+pub struct SpaceServiceRoom {
+    pub room_id: String,
+    pub canonical_alias: Option<String>,
+    pub name: Option<String>,
+    pub topic: Option<String>,
+    pub avatar_url: Option<String>,
+    pub room_type: RoomType,
+    pub num_joined_members: u64,
+    pub join_rule: Option<JoinRule>,
+    pub world_readable: Option<bool>,
+    pub guest_can_join: bool,
+
+    pub state: Option<Membership>,
+    pub heroes: Option<Vec<RoomHero>>,
+}
+
+impl From<UISpaceServiceRoom> for SpaceServiceRoom {
+    fn from(room: UISpaceServiceRoom) -> Self {
+        Self {
+            room_id: room.room_id.into(),
+            canonical_alias: room.canonical_alias.map(|alias| alias.into()),
+            name: room.name,
+            topic: room.topic,
+            avatar_url: room.avatar_url.map(|url| url.into()),
+            room_type: room.room_type.into(),
+            num_joined_members: room.num_joined_members,
+            join_rule: room.join_rule.map(Into::into),
+            world_readable: room.world_readable,
+            guest_can_join: room.guest_can_join,
+            state: room.state.map(Into::into),
+            heroes: room.heroes.map(|heroes| heroes.into_iter().map(Into::into).collect()),
+        }
     }
 }
