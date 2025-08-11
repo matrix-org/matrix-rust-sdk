c6dc070c31737145d9f320c71b17e305dea3e059
Doug <douglase@element.io>
2025-08-11 09:58:08 +0100

    chore: Refactor non_space.rs filter to space.rs, using new_filter_not in the FFI.

--- crates/matrix-sdk-ui/src/room_list_service/filters/space.rs
+++ crates/matrix-sdk-ui/src/room_list_service/filters/space.rs
@@ -14,26 +14,26 @@
 
 use super::{super::Room, Filter};
 
-struct NonSpaceRoomMatcher<F>
+struct SpaceRoomMatcher<F>
 where
     F: Fn(&Room) -> bool,
 {
-    is_not_space: F,
+    is_space: F,
 }
 
-impl<F> NonSpaceRoomMatcher<F>
+impl<F> SpaceRoomMatcher<F>
 where
     F: Fn(&Room) -> bool,
 {
     fn matches(&self, room: &Room) -> bool {
-        (self.is_not_space)(room)
+        (self.is_space)(room)
     }
 }
 
-/// Create a new filter that will filter out rooms that are not spaces, i.e.
+/// Create a new filter that will filter out rooms that are spaces, i.e.
 /// room with a `room_type` of `m.space` as defined in <https://spec.matrix.org/latest/client-server-api/#spaces>
 pub fn new_filter() -> impl Filter {
-    let matcher = NonSpaceRoomMatcher { is_not_space: move |room| !room.is_space() };
+    let matcher = SpaceRoomMatcher { is_space: move |room| room.is_space() };
 
     move |room| -> bool { matcher.matches(room) }
 }
@@ -47,14 +47,14 @@ mod tests {
     use super::{super::new_rooms, *};
 
     #[async_test]
-    async fn test_not_space() {
+    async fn test_space() {
         let (client, server) = logged_in_client_with_server().await;
         let [room] = new_rooms([room_id!("!a:b.c")], &client, &server).await;
 
-        let matcher = NonSpaceRoomMatcher { is_not_space: |_| false };
+        let matcher = SpaceRoomMatcher { is_space: |_| false };
         assert!(!matcher.matches(&room));
 
-        let matcher = NonSpaceRoomMatcher { is_not_space: |_| true };
+        let matcher = SpaceRoomMatcher { is_space: |_| true };
         assert!(matcher.matches(&room));
     }
 }
