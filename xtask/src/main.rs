b1a01a35eb3cfe0ba1e303e2c1a9e4dd5dcae0c1
Kévin Commaille <zecakeh@tedomum.fr>
2025-08-13 11:54:56 +0200

    ci: Upgrade the version of Rust nightly
    Signed-off-by: Kévin Commaille <zecakeh@tedomum.fr>

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -17,7 +17,7 @@ use release::ReleaseArgs;
 use swift::SwiftArgs;
 use xshell::{Shell, cmd};
 
-const NIGHTLY: &str = "nightly-2025-06-27";
+const NIGHTLY: &str = "nightly-2025-08-08";
 
 type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
