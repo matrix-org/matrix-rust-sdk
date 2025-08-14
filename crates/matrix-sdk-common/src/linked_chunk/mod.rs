d2ecc770149e6b48eeb42cca7f67ded70d3bef1d
Michael Goldenberg <m@mgoldenberg.net>
2025-08-14 10:14:18 -0400

    feat(linked chunk): derive ser/de traits for OwnedLinkedChunkId
    Signed-off-by: Michael Goldenberg <m@mgoldenberg.net>

--- crates/matrix-sdk-common/src/linked_chunk/mod.rs
+++ crates/matrix-sdk-common/src/linked_chunk/mod.rs
@@ -107,6 +107,7 @@ use std::{
 pub use as_vector::*;
 pub use order_tracker::OrderTracker;
 use ruma::{EventId, OwnedEventId, OwnedRoomId, RoomId};
+use serde::{Deserialize, Serialize};
 pub use updates::*;
 
 /// An identifier for a linked chunk; borrowed variant.
@@ -171,7 +172,7 @@ impl PartialEq<LinkedChunkId<'_>> for OwnedLinkedChunkId {
 }
 
 /// An identifier for a linked chunk; owned variant.
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum OwnedLinkedChunkId {
     Room(OwnedRoomId),
     Thread(OwnedRoomId, OwnedEventId),
