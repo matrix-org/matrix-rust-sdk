adaccbab2c7091008f8e941da90b3fc9f1b23f16
Skye Elliot <actuallyori@gmail.com>
2025-08-18 16:21:11 +0100

    feat(sdk): Add SendRawStateEvent future and deduplicate
    Deduplicates common code that would be shared between
    SendRawMessageLikeEvent and SendRawStateEvent to a helper method,
    `ensure_room_encryption_ready`.
    
    Signed-off-by: Skye Elliot <actuallyori@gmail.com>

--- crates/matrix-sdk/src/room/futures.rs
+++ crates/matrix-sdk/src/room/futures.rs
@@ -30,9 +30,13 @@ use ruma::{
     serde::Raw,
     OwnedTransactionId, TransactionId,
 };
+#[cfg(feature = "experimental-encrypted-state-events")]
+use ruma::{api::client::state::send_state_event, events::AnyStateEventContent};
 use tracing::{info, trace, Instrument, Span};
 
 use super::Room;
+#[cfg(feature = "experimental-encrypted-state-events")]
+use crate::utils::IntoRawStateEventContent;
 use crate::{
     attachment::AttachmentConfig, config::RequestConfig, utils::IntoRawMessageLikeEventContent,
     Result, TransmissionProgress,
@@ -153,6 +157,24 @@ impl<'a> SendRawMessageLikeEvent<'a> {
     }
 }
 
+/// Ensures the room is ready for encrypted events to be sent.
+#[cfg(feature = "e2e-encryption")]
+async fn ensure_room_encryption_ready(room: &Room) -> Result<()> {
+    if !room.are_members_synced() {
+        room.sync_members().await?;
+    }
+
+    // Query keys in case we don't have them for newly synced members.
+    //
+    // Note we do it all the time, because we might have sync'd members before
+    // sending a message (so didn't enter the above branch), but
+    // could have not query their keys ever.
+    room.query_keys_for_untracked_or_dirty_users().await?;
+    room.preshare_room_key().await?;
+
+    Ok(())
+}
+
 impl<'a> IntoFuture for SendRawMessageLikeEvent<'a> {
     type Output = Result<send_message_event::v3::Response>;
     boxed_into_future!(extra_bounds: 'a);
@@ -190,18 +212,7 @@ impl<'a> IntoFuture for SendRawMessageLikeEvent<'a> {
                         "Sending encrypted event because the room is encrypted.",
                     );
 
-                    if !room.are_members_synced() {
-                        room.sync_members().await?;
-                    }
-
-                    // Query keys in case we don't have them for newly synced members.
-                    //
-                    // Note we do it all the time, because we might have sync'd members before
-                    // sending a message (so didn't enter the above branch), but
-                    // could have not query their keys ever.
-                    room.query_keys_for_untracked_or_dirty_users().await?;
-
-                    room.preshare_room_key().await?;
+                    ensure_room_encryption_ready(room).await?;
 
                     let olm = room.client.olm_machine().await;
                     let olm = olm.as_ref().expect("Olm machine wasn't started");
@@ -314,6 +325,143 @@ impl<'a> IntoFuture for SendAttachment<'a> {
                 store_in_cache,
             )
             .await
+        };
+
+        Box::pin(fut.instrument(tracing_span))
+    }
+}
+
+/// Future returned by `Room::send_state_event_raw`.
+#[cfg(feature = "experimental-encrypted-state-events")]
+#[allow(missing_debug_implementations)]
+pub struct SendRawStateEvent<'a> {
+    room: &'a Room,
+    event_type: &'a str,
+    state_key: &'a str,
+    content: Raw<AnyStateEventContent>,
+    tracing_span: Span,
+    request_config: Option<RequestConfig>,
+}
+
+#[cfg(feature = "experimental-encrypted-state-events")]
+impl<'a> SendRawStateEvent<'a> {
+    pub(crate) fn new(
+        room: &'a Room,
+        event_type: &'a str,
+        state_key: &'a str,
+        content: impl IntoRawStateEventContent,
+    ) -> Self {
+        let content = content.into_raw_state_event_content();
+        Self {
+            room,
+            event_type,
+            state_key,
+            content,
+            tracing_span: Span::current(),
+            request_config: None,
+        }
+    }
+
+    /// Assign a given [`RequestConfig`] to configure how this request should
+    /// behave with respect to the network.
+    pub fn with_request_config(mut self, request_config: RequestConfig) -> Self {
+        self.request_config = Some(request_config);
+        self
+    }
+
+    /// Determines whether the inner state event should be encrypted before
+    /// sending.
+    ///
+    /// This method checks two conditions:
+    /// 1. Whether the room supports encrypted state events, by inspecting the
+    ///    room's encryption state.
+    /// 2. Whether the event type is considered "critical" or excluded from
+    ///    encryption under MSC3414.
+    ///
+    /// # Returns
+    ///
+    /// Returns `true` if the event should be encrypted, otherwise returns
+    /// `false`.
+    fn should_encrypt(room: &Room, event_type: &str) -> bool {
+        if !room.encryption_state().is_state_encrypted() {
+            trace!("Sending plaintext event as the room does NOT support encrypted state events.");
+            return false;
+        }
+
+        // Check the event is not critical.
+        if matches!(
+            event_type,
+            "m.room.create"
+                | "m.room.member"
+                | "m.room.join_rules"
+                | "m.room.power_levels"
+                | "m.room.third_party_invite"
+                | "m.room.history_visibility"
+                | "m.room.guest_access"
+                | "m.room.encryption"
+                | "m.space.child"
+                | "m.space.parent"
+        ) {
+            trace!("Sending plaintext event as its type is excluded from encryption.");
+            return false;
+        }
+
+        true
+    }
+}
+
+#[cfg(feature = "experimental-encrypted-state-events")]
+impl<'a> IntoFuture for SendRawStateEvent<'a> {
+    type Output = Result<send_state_event::v3::Response>;
+    boxed_into_future!(extra_bounds: 'a);
+
+    fn into_future(self) -> Self::IntoFuture {
+        let Self { room, mut event_type, state_key, mut content, tracing_span, request_config } =
+            self;
+
+        let fut = async move {
+            room.ensure_room_joined()?;
+
+            let mut state_key = state_key.to_owned();
+
+            if Self::should_encrypt(room, event_type) {
+                use tracing::debug;
+
+                Span::current().record("is_room_encrypted", true);
+                debug!(
+                    room_id = ?room.room_id(),
+                    "Sending encrypted event because the room is encrypted.",
+                );
+
+                ensure_room_encryption_ready(room).await?;
+
+                let olm = room.client.olm_machine().await;
+                let olm = olm.as_ref().expect("Olm machine wasn't started");
+
+                content = olm
+                    .encrypt_state_event_raw(room.room_id(), event_type, &state_key, &content)
+                    .await?
+                    .cast_unchecked();
+
+                state_key = format!("{event_type}:{state_key}");
+                event_type = "m.room.encrypted";
+            } else {
+                Span::current().record("is_room_encrypted", false);
+            }
+
+            let request = send_state_event::v3::Request::new_raw(
+                room.room_id().to_owned(),
+                event_type.into(),
+                state_key.to_owned(),
+                content,
+            );
+
+            let response = room.client.send(request).with_request_config(request_config).await?;
+
+            Span::current().record("event_id", tracing::field::debug(&response.event_id));
+            info!("Sent event in room");
+
+            Ok(response)
         };
 
         Box::pin(fut.instrument(tracing_span))
