57a51a8841be5b9eee2f185acfcd118114c8b7c0
Shrey Patel <shreyp@element.io>
2025-08-27 11:43:46 +0100

    fixup! test(search): Add tests for indexing redactions.

--- crates/matrix-sdk-search/src/index.rs
+++ crates/matrix-sdk-search/src/index.rs
@@ -312,25 +312,22 @@ mod tests {
     }
 
     #[test]
-    fn test_index_contains_false() -> Result<(), Box<dyn Error>> {
+    fn test_index_contains_false() {
         let room_id = room_id!("!room_id:localhost");
         let mut index =
             RoomIndex::new_in_memory(room_id).expect("failed to make index in ram: {index:?}");
 
         let event_id = event_id!("$event_id:localhost");
 
-        index.commit_and_reload()?;
+        index.commit_and_reload().unwrap();
 
         assert!(!index.contains(event_id), "Index should not contain event");
-
-        Ok(())
     }
 
     #[test]
     fn test_index_contains_true() -> Result<(), Box<dyn Error>> {
         let room_id = room_id!("!room_id:localhost");
-        let mut index =
-            RoomIndex::new_in_memory(room_id).expect("failed to make index in ram: {index:?}");
+        let mut index = RoomIndex::new_in_memory(room_id)?;
 
         let event_id = event_id!("$event_id:localhost");
         let event = EventFactory::new()
@@ -352,8 +349,7 @@ mod tests {
     #[test]
     fn test_indexing_idempotency() -> Result<(), Box<dyn Error>> {
         let room_id = room_id!("!room_id:localhost");
-        let mut index =
-            RoomIndex::new_in_memory(room_id).expect("failed to make index in ram: {index:?}");
+        let mut index = RoomIndex::new_in_memory(room_id)?;
 
         let event_id = event_id!("$event_id:localhost");
         let event = EventFactory::new()
@@ -386,33 +382,26 @@ mod tests {
     #[test]
     fn test_redaction_removes_event() -> Result<(), Box<dyn Error>> {
         let room_id = room_id!("!room_id:localhost");
-        let mut index =
-            RoomIndex::new_in_memory(room_id).expect("failed to make index in ram: {index:?}");
+        let mut index = RoomIndex::new_in_memory(room_id)?;
 
         let event_id = event_id!("$event_id:localhost");
         let user_id = user_id!("@user_id:localhost");
-        let event = EventFactory::new()
-            .text_msg("This is a sentence")
-            .event_id(event_id)
-            .room(room_id)
-            .sender(user_id)
-            .into_any_sync_message_like_event();
+        let f = EventFactory::new().room(room_id).sender(user_id);
 
-        index.handle_event(event.clone())?;
+        let event =
+            f.text_msg("This is a sentence").event_id(event_id).into_any_sync_message_like_event();
+
+        index.handle_event(event)?;
 
         index.commit_and_reload()?;
 
         assert!(index.contains(event_id), "Index should contain event");
 
         let redaction_event_id = event_id!("$redaction_event_id:localhost");
-        let redaction = EventFactory::new()
-            .redaction(event_id)
-            .event_id(redaction_event_id)
-            .room(room_id)
-            .sender(user_id)
-            .into_any_sync_message_like_event();
+        let redaction =
+            f.redaction(event_id).event_id(redaction_event_id).into_any_sync_message_like_event();
 
-        index.handle_event(redaction.clone())?;
+        index.handle_event(redaction)?;
 
         index.commit_and_reload()?;
 
