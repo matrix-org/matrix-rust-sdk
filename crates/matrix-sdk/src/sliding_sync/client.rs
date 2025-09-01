31bb0f475ec02ec95aad584ab30ff907a16f08b9
Benjamin Bouvier <benjamin@bouvier.cc>
2025-09-01 15:01:40 +0200

    feat(sliding sync): add support for the thread subscriptions extension

--- crates/matrix-sdk/src/sliding_sync/client.rs
+++ crates/matrix-sdk/src/sliding_sync/client.rs
@@ -1,8 +1,13 @@
 use std::collections::BTreeSet;
 
-use matrix_sdk_base::{sync::SyncResponse, RequestedRequiredStates};
+use matrix_sdk_base::{
+    sync::SyncResponse, RequestedRequiredStates, ThreadSubscriptionCatchupToken,
+};
 use matrix_sdk_common::deserialized_responses::ProcessedToDeviceEvent;
-use ruma::api::{client::sync::sync_events::v5 as http, FeatureFlag, SupportedVersions};
+use ruma::api::{
+    client::sync::sync_events::v5::{self as http, response},
+    FeatureFlag, SupportedVersions,
+};
 use tracing::error;
 
 use super::{SlidingSync, SlidingSyncBuilder};
@@ -160,10 +165,7 @@ impl SlidingSyncResponseProcessor {
     }
 
     #[cfg(feature = "e2e-encryption")]
-    pub async fn handle_encryption(
-        &mut self,
-        extensions: &http::response::Extensions,
-    ) -> Result<()> {
+    pub async fn handle_encryption(&mut self, extensions: &response::Extensions) -> Result<()> {
         // This is an internal API misuse if this is triggered (calling
         // `handle_room_response` before this function), so panic is fine.
         assert!(self.response.is_none());
@@ -200,6 +202,16 @@ impl SlidingSyncResponseProcessor {
         update_in_memory_caches(&self.client, &sync_response).await?;
 
         self.response = Some(sync_response);
+
+        Ok(())
+    }
+
+    pub async fn handle_thread_subscriptions(
+        &mut self,
+        previous_pos: Option<&str>,
+        thread_subs: response::ThreadSubscriptions,
+    ) -> Result<()> {
+        // TODO
 
         Ok(())
     }
