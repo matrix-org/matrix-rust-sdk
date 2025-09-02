f1190deef940571ba88217f08802e83213d588a9
Ivan Enderlin <ivan@mnt.io>
2025-09-02 13:22:04 +0200

    refactor(ui): `TimelineAction::from_event` zips two arguments.
    This patch zips the `unable_to_decrypt_info` and the `meta` arguments
    into a single one. They are strictly related, `meta` has no sense if
    `unable_to_decrypt_info` has no sense neither.

--- crates/matrix-sdk-ui/src/timeline/controller/state_transaction.rs
+++ crates/matrix-sdk-ui/src/timeline/controller/state_transaction.rs
@@ -207,7 +207,6 @@ impl<'a, P: RoomDataProvider> TimelineStateTransaction<'a, P> {
             raw_event,
             room_data_provider,
             None,
-            &self.meta,
             None,
             None,
             None,
@@ -648,8 +647,8 @@ impl<'a, P: RoomDataProvider> TimelineStateTransaction<'a, P> {
                         event,
                         &raw,
                         room_data_provider,
-                        utd_info,
-                        &self.meta,
+                        utd_info
+                            .map(|utd_info| (utd_info, self.meta.unable_to_decrypt_hook.as_ref())),
                         in_reply_to,
                         thread_root,
                         thread_summary,
