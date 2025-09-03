db5503e30e8058986be5e2dea62c49b22c57410c
Valere <bill.carson@valrsoft.com>
2025-09-03 12:43:10 +0200

    tests: add decline call tests

--- crates/matrix-sdk/tests/integration/room/calls.rs
+++ crates/matrix-sdk/tests/integration/room/calls.rs
@@ -1,8 +1,10 @@
 #![cfg(feature = "unstable-msc4310")]
+
 use std::sync::{Arc, Mutex};
 
-use matrix_sdk::test_utils::mocks::MatrixMockServer;
-use matrix_sdk_test::{async_test, event_factory::EventFactory, JoinedRoomBuilder};
+use assert_matches2::assert_matches;
+use matrix_sdk::{room::calls::CallError, test_utils::mocks::MatrixMockServer};
+use matrix_sdk_test::{async_test, event_factory::EventFactory, JoinedRoomBuilder, StateTestEvent};
 use ruma::{owned_event_id, room_id, user_id, OwnedUserId};
 use tokio::spawn;
 
@@ -17,8 +19,6 @@ async fn test_subscribe_to_decline_call_events() {
     let room_id = room_id!("!test:example.org");
 
     let notification_event_id = owned_event_id!("$00000:localhost");
-    // Initial sync with our test room.
-    // let room = server.sync_joined_room(&client, room_id).await;
 
     let f = EventFactory::new();
     server
@@ -82,4 +82,80 @@ async fn test_subscribe_to_decline_call_events() {
 
     join_handle.await.unwrap();
     assert_eq!(decliners_sequences.lock().unwrap().to_vec(), asserted_decliners);
+}
+
+#[async_test]
+async fn test_decline_call() {
+    let server = MatrixMockServer::new().await;
+    let client = server.client_builder().build().await;
+
+    println!("client id is: {}", client.user_id().unwrap());
+    let room_id = room_id!("!test:example.org");
+
+    let notification_event_id = owned_event_id!("$00000:localhost");
+    let a_message_event_id = owned_event_id!("$00001:localhost");
+    let unknown_event_id = owned_event_id!("$00002:localhost");
+    let own_notification_event_id = owned_event_id!("$00003:localhost");
+
+    // Subscribe to the event cache (to avoid having to remotly fetch the related
+    // event)
+    let event_cache = client.event_cache();
+    event_cache.subscribe().unwrap();
+
+    let f = EventFactory::new();
+    server
+        .sync_room(
+            &client,
+            JoinedRoomBuilder::new(room_id)
+                .add_state_event(StateTestEvent::Encryption)
+                .add_timeline_event(
+                    f.call_notify(
+                        "call_id".to_owned(),
+                        ruma::events::call::notify::ApplicationType::Call,
+                        ruma::events::call::notify::NotifyType::Ring,
+                        ruma::events::Mentions::new(),
+                    )
+                    .sender(user_id!("@alice:matrix.org"))
+                    .event_id(&notification_event_id),
+                )
+                .add_timeline_event(
+                    f.call_notify(
+                        "call_id_1".to_owned(),
+                        ruma::events::call::notify::ApplicationType::Call,
+                        ruma::events::call::notify::NotifyType::Ring,
+                        ruma::events::Mentions::new(),
+                    )
+                    .sender(user_id!("@example:localhost"))
+                    .event_id(&own_notification_event_id),
+                )
+                .add_timeline_event(
+                    f.text_msg("Hello, HRU? ")
+                        .event_id(&a_message_event_id)
+                        .sender(user_id!("@alice:matrix.org")),
+                ),
+        )
+        .await;
+
+    let room = server.sync_joined_room(&client, room_id).await;
+
+    // Declining an unknown call should fail
+    let result = room.make_decline_call_event(&unknown_event_id).await;
+    assert!(result.is_err());
+    assert_matches!(result.unwrap_err(), CallError::Fetch(_));
+
+    // Declining a non call notification event should fail
+    let result = room.make_decline_call_event(&a_message_event_id).await;
+    assert!(result.is_err());
+    assert_matches!(result.unwrap_err(), CallError::BadEventType);
+
+    // Declining your own call notification event should fail
+    let result = room.make_decline_call_event(&own_notification_event_id).await;
+    assert!(result.is_err());
+    assert_matches!(result.unwrap_err(), CallError::DeclineOwnCall);
+
+    let event =
+        room.make_decline_call_event(&notification_event_id).await.expect("Should not fail");
+
+    // try to queue
+    room.send_queue().send(event.into()).await.expect("Should not fail");
 }
