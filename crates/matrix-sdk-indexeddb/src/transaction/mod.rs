67376c8b9ee5e0f70c0c6881be795885f40f3762
Michael Goldenberg <m@mgoldenberg.net>
2025-09-16 09:26:06 -0400

    refactor(indexeddb): allow dead code in transaction and indexed type serializer modules while media store under development
    Signed-off-by: Michael Goldenberg <m@mgoldenberg.net>

--- crates/matrix-sdk-indexeddb/src/transaction/mod.rs
+++ crates/matrix-sdk-indexeddb/src/transaction/mod.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License
 
+// Allow dead code here, as this module is still in the process
+// of being developed, so some functions will be used later on.
+// Once development is complete, we can remove this line and
+// clean up any dead code.
+#![allow(dead_code)]
+
 use indexed_db_futures::{prelude::IdbTransaction, IdbQuerySource};
 use matrix_sdk_base::media::store::{MediaRetentionPolicy, MediaStoreError};
 use serde::{
