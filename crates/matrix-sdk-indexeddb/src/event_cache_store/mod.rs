49db60d95144995ce7e1839d52a5946a1ce188be
Damir Jelić <poljar@termina.org.uk>
2025-09-22 08:40:31 +0200

    feat: Allow events to be fetched by event type

--- crates/matrix-sdk-indexeddb/src/event_cache_store/mod.rs
+++ crates/matrix-sdk-indexeddb/src/event_cache_store/mod.rs
@@ -479,14 +479,29 @@ impl EventCacheStore for IndexeddbEventCacheStore {
     async fn get_room_events(
         &self,
         room_id: &RoomId,
+        event_type: Option<&str>,
+        session_id: Option<&str>,
     ) -> Result<Vec<Event>, IndexeddbEventCacheStoreError> {
         let _timer = timer!("method");
+
+        // TODO: Make this more efficient so we don't load all events and filter them
+        // here. We should instead only load the relevant events.
 
         let transaction = self.transaction(&[keys::EVENTS], IdbTransactionMode::Readonly)?;
         transaction
             .get_room_events(room_id)
             .await
-            .map(|vec| vec.into_iter().map(Into::into).collect())
+            .map(|vec| {
+                vec.into_iter()
+                    .map(Event::from)
+                    .filter(|e| {
+                        event_type.is_none_or(|event_type| {
+                            Some(event_type) == e.kind.event_type().as_deref()
+                        })
+                    })
+                    .filter(|e| session_id.is_none_or(|s| Some(s) == e.kind.session_id()))
+                    .collect()
+            })
             .map_err(Into::into)
     }
 
