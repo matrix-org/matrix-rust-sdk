3a60d34f3fd0c3b0c6a2bb44e39c0c1bd4ea6b54
Stefan Ceriu <stefanc@matrix.org>
2025-09-30 14:06:58 +0300

    feat(ffi): expose the space service leaving interfaces
    fix newline ffi

--- bindings/matrix-sdk-ffi/src/error.rs
+++ bindings/matrix-sdk-ffi/src/error.rs
@@ -10,7 +10,7 @@ use matrix_sdk::{
     HttpError, IdParseError, NotificationSettingsError as SdkNotificationSettingsError,
     QueueWedgeError as SdkQueueWedgeError, StoreError,
 };
-use matrix_sdk_ui::{encryption_sync_service, notification_client, sync_service, timeline};
+use matrix_sdk_ui::{encryption_sync_service, notification_client, spaces, sync_service, timeline};
 use ruma::{
     api::client::error::{ErrorBody, ErrorKind as RumaApiErrorKind, RetryAfter},
     MilliSecondsSinceUnixEpoch,
@@ -213,6 +213,12 @@ impl From<FocusEventError> for ClientError {
 
 impl From<RequestVerificationError> for ClientError {
     fn from(e: RequestVerificationError) -> Self {
+        Self::from_err(e)
+    }
+}
+
+impl From<spaces::Error> for ClientError {
+    fn from(e: spaces::Error) -> Self {
         Self::from_err(e)
     }
 }
