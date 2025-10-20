85b814273f93aa3047cd21619357e8de3e72bcf9
Michael Goldenberg <m@mgoldenberg.net>
2025-10-20 18:36:37 -0400

    refactor(indexeddb): remove media object store and associated types
    Signed-off-by: Michael Goldenberg <m@mgoldenberg.net>

--- crates/matrix-sdk-indexeddb/src/media_store/migrations.rs
+++ crates/matrix-sdk-indexeddb/src/media_store/migrations.rs
@@ -114,16 +114,6 @@ pub mod v1 {
         pub const LEASES_KEY_PATH: &str = "id";
         pub const MEDIA_RETENTION_POLICY_KEY: &str = "media_retention_policy";
         pub const MEDIA_CLEANUP_TIME_KEY: &str = "media_cleanup_time";
-        pub const MEDIA: &str = "media";
-        pub const MEDIA_KEY_PATH: &str = "id";
-        pub const MEDIA_URI: &str = "media_uri";
-        pub const MEDIA_URI_KEY_PATH: &str = "uri";
-        pub const MEDIA_CONTENT_SIZE: &str = "media_content_size";
-        pub const MEDIA_CONTENT_SIZE_KEY_PATH: &str = "content_size";
-        pub const MEDIA_LAST_ACCESS: &str = "media_last_access";
-        pub const MEDIA_LAST_ACCESS_KEY_PATH: &str = "last_access";
-        pub const MEDIA_RETENTION_METADATA: &str = "media_retention_metadata";
-        pub const MEDIA_RETENTION_METADATA_KEY_PATH: &str = "retention_metadata";
         pub const MEDIA_METADATA: &str = "media_metadata";
         pub const MEDIA_METADATA_KEY_PATH: &str = "id";
         pub const MEDIA_METADATA_URI: &str = "media_metadata_uri";
@@ -142,7 +132,6 @@ pub mod v1 {
     pub fn create_object_stores(db: &Database) -> Result<(), Error> {
         create_core_object_store(db)?;
         create_lease_object_store(db)?;
-        create_media_object_store(db)?;
         create_media_metadata_object_store(db)?;
         create_media_content_object_store(db)?;
         Ok(())
@@ -162,40 +151,6 @@ pub mod v1 {
         let _ = db
             .create_object_store(keys::LEASES)
             .with_key_path(keys::LEASES_KEY_PATH.into())
-            .build()?;
-        Ok(())
-    }
-
-    /// Create an object store for tracking information about media.
-    ///
-    /// * Primary Key - `id`
-    /// * Index - `uri` - tracks the [`MxcUri`][1] of the associated media
-    /// * Index - `content_size` - tracks the size of the media content and
-    ///   whether to ignore the [`MediaRetentionPolicy`][2]
-    /// * Index - `last_access` - tracks the last time the associated media was
-    ///   accessed
-    /// * Index - `retention_metadata` - tracks all retention metadata - i.e.,
-    ///   joins `content_size` and `last_access`
-    ///
-    /// [1]: ruma::MxcUri
-    /// [2]: matrix_sdk_base::media::store::MediaRetentionPolicy
-    fn create_media_object_store(db: &Database) -> Result<(), Error> {
-        let media = db
-            .create_object_store(keys::MEDIA)
-            .with_key_path(keys::MEDIA_KEY_PATH.into())
-            .build()?;
-        let _ = media.create_index(keys::MEDIA_URI, keys::MEDIA_URI_KEY_PATH.into()).build()?;
-        let _ = media
-            .create_index(keys::MEDIA_CONTENT_SIZE, keys::MEDIA_CONTENT_SIZE_KEY_PATH.into())
-            .build()?;
-        let _ = media
-            .create_index(keys::MEDIA_LAST_ACCESS, keys::MEDIA_LAST_ACCESS_KEY_PATH.into())
-            .build()?;
-        let _ = media
-            .create_index(
-                keys::MEDIA_RETENTION_METADATA,
-                keys::MEDIA_RETENTION_METADATA_KEY_PATH.into(),
-            )
             .build()?;
         Ok(())
     }
