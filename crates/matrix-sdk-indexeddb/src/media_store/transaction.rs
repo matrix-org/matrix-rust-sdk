c169cab3b0f840866e701563b13aa223db9b79ee
Michael Goldenberg <m@mgoldenberg.net>
2025-10-20 18:36:37 -0400

    refactor(indexeddb): remove media object store and associated types
    Signed-off-by: Michael Goldenberg <m@mgoldenberg.net>

--- crates/matrix-sdk-indexeddb/src/media_store/transaction.rs
+++ crates/matrix-sdk-indexeddb/src/media_store/transaction.rs
@@ -24,12 +24,11 @@ use ruma::MxcUri;
 use crate::{
     media_store::{
         serializer::indexed_types::{
-            IndexedCoreIdKey, IndexedLease, IndexedLeaseIdKey, IndexedMedia,
-            IndexedMediaCleanupTime, IndexedMediaContent, IndexedMediaContentIdKey,
-            IndexedMediaContentSizeKey, IndexedMediaIdKey, IndexedMediaMetadata,
+            IndexedCoreIdKey, IndexedLease, IndexedLeaseIdKey, IndexedMediaCleanupTime,
+            IndexedMediaContent, IndexedMediaContentIdKey, IndexedMediaMetadata,
             IndexedMediaMetadataContentSizeKey, IndexedMediaMetadataIdKey,
             IndexedMediaMetadataLastAccessKey, IndexedMediaMetadataRetentionKey,
-            IndexedMediaMetadataUriKey, IndexedMediaRetentionMetadataKey, IndexedMediaUriKey,
+            IndexedMediaMetadataUriKey,
         },
         types::{Lease, Media, MediaCleanupTime, MediaContent, MediaMetadata, UnixTime},
     },
@@ -109,16 +108,6 @@ impl<'a> IndexeddbMediaStoreTransaction<'a> {
         self.transaction.put_item(&time).await
     }
 
-    /// Query IndexedDB for [`Media`] that matches the given
-    /// [`MediaRequestParameters`]. If more than one item is found, an error
-    /// is returned.
-    pub async fn get_media_by_id(
-        &self,
-        request_parameters: &MediaRequestParameters,
-    ) -> Result<Option<Media>, TransactionError> {
-        self.get_item_by_key_components::<Media, IndexedMediaIdKey>(request_parameters).await
-    }
-
     /// Query IndexedDB for [`MediaMetadata`] and [`MediaContent`] that matches
     /// the given [`MediaRequestParameters`]. If an item is found, update
     /// [`MediaMetadata::last_access`] using `current_time`. If more than one
@@ -146,11 +135,6 @@ impl<'a> IndexeddbMediaStoreTransaction<'a> {
         }
     }
 
-    /// Query IndexedDB for [`Media`] that match the given [`MxcUri`].
-    pub async fn get_media_by_uri(&self, uri: &MxcUri) -> Result<Vec<Media>, TransactionError> {
-        self.get_items_by_key_components::<Media, IndexedMediaUriKey>(uri).await
-    }
-
     /// Query IndexedDB for [`MediaMetadata`] and [`MediaContent`] that matches
     /// the given [`MxcUri`]. If an item is found, update
     /// [`MediaMetadata::last_access`] using `current_time`. If more than
@@ -176,51 +160,6 @@ impl<'a> IndexeddbMediaStoreTransaction<'a> {
         Ok(medias)
     }
 
-    /// Query IndexedDB for all [content size](IndexedMediaContentSizeKey) keys
-    /// whose associated [`Media`] matches the given
-    /// [`IgnoreMediaRetentionPolicy`].
-    pub async fn get_media_keys_by_content_size(
-        &self,
-        ignore_policy: IgnoreMediaRetentionPolicy,
-    ) -> Result<Vec<IndexedMediaContentSizeKey>, TransactionError> {
-        self.get_keys::<Media, IndexedMediaContentSizeKey>(IndexedKeyRange::all_with_prefix(
-            ignore_policy,
-            self.serializer().inner(),
-        ))
-        .await
-    }
-
-    /// Query IndexedDB for [retention metadata][1] keys that match the given
-    /// key range. Iterate over the keys in the given
-    /// [`direction`](CursorDirection) using a cursor and fold them into an
-    /// accumulator while the given function `f` returns [`Some`].
-    ///
-    /// This function returns the final value of the accumulator and the key, if
-    /// any, which caused the fold to short circuit.
-    ///
-    /// Note that the use of cursor means that keys are read lazily from
-    /// IndexedDB.
-    ///
-    /// [1]: IndexedMediaRetentionMetadataKey
-    pub async fn fold_media_keys_by_retention_metadata_while<Acc, F>(
-        &self,
-        direction: CursorDirection,
-        ignore_policy: IgnoreMediaRetentionPolicy,
-        init: Acc,
-        f: F,
-    ) -> Result<(Acc, Option<IndexedMediaRetentionMetadataKey>), TransactionError>
-    where
-        F: FnMut(&Acc, &IndexedMediaRetentionMetadataKey) -> Option<Acc>,
-    {
-        self.fold_keys_while::<Media, IndexedMediaRetentionMetadataKey, Acc, F>(
-            direction,
-            IndexedKeyRange::all_with_prefix(ignore_policy, self.serializer().inner()),
-            init,
-            f,
-        )
-        .await
-    }
-
     /// Query IndexedDB for the size recorded in each
     /// [`MediaMetadata::content_size`] which match
     /// the given [`IgnoreMediaRetentionPolicy`]. Returns the sum of all sizes
@@ -237,22 +176,6 @@ impl<'a> IndexeddbMediaStoreTransaction<'a> {
             .await?
             .iter()
             .try_fold(0usize, |size, key| size.checked_add(key.content_size())))
-    }
-
-    /// Adds [`Media`] to IndexedDB. If an item with the same key already
-    /// exists, it will be rejected. When the item is successfully added, the
-    /// function returns the intermediary type [`IndexedMedia`] in case
-    /// inspection is needed.
-    pub async fn add_media(&self, media: &Media) -> Result<IndexedMedia, TransactionError> {
-        self.add_item(media).await
-    }
-
-    /// Puts [`Media`] in IndexedDB object. If an item with the same key already
-    /// exists, it will be overwritten. When the item is successfully put, the
-    /// function returns the intermediary type [`IndexedMedia`] in case
-    /// inspection is needed.
-    pub async fn put_media(&self, media: &Media) -> Result<IndexedMedia, TransactionError> {
-        self.put_item(media).await
     }
 
     /// Adds [`MediaMetadata`] and [`MediaContent`] to IndexedDB if the size of
