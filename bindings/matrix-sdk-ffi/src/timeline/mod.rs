487b84246f0c34a41b5cced7df33c428b4ee8fdd
Jorge Martín <jorgem@element.io>
2025-11-14 10:18:10 +0100

    reafactor(ffi): Have `ffi::Timeline::latest_event_id` use `ui::Timeline::latest_event_id`, instead of `ui::Timeline::latest_event`
    This is important because `latest_event` would also return local events, which won't have an event id.

--- bindings/matrix-sdk-ffi/src/timeline/mod.rs
+++ bindings/matrix-sdk-ffi/src/timeline/mod.rs
@@ -367,7 +367,7 @@ impl Timeline {
 
     /// Returns the [`EventId`] of the latest event in the timeline.
     pub async fn latest_event_id(&self) -> Option<String> {
-        self.inner.latest_event().await.and_then(|event| event.event_id().map(ToString::to_string))
+        self.inner.latest_event_id().await.as_deref().map(ToString::to_string)
     }
 
     /// Queues an event in the room's send queue so it's processed for
