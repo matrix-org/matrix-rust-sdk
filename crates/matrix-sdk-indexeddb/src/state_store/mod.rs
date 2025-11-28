e1cda064eeb2660b055806958389a3c27de02836
Jorge Martín <jorgem@element.io>
2025-11-28 15:30:44 +0100

    refactor: hopefully fix another lint error

--- crates/matrix-sdk-indexeddb/src/state_store/mod.rs
+++ crates/matrix-sdk-indexeddb/src/state_store/mod.rs
@@ -2028,7 +2028,7 @@ impl_state_store!({
     }
 
     #[allow(clippy::unused_async)]
-    async fn optimize(&self) -> Result<(), StoreError> {
+    async fn optimize(&self) -> Result<()> {
         Ok(())
     }
 });
