fa8012a876593685876220e03be6a49707897c6a
Ivan Enderlin <ivan@mnt.io>
2026-02-05 16:39:51 +0100

    feat(xtask): Add `log overview`.
    This patch adds the `log overview` task that generates a standalone HTML
    report representing the logs as a tree where each node is a target, and
    each leaf is a log location with occurrences, spans and fields.
    
    Each node displays the sum of errors and warnings for this node.
    It helps to quickly spot the problematic targets, and it guides to
    exploration of the node without taking the reader's hand by trying to
    draw conclusions. We don't want to guide the reader to a mistake: we
    just want to guide the reader to draw its own conclusions.
    
    Each line can be highlighted. When a node is folded/closed, it is also
    highlighted if at least one of its child is highlighted.
    
    The occurrences of logs are displayed on a timeline.

--- xtask/src/log/mod.rs
+++ xtask/src/log/mod.rs
@@ -1,7 +1,9 @@
+mod overview;
+mod sync;
+
 use std::path::PathBuf;
 
 use clap::{Args, Subcommand};
-mod sync;
 
 use crate::{Result, sh, workspace};
 
@@ -14,12 +16,26 @@ pub struct LogArgs {
 /// Analysis around logs (formatted by `matrix-sdk-ffi`).
 #[derive(Subcommand)]
 enum LogCommand {
-    /// Visualise the sync requests and responses with a table and a duration
-    /// graph.
-    Sync {
+    /// Overview the logs as a tree where each node is a target and each leaf is
+    /// a log location with occurrences, spans and fields.
+    Overview {
+        /// The file containing the logs to analyse.
         #[clap(long)]
         log_file: PathBuf,
 
+        /// The output file that will receive the HTML report.
+        #[clap(long)]
+        html_output_file: PathBuf,
+    },
+
+    /// Visualise the sync requests and responses with a table and a duration
+    /// graph.
+    Sync {
+        /// The file containing the logs to analyse.
+        #[clap(long)]
+        log_file: PathBuf,
+
+        /// The output file that will receive the HTML report.
         #[clap(long)]
         html_output_file: PathBuf,
     },
@@ -31,6 +47,9 @@ impl LogArgs {
         let _p = sh.push_dir(workspace::root_path()?);
 
         match self.cmd {
+            LogCommand::Overview { log_file, html_output_file } => {
+                overview::run(log_file, html_output_file)?
+            }
             LogCommand::Sync { log_file, html_output_file } => {
                 sync::run(log_file, html_output_file)?
             }
