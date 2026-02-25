c95b494e04f9b51b0a2445b240f78e491dd4bea6
Skye Elliot <actuallyori@gmail.com>
2026-02-25 15:21:24 +0000

    refactor(sdk): Move `should_accept_bundle` into `shared_room_history`
    Signed-off-by: Skye Elliot <actuallyori@gmail.com>

--- crates/matrix-sdk/src/room/shared_room_history.rs
+++ crates/matrix-sdk/src/room/shared_room_history.rs
@@ -12,10 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{collections::HashSet, iter};
+use std::{collections::HashSet, iter, time::Duration};
 
 use matrix_sdk_base::{
-    crypto::{store::types::Changes, types::events::room_key_bundle::RoomKeyBundleContent},
+    RoomState,
+    crypto::{
+        store::types::{Changes, RoomKeyBundleInfo, RoomPendingKeyBundleDetails},
+        types::events::room_key_bundle::RoomKeyBundleContent,
+    },
     media::{MediaFormat, MediaRequestParameters},
 };
 use ruma::{OwnedUserId, UserId, events::room::MediaSource};
@@ -111,6 +115,48 @@ pub(super) async fn share_room_history(room: &Room, user_id: OwnedUserId) -> Res
     Ok(())
 }
 
+/// Determines whether a room key bundle should be accepted for a given room.
+///
+/// This function checks if the client has recorded invite acceptance details
+/// for the room and ensures that the bundle sender matches the inviter.
+/// Additionally, it verifies that the room is in a joined state and that the
+/// bundle is received within one day of the invite being accepted.
+///
+/// # Arguments
+///
+/// * `room` - The room for which the key bundle acceptance is being evaluated.
+/// * `bundle_info` - Information about the room key bundle being evaluated.
+///
+/// # Returns
+///
+/// Returns `true` if the key bundle should be accepted, otherwise `false`.
+pub(crate) async fn should_accept_key_bundle(room: &Room, bundle_info: &RoomKeyBundleInfo) -> bool {
+    // We accept historic room key bundles up to one day after we have accepted an
+    // invite.
+    const DAY: Duration = Duration::from_secs(24 * 60 * 60);
+
+    // If we don't have any invite acceptance details, then this client wasn't the
+    // one that accepted the invite.
+    let Ok(Some(RoomPendingKeyBundleDetails { invite_accepted_at, inviter, .. })) =
+        room.client.base_client().get_pending_key_bundle_details_for_room(room.room_id()).await
+    else {
+        debug!("Not accepting key bundle as there are no recorded invite acceptance details");
+        return false;
+    };
+
+    let state = room.state();
+    let elapsed_since_join = invite_accepted_at.to_system_time().and_then(|t| t.elapsed().ok());
+    let bundle_sender = &bundle_info.sender;
+
+    match (state, elapsed_since_join) {
+        (RoomState::Joined, Some(elapsed_since_join)) => {
+            elapsed_since_join < DAY && bundle_sender == &inviter
+        }
+        (RoomState::Joined, None) => false,
+        (RoomState::Left | RoomState::Invited | RoomState::Knocked | RoomState::Banned, _) => false,
+    }
+}
+
 /// Having accepted an invite for the given room from the given user, attempt to
 /// find a information about a room key bundle and, if found, download the
 /// bundle and import the room keys, as per [MSC4268].
@@ -193,4 +239,101 @@ pub(crate) async fn maybe_accept_key_bundle(room: &Room, inviter: &UserId) -> Re
     // user_id).await?;
 
     Ok(())
+}
+
+#[cfg(all(test, not(target_family = "wasm")))]
+mod test {
+    use matrix_sdk_base::crypto::store::types::RoomKeyBundleInfo;
+    use matrix_sdk_test::{
+        InvitedRoomBuilder, JoinedRoomBuilder, async_test, event_factory::EventFactory,
+    };
+    use ruma::{room_id, user_id};
+    use vodozemac::Curve25519PublicKey;
+
+    use crate::{room::shared_room_history, test_utils::mocks::MatrixMockServer};
+
+    /// Test that ensures that we only accept a bundle if a certain set of
+    /// conditions is met.
+    #[async_test]
+    async fn test_should_accept_bundle() {
+        let server = MatrixMockServer::new().await;
+
+        let alice_user_id = user_id!("@alice:localhost");
+        let bob_user_id = user_id!("@bob:localhost");
+        let joined_room_id = room_id!("!joined:localhost");
+        let invited_rom_id = room_id!("!invited:localhost");
+
+        let client = server
+            .client_builder()
+            .logged_in_with_token("ABCD".to_owned(), alice_user_id.into(), "DEVICEID".into())
+            .build()
+            .await;
+
+        let event_factory = EventFactory::new().room(invited_rom_id);
+        let bob_member_event = event_factory.member(bob_user_id);
+        let alice_member_event = event_factory.member(bob_user_id).invited(alice_user_id);
+
+        server
+            .mock_sync()
+            .ok_and_run(&client, |builder| {
+                builder.add_joined_room(JoinedRoomBuilder::new(joined_room_id)).add_invited_room(
+                    InvitedRoomBuilder::new(invited_rom_id)
+                        .add_state_event(bob_member_event)
+                        .add_state_event(alice_member_event),
+                );
+            })
+            .await;
+
+        let room =
+            client.get_room(joined_room_id).expect("We should have access to our joined room now");
+
+        assert!(
+            client
+                .base_client()
+                .get_pending_key_bundle_details_for_room(room.room_id())
+                .await
+                .unwrap()
+                .is_none(),
+            "We shouldn't have any invite acceptance details if we didn't join the room on this Client"
+        );
+
+        let bundle_info = RoomKeyBundleInfo {
+            sender: bob_user_id.to_owned(),
+            sender_key: Curve25519PublicKey::from_bytes([0u8; 32]),
+            room_id: joined_room_id.to_owned(),
+        };
+
+        assert!(
+            !shared_room_history::should_accept_key_bundle(&room, &bundle_info).await,
+            "We should not accept a bundle if we did not join the room from this Client"
+        );
+
+        let invited_room =
+            client.get_room(invited_rom_id).expect("We should have access to our invited room now");
+
+        assert!(
+            !shared_room_history::should_accept_key_bundle(&invited_room, &bundle_info).await,
+            "We should not accept a bundle if we didn't join the room."
+        );
+
+        server.mock_room_join(invited_rom_id).ok().mock_once().mount().await;
+
+        let room = client
+            .join_room_by_id(invited_rom_id)
+            .await
+            .expect("We should be able to join the invited room");
+
+        let details = client
+            .base_client()
+            .get_pending_key_bundle_details_for_room(room.room_id())
+            .await
+            .unwrap()
+            .expect("We should have stored the invite acceptance details");
+        assert_eq!(details.inviter, bob_user_id, "We should have recorded that Bob has invited us");
+
+        assert!(
+            shared_room_history::should_accept_key_bundle(&room, &bundle_info).await,
+            "We should accept a bundle if we just joined the room and did so from this very Client object"
+        );
+    }
 }
