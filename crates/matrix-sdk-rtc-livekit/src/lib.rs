3de4663851d43c1678ddbf30c2e7c7e4beb1c433
David Lassig <14598229+herrfeder@users.noreply.github.com>
2026-02-25 17:03:22 +0100

    rtc-livekit: preserve provider e2ee options in connector connect

--- crates/matrix-sdk-rtc-livekit/src/lib.rs
+++ crates/matrix-sdk-rtc-livekit/src/lib.rs
@@ -5,8 +5,8 @@ use matrix_sdk::Room as MatrixRoom;
 use matrix_sdk_rtc::{LiveKitConnection, LiveKitConnector, LiveKitError, LiveKitResult};
 
 pub use livekit;
-pub use livekit::e2ee;
 use livekit::RoomEvent;
+pub use livekit::e2ee;
 pub use livekit::{ConnectionState, Room, RoomOptions};
 use tokio::sync::Mutex;
 use tracing::info;
@@ -14,11 +14,11 @@ use tracing::info;
 #[cfg(feature = "crypto")]
 pub mod matrix_keys {
     use async_trait::async_trait;
-    use matrix_sdk::ruma::RoomId;
     use matrix_sdk::Room as MatrixRoom;
+    use matrix_sdk::ruma::RoomId;
     use matrix_sdk_crypto::{
-        olm::ExportedRoomKey, store::CryptoStoreError, types::room_history::RoomKeyBundle,
-        OlmMachine,
+        OlmMachine, olm::ExportedRoomKey, store::CryptoStoreError,
+        types::room_history::RoomKeyBundle,
     };
     use thiserror::Error;
 
@@ -108,7 +108,7 @@ pub mod matrix_keys {
         use std::sync::Arc;
 
         use matrix_sdk::ruma::{device_id, room_id, user_id};
-        use matrix_sdk_crypto::{store::MemoryStore, OlmMachine};
+        use matrix_sdk_crypto::{OlmMachine, store::MemoryStore};
 
         use super::{OlmMachineKeyMaterialProvider, PerParticipantKeyMaterialProvider};
 
@@ -224,7 +224,10 @@ where
         room: &MatrixRoom,
     ) -> LiveKitResult<Self::Connection> {
         let token = self.token_provider.token(room).await?;
-        let room_options = self.room_options.room_options(room);
+        let mut room_options = self.room_options_provider().room_options(room);
+        if room_options.encryption.is_none() {
+            room_options.encryption = room_options.e2ee.clone();
+        }
         info!(
             room_id = ?room.room_id(),
             service_url,
