36d71a0dbe111c603d1a1b0604d96d57a9e1a709
Damir Jelić <poljar@termina.org.uk>
2026-02-26 10:25:29 +0100

    fix(bindings): Import the bundle in the OIDC login case as well

--- bindings/matrix-sdk-ffi/src/client.rs
+++ bindings/matrix-sdk-ffi/src/client.rs
@@ -707,6 +707,12 @@ impl Client {
 
         self.inner.oauth().finish_login(url.into()).await?;
 
+        if let Some(bundle) = secrets_bundle {
+            self.import_secrets_bundle(&bundle)
+                .await
+                .map_err(|e| OidcError::Generic { message: e.to_string() })?;
+        }
+
         Ok(())
     }
 
