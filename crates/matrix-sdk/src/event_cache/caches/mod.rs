c7fdd4fe159bcc93386a61cb6f1b2881b356bdf9
Ivan Enderlin <ivan@mnt.io>
2026-02-27 16:23:47 +0100

    chore(sdk): Creates the `Caches` type.
    This patch is the first step for the `Caches` type. It creates it, along
    with the `new` constructor. More changes are required, but this is a
    first step.
    
    The pattern `Caches { room }` is used everywhere where a
    `RoomEventCache` was read previously, so that we are sure the type
    system will complain when we will add more fields to `Caches` (like for
    threads and pinned events).

--- crates/matrix-sdk/src/event_cache/caches/mod.rs
+++ crates/matrix-sdk/src/event_cache/caches/mod.rs
@@ -12,10 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use eyeball::SharedObservable;
 use eyeball_im::VectorDiff;
-use matrix_sdk_base::event_cache::Event;
+use matrix_sdk_base::{
+    ThreadingSupport,
+    event_cache::{Event, store::EventCacheStoreLock},
+};
+use ruma::{OwnedRoomId, RoomId};
+use tokio::sync::{broadcast::Sender, mpsc};
 
-use crate::event_cache::EventsOrigin;
+use super::{EventCacheError, Result};
+use crate::{client::WeakClient, event_cache::EventsOrigin};
 
 pub mod event_linked_chunk;
 pub(super) mod lock;
@@ -23,6 +30,77 @@ pub mod pagination;
 pub mod pinned_events;
 pub mod room;
 pub mod thread;
+
+/// A type to hold all the caches for a given room.
+#[derive(Clone)]
+pub struct Caches {
+    pub room: room::RoomEventCache,
+}
+
+impl Caches {
+    /// Create a new [`Caches`].
+    pub(super) async fn new(
+        weak_client: &WeakClient,
+        room_id: &RoomId,
+        generic_update_sender: Sender<room::RoomEventCacheGenericUpdate>,
+        linked_chunk_update_sender: Sender<room::RoomEventCacheLinkedChunkUpdate>,
+        auto_shrink_sender: mpsc::Sender<OwnedRoomId>,
+        store: EventCacheStoreLock,
+    ) -> Result<Self> {
+        let Some(client) = weak_client.get() else {
+            return Err(EventCacheError::ClientDropped);
+        };
+
+        let pagination_status =
+            SharedObservable::new(pagination::PaginationStatus::Idle { hit_timeline_start: false });
+
+        let room = client
+            .get_room(room_id)
+            .ok_or_else(|| EventCacheError::RoomNotFound { room_id: room_id.to_owned() })?;
+        let room_version_rules = room.clone_info().room_version_rules_or_default();
+
+        let enabled_thread_support =
+            matches!(client.base_client().threading_support, ThreadingSupport::Enabled { .. });
+
+        let update_sender = room::RoomEventCacheUpdateSender::new(generic_update_sender.clone());
+
+        let own_user_id =
+            client.user_id().expect("the user must be logged in, at this point").to_owned();
+
+        let room_state = room::RoomEventCacheStateLock::new(
+            own_user_id,
+            room_id.to_owned(),
+            room_version_rules,
+            enabled_thread_support,
+            update_sender.clone(),
+            linked_chunk_update_sender,
+            store,
+            pagination_status.clone(),
+        )
+        .await?;
+
+        let timeline_is_not_empty =
+            room_state.read().await?.room_linked_chunk().revents().next().is_some();
+
+        let room_event_cache = room::RoomEventCache::new(
+            weak_client.clone(),
+            room_state,
+            pagination_status,
+            room_id.to_owned(),
+            auto_shrink_sender,
+            update_sender,
+        );
+
+        // If at least one event has been loaded, it means there is a timeline. Let's
+        // emit a generic update.
+        if timeline_is_not_empty {
+            let _ = generic_update_sender
+                .send(room::RoomEventCacheGenericUpdate { room_id: room_id.to_owned() });
+        }
+
+        Ok(Self { room: room_event_cache })
+    }
+}
 
 /// A diff update for an event cache timeline represented as a vector.
 #[derive(Clone, Debug)]
