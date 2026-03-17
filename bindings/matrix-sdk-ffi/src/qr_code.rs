b5a51aa894777336e5d6e15d3d83744cd4f25083
Damir Jelić <poljar@termina.org.uk>
2026-03-17 13:08:42 +0100

    feat(ffi): Request the MSC4388 variant of the QR code login by default

--- bindings/matrix-sdk-ffi/src/qr_code.rs
+++ bindings/matrix-sdk-ffi/src/qr_code.rs
@@ -120,7 +120,8 @@ impl LoginWithQrCodeHandler {
             .registration_data()
             .map_err(|_| HumanQrLoginError::OAuthMetadataInvalid)?;
 
-        let login = self.oauth.login_with_qr_code(Some(&registration_data)).generate();
+        let mut login = self.oauth.login_with_qr_code(Some(&registration_data)).generate();
+        login.with_msc4388_support();
 
         let mut progress = login.subscribe_to_progress();
 
@@ -214,7 +215,8 @@ impl GrantLoginWithQrCodeHandler {
         self: Arc<Self>,
         progress_listener: Box<dyn GrantGeneratedQrLoginProgressListener>,
     ) -> Result<(), HumanQrGrantLoginError> {
-        let grant = self.oauth.grant_login_with_qr_code().generate();
+        let mut grant = self.oauth.grant_login_with_qr_code().generate();
+        grant.with_msc4388_support();
 
         let mut progress = grant.subscribe_to_progress();
 
