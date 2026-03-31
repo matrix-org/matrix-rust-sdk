255ec9c94c1778c775da09c2ae3424ac5d409049
Hubert Chathi <hubertc@matrix.org>
2026-03-31 14:26:14 -0400

    use derivation, as noted in review

--- crates/matrix-sdk-crypto/src/types/events/secret_push.rs
+++ crates/matrix-sdk-crypto/src/types/events/secret_push.rs
@@ -14,12 +14,18 @@
 
 //! Types for `io.element.msc4385.secret.push` to-device events.
 
+// This is here because we have a zeroize(skip) further below, which incorrectly triggers a
+// unused_assignments warning due to the macro not using a variable.
+//
+// This will be fixed once we bump Zeroize.
+#![allow(unused_assignments)]
+
 use std::collections::BTreeMap;
 
 use ruma::events::secret::request::SecretName;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use zeroize::Zeroize;
+use zeroize::{Zeroize, ZeroizeOnDrop};
 
 use super::{EventType, ToDeviceEvent};
 
@@ -30,14 +36,16 @@ pub type SecretPushEvent = ToDeviceEvent<SecretPushContent>;
 ///
 /// Sent by a client to push a secret with another device. It must be encrypted
 /// as an `m.room.encrypted` event, then sent as a to-device event.
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize, Zeroize, ZeroizeOnDrop)]
 pub struct SecretPushContent {
     /// The name of the secret.
+    #[zeroize(skip)]
     pub name: SecretName,
     /// The contents of the secret.
     pub secret: String,
     /// Any other, custom and non-specced fields of the content.
     #[serde(flatten)]
+    #[zeroize(skip)]
     other: BTreeMap<String, Value>,
 }
 
@@ -45,18 +53,6 @@ impl SecretPushContent {
     /// Create a new `io.element.msc4385.secret.push` content.
     pub fn new(name: SecretName, secret: String) -> Self {
         Self { name, secret, other: Default::default() }
-    }
-}
-
-impl Zeroize for SecretPushContent {
-    fn zeroize(&mut self) {
-        self.secret.zeroize();
-    }
-}
-
-impl Drop for SecretPushContent {
-    fn drop(&mut self) {
-        self.zeroize()
     }
 }
 
