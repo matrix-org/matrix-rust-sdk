f1716d04df365f1ddea2c495aa296fa4c9153c1b
Jorge Martín <jorgem@element.io>
2026-04-24 09:07:05 +0200

    refactor(sdk-ui): Apply different logic for the `People` room list filter based on the selected enum case
    - `MatrixSpec` will keep the current behaviour, checking if the room is direct and has a single target.
    - `TwoMembers` will also try to check there are at most 2 non-service members in the room.

--- crates/matrix-sdk-ui/src/room_list_service/filters/category.rs
+++ crates/matrix-sdk-ui/src/room_list_service/filters/category.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use matrix_sdk_base::DmRoomDefinition;
+
 use super::{super::RoomListItem, Filter};
 
 /// An enum to represent whether a room is about “people” (strictly 2 users) or
@@ -38,18 +40,46 @@ fn matches<F>(number_of_direct_targets: F, room: &RoomListItem, expected_kind: R
 where
     F: Fn(&RoomListItem) -> Option<DirectTargetsLength>,
 {
-    let kind = match number_of_direct_targets(room) {
-        // If 1, we are sure it's a direct room between two users. It's the strict
-        // definition of the `People` category, all good.
-        Some(1) => RoomCategory::People,
-
-        // If smaller than 1, we are not sure it's a direct room, it's then a `Group`.
-        // If greater than 1, we are sure it's a direct room but not between
-        // two users, so it's a `Group` based on our expectation.
-        Some(_) => RoomCategory::Group,
-
+    let client = room.client();
+    let dm_room_definition = client.dm_room_definition();
+    let Some(targets) = number_of_direct_targets(room) else {
         // Don't know.
-        None => return false,
+        return false;
+    };
+    let kind = match dm_room_definition {
+        DmRoomDefinition::MatrixSpec => {
+            if targets == 1 {
+                // If there is a single target, it's a direct room.
+                RoomCategory::People
+            } else {
+                // If smaller than 1, we are not sure it's a direct room, it's then a `Group`.
+                // If greater than 1, we are sure it's a direct room but not between
+                // two users, so it's a `Group` based on our expectation.
+                RoomCategory::Group
+            }
+        }
+        DmRoomDefinition::TwoMembers => {
+            // We can't fully rely on the `service_members` value, as it only contains the
+            // user ids that should be considered service members in the room,
+            // not the service members that are active in the room. We can only
+            // make a good guess and assume those service members are in the room.
+            // Note this can return false positives if some service members aren't in the
+            // room, but they were replaced by other members.
+            let service_member_count =
+                room.service_members().map(|members| members.len()).unwrap_or_default() as u64;
+            let active_non_service_members =
+                room.active_members_count().saturating_sub(service_member_count);
+            if targets == 1 && active_non_service_members <= 2 {
+                // If lower than or equal to 2, we are sure it's a direct room between two
+                // users.
+                RoomCategory::People
+            } else {
+                // If smaller than 1, we are not sure it's a direct room, it's then a `Group`.
+                // If greater than 1, we are sure it's a direct room but not between
+                // two users, so it's a `Group` based on our expectation.
+                RoomCategory::Group
+            }
+        }
     };
 
     kind == expected_kind
@@ -68,14 +98,14 @@ pub fn new_filter(expected_category: RoomCategory) -> impl Filter {
 mod tests {
     use std::ops::Not;
 
-    use matrix_sdk::test_utils::mocks::MatrixMockServer;
-    use matrix_sdk_test::async_test;
+    use matrix_sdk::test_utils::mocks::{AnyRoomBuilder, MatrixMockServer};
+    use matrix_sdk_test::{JoinedRoomBuilder, async_test};
     use ruma::room_id;
 
     use super::{super::new_rooms, *};
 
     #[async_test]
-    async fn test_kind_is_group() {
+    async fn test_kind_is_group_with_matrix_spec_definition() {
         let server = MatrixMockServer::new().await;
         let client = server.client_builder().build().await;
         let [room] = new_rooms([room_id!("!a:b.c")], &client, &server).await;
@@ -98,7 +128,7 @@ mod tests {
     }
 
     #[async_test]
-    async fn test_kind_is_people() {
+    async fn test_kind_is_people_with_matrix_spec_definition() {
         let server = MatrixMockServer::new().await;
         let client = server.client_builder().build().await;
         let [room] = new_rooms([room_id!("!a:b.c")], &client, &server).await;
@@ -117,6 +147,133 @@ mod tests {
             let expected_kind = RoomCategory::Group;
 
             assert!(matches(number_of_direct_targets, &room, expected_kind).not());
+        }
+    }
+
+    #[async_test]
+    async fn test_kind_is_group_with_two_people_definition() {
+        let server = MatrixMockServer::new().await;
+        let client = server
+            .client_builder()
+            .on_builder(|b| b.dm_room_definition(DmRoomDefinition::TwoMembers))
+            .build()
+            .await;
+        let room_id = room_id!("!a:b.c");
+
+        // We have 3 members in the room.
+        let room = server
+            .sync_room(
+                &client,
+                AnyRoomBuilder::Joined(JoinedRoomBuilder::new(room_id).set_joined_members_count(3)),
+            )
+            .await;
+
+        let number_of_direct_targets = |_room: &RoomListItem| Some(1);
+
+        // It's Group.
+        {
+            let expected_kind = RoomCategory::Group;
+
+            assert!(matches(number_of_direct_targets, &room.into(), expected_kind));
+        }
+    }
+
+    #[async_test]
+    async fn test_kind_is_people_with_two_people_definition() {
+        let server = MatrixMockServer::new().await;
+        let client = server
+            .client_builder()
+            .on_builder(|b| b.dm_room_definition(DmRoomDefinition::TwoMembers))
+            .build()
+            .await;
+        let room_id = room_id!("!a:b.c");
+
+        // The room has 2 members, but it has no direct targets, so it should not be
+        // considered a `People` room.
+        let room = server
+            .sync_room(
+                &client,
+                AnyRoomBuilder::Joined(JoinedRoomBuilder::new(room_id).set_joined_members_count(2)),
+            )
+            .await;
+
+        let number_of_direct_targets = |_room: &RoomListItem| Some(0);
+
+        // It's not People.
+        {
+            let expected_kind = RoomCategory::People;
+
+            assert!(matches(number_of_direct_targets, &room.into(), expected_kind).not());
+        }
+
+        // The room has 2 members, but it has several direct targets, so it should not
+        // be considered a `People` room.
+        let room = server
+            .sync_room(
+                &client,
+                AnyRoomBuilder::Joined(JoinedRoomBuilder::new(room_id).set_joined_members_count(2)),
+            )
+            .await;
+
+        let number_of_direct_targets = |_room: &RoomListItem| Some(43);
+
+        // It's not People.
+        {
+            let expected_kind = RoomCategory::People;
+
+            assert!(matches(number_of_direct_targets, &room.into(), expected_kind).not());
+        }
+
+        // The room has 2 members and a single target, so it should be considered a
+        // `People` room.
+        let room = server
+            .sync_room(
+                &client,
+                AnyRoomBuilder::Joined(JoinedRoomBuilder::new(room_id).set_joined_members_count(2)),
+            )
+            .await;
+
+        let number_of_direct_targets = |_room: &RoomListItem| Some(1);
+
+        // It's People.
+        {
+            let expected_kind = RoomCategory::People;
+
+            assert!(matches(number_of_direct_targets, &room.into(), expected_kind));
+        }
+
+        // The room has 1 member, so it should be considered a `People` room.
+        let room = server
+            .sync_room(
+                &client,
+                AnyRoomBuilder::Joined(JoinedRoomBuilder::new(room_id).set_joined_members_count(1)),
+            )
+            .await;
+
+        let number_of_direct_targets = |_room: &RoomListItem| Some(1);
+
+        // It's People.
+        {
+            let expected_kind = RoomCategory::People;
+
+            assert!(matches(number_of_direct_targets, &room.into(), expected_kind));
+        }
+
+        // The room has no members, so it should be considered a `People` room.
+        let room = server
+            .sync_room(
+                &client,
+                AnyRoomBuilder::Joined(JoinedRoomBuilder::new(room_id).set_joined_members_count(0)),
+            )
+            .await;
+
+        let number_of_direct_targets = |_room: &RoomListItem| Some(1);
+
+        // It's People.
+        {
+            let expected_kind = RoomCategory::People;
+
+            assert!(matches(number_of_direct_targets, &room.into(), expected_kind));
         }
     }
 
