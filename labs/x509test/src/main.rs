a059caf9ef69185a001de5c586d8cb9b519267b4
Richard van der Hoff <richard@matrix.org>
2026-04-29 12:19:32 +0100

    verify signatures

--- labs/x509test/src/main.rs
+++ labs/x509test/src/main.rs
@@ -5,8 +5,9 @@ use rustls::{
     crypto::{CryptoProvider, aws_lc_rs},
     pki_types::{CertificateDer, PrivateKeyDer, UnixTime, pem::PemObject},
     server::WebPkiClientVerifier,
-    sign::{Signer, SigningKey},
+    sign::SigningKey,
 };
+use webpki::EndEntityCert;
 
 // TODO: how to load from a PKCS12 bundle? cms::encrypted_data, I think
 const KEY_BUNDLE: &[u8] = include_bytes!("bundle.p12");
@@ -14,6 +15,21 @@ const KEY_BUNDLE: &[u8] = include_bytes!("bundle.p12");
 const CA_CERT: &[u8] = include_bytes!("cacert.pem");
 const CERT_BUNDLE_PEM: &[u8] = include_bytes!("cert.pem");
 const PRIVATE_KEY_PEM: &[u8] = include_bytes!("key.pem");
+
+/*
+
+{"signatures": {"@user:domain": {
+  "x509:<key_id>": {
+    "certificates": [
+      "<pem-encoded-device-cert>",
+      "<pem-encoded-intermediate-cert>"
+    ],
+    "signature_scheme": 0x806, // A number from https://www.iana.org/assignments/tls-parameters/tls-parameters.xhtml#tls-signaturescheme
+    "signature": <...>
+  }
+}}}
+
+ */
 
 fn main() {
     aws_lc_rs::default_provider().install_default().expect("unable to install default provider");
@@ -44,7 +60,7 @@ fn verify(sig: Vec<u8>) {
             .add(result.expect("Unable to parse certificate in root store"))
             .expect("Unable to add certificate to root store");
     }
-    let verifier = WebPkiClientVerifier::builder(Arc::new(root_store))
+    let cert_verifier = WebPkiClientVerifier::builder(Arc::new(root_store))
         //.with_crls(...)
         .build()
         .unwrap();
@@ -55,10 +71,26 @@ fn verify(sig: Vec<u8>) {
     let end_cert = cert_iter.next().expect("Empty certificate chain");
     let intermediate_certs: Vec<_> = cert_iter.collect();
 
-    verifier
+    cert_verifier
         .verify_client_cert(&end_cert, intermediate_certs.as_ref(), UnixTime::now())
         .expect("Unable to verify client certificate");
 
     // TODO: verify that the end cert is valid for the user id in question
-    // TODO: verify signature from the end cert
+
+    let provider = CryptoProvider::get_default().expect("unable to get default provider");
+
+    let alg = provider
+        .signature_verification_algorithms
+        .mapping
+        .iter()
+        .filter(|item| item.0 == SignatureScheme::RSA_PSS_SHA512)
+        .filter_map(|item| item.1.get(0).map(|i| i.deref()))
+        .next()
+        .expect("Unable to find RSA_PSS_SHA512 in provider");
+
+    let cert = EndEntityCert::try_from(&end_cert).expect("Unable to parse end entity cert");
+    let subject = cert.subject();
+
+    println!("subject: {:?}", vodozemac::base64_encode(subject));
+    cert.verify_signature(alg, b"hello world", &sig).expect("Unable to verify signature");
 }
