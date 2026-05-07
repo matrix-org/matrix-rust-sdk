66c4e70ff5983521ed8c9e80fce6aceacaa9a9c0
Jorge Martín <jorgem@element.io>
2026-05-07 13:19:08 +0200

    dependencies: Update `jni` to `0.22.4`
    Also fix breaking API changes

--- bindings/matrix-sdk-ffi/src/platform/android_platform.rs
+++ bindings/matrix-sdk-ffi/src/platform/android_platform.rs
@@ -1,9 +1,7 @@
 use std::{error::Error, mem::MaybeUninit};
 
-use jni::{
-    errors::JniError,
-    sys::{JNI_OK, JavaVM as RawJavaVM},
-};
+use jni::{errors::JniError, jni_sig, jni_str, sys::JNI_OK};
+use jni::sys::JavaVM;
 use tracing::debug;
 
 static ANDROID_JVM: once_cell::sync::OnceCell<jni::JavaVM> = once_cell::sync::OnceCell::new();
@@ -18,10 +16,9 @@ pub(crate) fn init() {
         match get_java_vm() {
             Ok(jvm) => {
                 // Initialize rustls platform verifier
-                let mut env =
-                    jvm.attach_current_thread_permanently().expect("Failed to attach thread");
-                init_rustls_platform_verifier(&mut env)
-                    .expect("Failed to initialize rustls platform verifier");
+                jvm.attach_current_thread(|env| {
+                    init_rustls_platform_verifier(env)
+                }).expect("Failed to initialize rustls platform verifier");
 
                 debug!("Android platform support initialized successfully");
 
@@ -41,39 +38,41 @@ fn get_java_vm() -> Result<jni::JavaVM, Box<dyn Error>> {
         jvm_getter::find_jni_get_created_java_vms().expect("Failed to find JNI_GetCreatedJavaVMs")
     };
 
-    let mut vm: MaybeUninit<*mut RawJavaVM> = MaybeUninit::uninit();
+    let mut vm: MaybeUninit<*mut JavaVM> = MaybeUninit::uninit();
     let status = unsafe { JNI_GetCreatedJavaVMs(vm.as_mut_ptr(), 1, &mut 0) };
     if status != JNI_OK {
         panic!("no JavaVM was found by JNI_GetCreatedJavaVMs");
     }
 
-    unsafe { jni::JavaVM::from_raw(vm.assume_init()).map_err(|e| e.into()) }
+    Ok(unsafe { jni::JavaVM::from_raw(vm.assume_init()) })
 }
 
-fn init_rustls_platform_verifier(env: &mut jni::JNIEnv<'_>) -> jni::errors::Result<()> {
+fn init_rustls_platform_verifier(env: &mut jni::Env<'_>) -> jni::errors::Result<()> {
     // Get the current activity thread
+    let class = env
+        .find_class(jni_str!("android/app/ActivityThread"))?;
     let activity_thread = env
         .call_static_method(
-            "android/app/ActivityThread",
-            "currentActivityThread",
-            "()Landroid/app/ActivityThread;",
+            class,
+            jni_str!("currentActivityThread"),
+            jni_sig!("()Landroid/app/ActivityThread;"),
             &[],
         )?
         .l()?;
 
     // Then get the application context
     let context = env
-        .call_method(activity_thread, "getApplication", "()Landroid/app/Application;", &[])?
+        .call_method(activity_thread, jni_str!("getApplication"), jni_sig!("()Landroid/app/Application;"), &[])?
         .l()?;
 
-    Ok(rustls_platform_verifier::android::init_hosted(env, context)?)
+    Ok(rustls_platform_verifier::android::init_with_env(env, context)?)
 }
 
 /// Attach the current thread to a JVM one.
 pub(crate) fn android_attach_current_thread_permanently()
--> jni::errors::Result<jni::JNIEnv<'static>> {
+-> jni::errors::Result<()> {
     ANDROID_JVM
         .get()
         .ok_or_else(|| jni::errors::Error::JniCall(JniError::Unknown))?
-        .attach_current_thread_permanently()
+        .attach_current_thread(|_| Ok(()))
 }
