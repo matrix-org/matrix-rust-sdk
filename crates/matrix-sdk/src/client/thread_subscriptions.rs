b01ce21a24c03ccd0233d98f3eea85f5ac2d71ff
Ivan Enderlin <ivan@mnt.io>
2026-05-19 15:04:33 +0200

    fix(sdk): Remove a cyclic reference of `Client` in `ThreadSubscriptionCatchup`.
    This patch fixes cyclic reference of `Client` in
    `ThreadSubscriptionCatchup`.
    
    `ThreadSubscriptionCatchup` starts a task that call
    `thread_subscriptions_catchup_task`. This function captures a clone of
    `ThreadSubscriptionCatchup` (why not…) which contains a `WeakClient`,
    all good, no cycle here! However, the real task (not the function)
    captures `Client` to call `enabled_thread_subscriptions` to know
    if the function `thread_subscriptions_catchup_task` must be called.
    Consequently, the task captures a clone of `Client`, boom, we have
    a cycle.
    
    This patch fixes the problem by spawning the task if and only if
    `Client::enabled_thread_subscriptions` returns `Ok(true)`. This check is
    done outside the task. It seems saner and avoid creating this cycle.
    
    Consequently, the `ThreadSubscriptionCatchup::new` method becomes
    `async`, which is perfectly fine as it was already wrapper inside an
    `async` block when initialised by the `Client`.

--- crates/matrix-sdk/src/client/thread_subscriptions.rs
+++ crates/matrix-sdk/src/client/thread_subscriptions.rs
@@ -131,50 +131,44 @@ pub struct ThreadSubscriptionCatchup {
 }
 
 impl ThreadSubscriptionCatchup {
-    pub fn new(client: Client) -> Arc<Self> {
+    pub async fn new(client: Client) -> Arc<Self> {
         let is_outdated = Arc::new(AtomicBool::new(true));
-
         let weak_client = WeakClient::from_client(&client);
-
         let (ping_sender, ping_receiver) = channel(8);
-
         let uniq_mutex = Arc::new(Mutex::new(()));
 
         let this = Arc::new(Self {
             _task: OnceLock::new(),
             is_outdated,
-            client: weak_client,
+            client: weak_client.clone(),
             ping_sender,
             uniq_mutex,
         });
 
         // Create the task only if the client is configured to handle thread
         // subscriptions.
-        let _ = this._task.get_or_init(|| {
-            let that = this.clone();
-            let client_clone = client.clone();
+        match client.enabled_thread_subscriptions().await {
+            Ok(true) => {
+                let _ = this._task.get_or_init(|| {
+                    let that = this.clone();
 
-            client.task_monitor().spawn_infinite_task("client::thread_subscriptions_catchup", async move {
-                match client_clone.enabled_thread_subscriptions().await {
-                    Ok(enabled) => {
-                        if !enabled {
-                            debug!("Thread subscriptions catchup not enabled, not starting the catchup task");
-                            return;
-                        }
-                    }
+                    client
+                        .task_monitor()
+                        .spawn_infinite_task("client::thread_subscriptions_catchup", async move {
+                            Self::thread_subscriptions_catchup_task(that, ping_receiver).await;
+                        })
+                        .abort_on_drop()
+                });
+            }
 
-                    Err(err) => {
-                        warn!("Failed to check if thread subscriptions catchup is enabled: {err}");
-                        return;
-                    }
-                }
+            Ok(false) => {
+                debug!("Thread subscriptions catchup not enabled, not starting the catchup task");
+            }
 
-                Self::thread_subscriptions_catchup_task(
-                    that,
-                    ping_receiver,
-                ).await
-            }).abort_on_drop()
-        });
+            Err(err) => {
+                warn!("Failed to check if thread subscriptions catchup is enabled: {err}");
+            }
+        }
 
         this
     }
