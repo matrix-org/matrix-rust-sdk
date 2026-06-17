45d25c6be9386b9919e3827cc986f87ee3ac4aae
Richard van der Hoff <richard@matrix.org>
2026-06-17 16:26:48 +0100

    cms verification

--- labs/x509test/src/main.rs
+++ labs/x509test/src/main.rs
@@ -1,4 +1,4 @@
-use std::{ops::Deref, sync::Arc};
+use std::{collections::BTreeMap, ops::Deref, sync::Arc};
 
 use cms::{
     cert::CertificateChoices,
@@ -17,14 +17,16 @@ use rustls::{
     server::WebPkiClientVerifier,
     sign::SigningKey,
 };
+use serde::{Deserializer, Serializer};
 use webpki::EndEntityCert;
 use x509_cert::{
     der::{
-        self as der, AnyRef, Decode, Encode, EncodePem, Length, PemReader, Writer,
+        self as der, AnyRef, Decode, DecodePem, Encode, EncodePem, Length, PemReader, Reader,
+        Writer,
         pem::{LineEnding, PemLabel},
     },
     ext::pkix::SubjectKeyIdentifier,
-    spki::{AlgorithmIdentifier, AlgorithmIdentifierRef},
+    spki::{AlgorithmIdentifier, AlgorithmIdentifierOwned, AlgorithmIdentifierRef},
 };
 
 // TODO: how to load from a PKCS12 bundle? cms::encrypted_data, I think
@@ -35,10 +37,10 @@ const CERT_BUNDLE_PEM: &[u8] = include_bytes!("cert.pem");
 const PRIVATE_KEY_PEM: &[u8] = include_bytes!("key.pem");
 
 /*
-openssl cms -sign -md sha512 -in ../../README.md -signer src/cert.pem -inkey src/key.pem \
-  -certfile src/cacert.pem -outform PEM -keyopt rsa_padding_mode:pss  > src/cms.pem
+openssl cms -sign -md sha512 -in <(echo -n "hello world") -signer src/cert.pem -inkey src/key.pem \
+  -certfile src/cacert.pem -outform PEM -noattr -keyopt rsa_padding_mode:pss  > src/cms.pem
 
-openssl cms -verify -CAfile src/cacert.pem -inform PEM -in src/cms.pem -content ../../README.md
+openssl cms -verify -CAfile src/cacert.pem -inform PEM -in src/cms.pem -content <(echo -n "hello world")
 */
 const CMS_PEM: &[u8] = include_bytes!("cms.pem");
 
@@ -59,9 +61,39 @@ const CMS_PEM: &[u8] = include_bytes!("cms.pem");
 
 fn main() {
     // dump_cms_file(CMS_PEM);
-    default_provider().install_default().expect("unable to install default provider");
+    verify_content_info_pem(CMS_PEM);
+
+    //default_provider().install_default().expect("unable to install default
+    // provider");
     let sig = build_signature();
 
+    let content_info_pem = build_content_info_pem(sig);
+
+    /*
+    let mut test = BTreeMap::new();
+    test.insert("1", content_info_pem);
+    print!("{}", serde_json::to_string(&test).unwrap());
+    */
+
+    //print!("{}", content_info_pem);
+
+    verify_content_info_pem(content_info_pem.as_bytes());
+}
+
+/// upload side
+fn build_signature() -> Vec<u8> {
+    let provider = default_provider();
+    let private_key =
+        PrivateKeyDer::from_pem_slice(PRIVATE_KEY_PEM).expect("unable to parse private key");
+    let private_key: Arc<dyn SigningKey> =
+        provider.key_provider.load_private_key(private_key).expect("unable to load private key");
+    let signer = private_key
+        .choose_scheme(&[SignatureScheme::RSA_PSS_SHA512])
+        .expect("unable to choose signature scheme");
+    signer.sign(b"hello world").expect("unable to sign")
+}
+
+fn build_content_info_pem(sig: Vec<u8>) -> String {
     let chain = x509_cert::Certificate::load_pem_chain(CERT_BUNDLE_PEM).unwrap();
 
     let leaf_cert = &chain[0].tbs_certificate;
@@ -143,14 +175,9 @@ fn main() {
         content_type: ID_SIGNED_DATA,
         content: der::Any::encode_from(&signed_data).unwrap(),
     };
-    let content_info = ContentInfoWrapper(content_info);
-    print!("{}", content_info.to_pem(LineEnding::CRLF).unwrap());
-
-    //content_info.to_pem();
-    // let mut pem_writer = PemWriter::new().unwrap();
-    // content_info.encode(&pem_writer)
-
-    //verify(sig);
+    let content_info = X509Signature(content_info);
+    let content_info_pem = content_info.to_pem(LineEnding::CRLF).unwrap();
+    content_info_pem
 }
 
 fn debug_cms_file(pem: &[u8]) {
@@ -176,13 +203,13 @@ fn debug_cms_file(pem: &[u8]) {
 }
 
 /// A wrapper for [`ContentInfo`] which implements [`PemLabel`] and can
-/// therefore be used with [`EncodePem`].
-struct ContentInfoWrapper(ContentInfo);
+/// therefore be used with [`EncodePem`] and [`DecodePem`].
+struct X509Signature(ContentInfo);
 
-impl PemLabel for ContentInfoWrapper {
+impl PemLabel for X509Signature {
     const PEM_LABEL: &'static str = "CMS";
 }
-impl Encode for ContentInfoWrapper {
+impl Encode for X509Signature {
     fn encoded_len(&self) -> der::Result<Length> {
         self.0.encoded_len()
     }
@@ -192,21 +219,37 @@ impl Encode for ContentInfoWrapper {
     }
 }
 
-/// upload side
-fn build_signature() -> Vec<u8> {
-    let provider = default_provider();
-    let private_key =
-        PrivateKeyDer::from_pem_slice(PRIVATE_KEY_PEM).expect("unable to parse private key");
-    let private_key: Arc<dyn SigningKey> =
-        provider.key_provider.load_private_key(private_key).expect("unable to load private key");
-    let signer = private_key
-        .choose_scheme(&[SignatureScheme::RSA_PSS_SHA512])
-        .expect("unable to choose signature scheme");
-    signer.sign(b"hello world").expect("unable to sign")
+impl serde::Serialize for X509Signature {
+    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(
+            self.to_pem(LineEnding::CRLF)
+                .map_err(|e| serde::ser::Error::custom(e.to_string()))?
+                .as_str(),
+        )
+    }
+}
+
+impl<'b> Decode<'b> for X509Signature {
+    fn decode<R: Reader<'b>>(decoder: &mut R) -> der::Result<Self> {
+        Ok(X509Signature(ContentInfo::decode(decoder)?))
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for X509Signature {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Self::from_pem(s.as_bytes()).map_err(|e| serde::de::Error::custom(e.to_string()))
+    }
 }
 
 /// verifier side
-fn verify(sig: Vec<u8>) {
+fn verify(sig: &[u8], certificate_chain_pem: &str) {
     // Load the trusted root certificates into a verifier. These would come from
     // local configuration.
     let mut root_store = RootCertStore::empty();
@@ -221,7 +264,7 @@ fn verify(sig: Vec<u8>) {
         .unwrap();
 
     // Verify the certificate chain presented by the device
-    let mut cert_iter = CertificateDer::pem_slice_iter(CERT_BUNDLE_PEM)
+    let mut cert_iter = CertificateDer::pem_slice_iter(certificate_chain_pem.as_bytes())
         .map(|r| r.expect("Unable to parse cert in cert chain"));
     let end_cert = cert_iter.next().expect("Empty certificate chain");
     let intermediate_certs: Vec<_> = cert_iter.collect();
@@ -250,6 +293,121 @@ fn verify(sig: Vec<u8>) {
     let cert = EndEntityCert::try_from(&end_cert).expect("Unable to parse end entity cert");
     let subject = cert.subject();
 
-    println!("subject: {:?}", vodozemac::base64_encode(subject));
-    cert.verify_signature(alg, b"hello world", &sig).expect("Unable to verify signature");
+    cert.verify_signature(alg, b"hello world", sig).expect("Unable to verify signature");
+}
+
+fn verify_content_info_pem(pem: &[u8]) {
+    // let mut pem_reader = PemReader::new(pem.as_bytes()).unwrap();
+    // dbg!(pem_reader.type_label());
+
+    let content_info = X509Signature::from_pem(pem).unwrap().0;
+    if content_info.content_type != ID_SIGNED_DATA {
+        panic!("ContentInfo should be ID_SIGNED_DATA");
+    }
+    let data: SignedData = content_info.content.decode_as().expect("Could not parse SignedData");
+    // TODO? check version?
+    // TODO? check digest_algorithms?
+    if data.encap_content_info.econtent_type != const_oid::db::rfc5911::ID_DATA {
+        panic!("EncapsulatedContentInfo should be ID_DATA");
+    }
+    if data.encap_content_info.econtent.is_some() {
+        panic!("EncapsulatedContentInfo.content should be None");
+    }
+
+    let certificates = data.certificates.expect("No certificates");
+    let leaf_cert = match certificates.0.get(0).expect("Expected at least one certificate") {
+        CertificateChoices::Certificate(c) => c,
+        CertificateChoices::Other(_) => {
+            panic!("CertificateChoices should be Certificate");
+        }
+    };
+    let leaf_ski: SubjectKeyIdentifier = leaf_cert.tbs_certificate.get().unwrap().unwrap().1;
+
+    let cert_pems = certificates
+        .0
+        .iter()
+        .map(|cert| match cert {
+            CertificateChoices::Certificate(c) => c.to_pem(LineEnding::CRLF).unwrap(),
+            CertificateChoices::Other(_) => {
+                panic!("CertificateChoices should be Certificate");
+            }
+        })
+        .collect::<Vec<_>>()
+        .join("\r\n");
+
+    if data.crls.is_some() {
+        panic!("CRLs should be None");
+    }
+
+    if data.signer_infos.0.len() != 1 {
+        panic!("SignerInfo should have exactly one entry");
+    }
+
+    let signer_info = data.signer_infos.0.get(0).unwrap();
+    // TODO: check version?
+    match &signer_info.sid {
+        SignerIdentifier::IssuerAndSerialNumber(_) => {
+            panic!("SignerIdentifier should be SubjectKeyIdentifier")
+        }
+        SignerIdentifier::SubjectKeyIdentifier(ski) => {
+            if *ski != leaf_ski {
+                panic!("SignerIdentifier should match SubjectKeyIdentifier of leaf certificate")
+            }
+        }
+    };
+
+    check_sha512_algorithm_owned(&signer_info.digest_alg);
+
+    if signer_info.signed_attrs.is_some() {
+        panic!("SignerInfo.signed_attrs should be None");
+    }
+
+    if signer_info.signature_algorithm.oid != const_oid::db::rfc5912::ID_RSASSA_PSS {
+        panic!("SignerInfo.signature_algorithm.oid should be RSASSA_PSS");
+    }
+
+    let params = signer_info
+        .signature_algorithm
+        .parameters
+        .as_ref()
+        .expect("SignerInfo.signature_algorithm.params should be set");
+    let params: RsaPssParams<'_> =
+        params.decode_as().expect("Could not parse SignatureAlgorithm.parameters as RsaPssParams");
+
+    check_sha512_algorithm_ref(params.hash);
+
+    if params.mask_gen.oid != const_oid::db::rfc5912::ID_MGF_1 {
+        panic!("RsaPssParams.mask_gen should be ID_MGF_1");
+    }
+
+    check_sha512_algorithm_ref(
+        params.mask_gen.parameters.expect("mask_gen.parameters must be set"),
+    );
+
+    if params.salt_len != 64 {
+        panic!("salt_len should be 64");
+    }
+
+    verify(signer_info.signature.as_bytes(), &cert_pems);
+}
+
+fn check_sha512_algorithm_owned(hash_algorithm: &AlgorithmIdentifierOwned) {
+    if hash_algorithm.oid != const_oid::db::rfc5912::ID_SHA_512 {
+        panic!("hash.oid should be ID_SHA_512");
+    }
+    match hash_algorithm.parameters {
+        None => {}
+        Some(_) => panic!("hash.parameters should be None"),
+    }
+}
+
+fn check_sha512_algorithm_ref(hash_algorithm: AlgorithmIdentifierRef<'_>) {
+    if hash_algorithm.oid != const_oid::db::rfc5912::ID_SHA_512 {
+        panic!("hash.oid should be ID_SHA_512");
+    }
+    match hash_algorithm.parameters {
+        None => {}
+        Some(AnyRef::NULL) => {}
+        Some(_) => panic!("hash.parameters should be None"),
+    }
 }
