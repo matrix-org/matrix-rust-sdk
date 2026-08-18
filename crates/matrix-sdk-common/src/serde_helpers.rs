6d1fd57cb5f935dec607bf7d2cc782fd5d0fa684
Ivan Enderlin <ivan@mnt.io>
2026-08-18 10:44:03 +0200

    refactor(sdk): Move `extract_read_receipt` to `matrix_sdk_common::serde_helpers`.
    This patch moves `extract_read_receipt` from
    `matrix_sdk::event_cache::caches::room::state` to
    `matrix_sdk_common::serde_helpers`. This way, it can be re-used
    elsewhere, like in `matrix_sdk::event_cache::caches::thread::state` ;-).

--- crates/matrix-sdk-common/src/serde_helpers.rs
+++ crates/matrix-sdk-common/src/serde_helpers.rs
@@ -18,14 +18,16 @@
 use ruma::{
     MilliSecondsSinceUnixEpoch, OwnedEventId,
     events::{
-        AnyMessageLikeEventContent, AnySyncMessageLikeEvent, AnySyncTimelineEvent,
-        MessageLikeEventType,
+        AnyMessageLikeEventContent, AnySyncEphemeralRoomEvent, AnySyncMessageLikeEvent,
+        AnySyncTimelineEvent, MessageLikeEventType,
+        receipt::{ReceiptEventContent, SyncReceiptEvent},
         relation::{BundledThread, RelationType},
     },
     room_version_rules::RedactionRules,
     serde::Raw,
 };
 use serde::Deserialize;
+use tracing::error;
 
 use crate::deserialized_responses::{ThreadSummary, ThreadSummaryStatus};
 
@@ -159,6 +161,28 @@ pub fn extract_timestamp(
     }
 
     Some(origin_server_ts)
+}
+
+/// Extract the first valid read receipt event from a list of ephemeral events,
+/// if available.
+pub fn extract_read_receipt(
+    ephemeral_events: &[Raw<AnySyncEphemeralRoomEvent>],
+) -> Option<ReceiptEventContent> {
+    for raw_ephemeral in ephemeral_events {
+        match raw_ephemeral.deserialize() {
+            Ok(AnySyncEphemeralRoomEvent::Receipt(SyncReceiptEvent { content, .. })) => {
+                return Some(content);
+            }
+
+            Ok(_) => {}
+
+            Err(err) => {
+                error!(%err, "error when deserializing an ephemeral event");
+            }
+        }
+    }
+
+    None
 }
 
 #[cfg(test)]
