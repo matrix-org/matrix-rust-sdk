c245e3a3df208b8c55aa30ee1c7351d03f3d56fd
Michael Goldenberg <m@mgoldenberg.net>
2026-08-18 11:28:06 -0400

    feat(indexeddb): allow add-put-delete calls to be awaited at transaction commit
    Signed-off-by: Michael Goldenberg <m@mgoldenberg.net>

--- crates/matrix-sdk-indexeddb/src/event_cache_store/transaction.rs
+++ crates/matrix-sdk-indexeddb/src/event_cache_store/transaction.rs
@@ -149,8 +149,8 @@ impl<'a> IndexeddbEventCacheStoreTransaction<'a> {
     /// exists, it will be overwritten. When the item is successfully put, the
     /// function returns the intermediary type [`IndexedLease`] in case
     /// inspection is needed.
-    pub async fn put_lease(&self, lease: &Lease) -> Result<IndexedLease, TransactionError> {
-        self.put_item(lease).await
+    pub fn put_lease(&self, lease: &Lease) -> Result<IndexedLease, TransactionError> {
+        self.put_item(lease)
     }
 
     /// Query IndexedDB for chunks that match the given chunk identifier and the
@@ -257,7 +257,7 @@ impl<'a> IndexeddbEventCacheStoreTransaction<'a> {
     /// function returns the intermediary type [`IndexedChunk`] in case
     /// inspection is needed.
     pub async fn add_chunk(&self, chunk: &Chunk) -> Result<IndexedChunk, TransactionError> {
-        let indexed = self.add_item(chunk).await?;
+        let indexed = self.add_item(chunk)?;
         if let Some(previous) = chunk.previous {
             let previous_identifier = ChunkIdentifier::new(previous);
             let mut previous_chunk = self
@@ -265,7 +265,7 @@ impl<'a> IndexeddbEventCacheStoreTransaction<'a> {
                 .await?
                 .ok_or(TransactionError::ItemNotFound)?;
             previous_chunk.next = Some(chunk.identifier);
-            self.put_item(&previous_chunk).await?;
+            self.put_item(&previous_chunk)?;
         }
         if let Some(next) = chunk.next {
             let next_identifier = ChunkIdentifier::new(next);
@@ -274,7 +274,7 @@ impl<'a> IndexeddbEventCacheStoreTransaction<'a> {
                 .await?
                 .ok_or(TransactionError::ItemNotFound)?;
             next_chunk.previous = Some(chunk.identifier);
-            self.put_item(&next_chunk).await?;
+            self.put_item(&next_chunk)?;
         }
         Ok(indexed)
     }
@@ -295,7 +295,7 @@ impl<'a> IndexeddbEventCacheStoreTransaction<'a> {
                     self.get_chunk_by_id(linked_chunk_id, previous_identifier).await?
                 {
                     previous_chunk.next = chunk.next;
-                    self.put_item(&previous_chunk).await?;
+                    self.put_item(&previous_chunk)?;
                 }
             }
             if let Some(next) = chunk.next {
@@ -304,7 +304,7 @@ impl<'a> IndexeddbEventCacheStoreTransaction<'a> {
                     self.get_chunk_by_id(linked_chunk_id, next_identifier).await?
                 {
                     next_chunk.previous = chunk.previous;
-                    self.put_item(&next_chunk).await?;
+                    self.put_item(&next_chunk)?;
                 }
             }
             self.delete_item_by_key::<Chunk, IndexedChunkIdKey>((linked_chunk_id, chunk_id))
@@ -461,9 +461,9 @@ impl<'a> IndexeddbEventCacheStoreTransaction<'a> {
 
         let indexed =
             if matches!(event, Event::InBand(_)) && matches!(existing, Some(Event::OutOfBand(_))) {
-                self.put_item(event).await?
+                self.put_item(event)?
             } else {
-                self.add_item(event).await?
+                self.add_item(event)?
             };
 
         self.update_events_by_event_id(event_id, |existing| {
@@ -487,7 +487,7 @@ impl<'a> IndexeddbEventCacheStoreTransaction<'a> {
             return Err(TransactionError::Serialization(Box::new(IndexedEventError::NoEventId)));
         };
 
-        let indexed = self.put_item(event).await?;
+        let indexed = self.put_item(event)?;
 
         self.update_events_by_event_id(event_id, |existing| {
             existing.with_content(event.content().clone())
@@ -641,11 +641,8 @@ impl<'a> IndexeddbEventCacheStoreTransaction<'a> {
     }
 
     /// Update a thread info.
-    pub async fn update_thread_info(
-        &self,
-        thread: &Thread,
-    ) -> Result<IndexedThread, TransactionError> {
-        self.put_item(thread).await
+    pub fn update_thread_info(&self, thread: &Thread) -> Result<IndexedThread, TransactionError> {
+        self.put_item(thread)
     }
 
     /// List all threads (remembered with [`Self::update_thread_info`]) for a
