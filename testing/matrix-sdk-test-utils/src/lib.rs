82861a146a47a77fdb2d625f2ff8923d30a824ab
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-09-15 16:20:38 +0800

    chore(deps): deduplicate ctor
    Signed-off-by: Yorusaka Miyabi <23130178+ShadowRZ@users.noreply.github.com>

--- testing/matrix-sdk-test-utils/src/lib.rs
+++ testing/matrix-sdk-test-utils/src/lib.rs
@@ -10,24 +10,26 @@
 macro_rules! init_tracing_for_tests {
     () => {
         #[cfg(not(target_family = "wasm"))]
-        #[$crate::__macro_support::ctor]
-        fn init_logging() {
-            use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-            use $crate::__macro_support::tracing_subscriber;
+        $crate::__macro_support::ctor! {
+            #[ctor(unsafe)]
+            fn init_logging() {
+                use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+                use $crate::__macro_support::tracing_subscriber;
 
-            tracing_subscriber::registry()
-                .with(tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                    // Output is only printed for failing tests, but still we shouldn't overload
-                    // the output with unnecessary info. When debugging a specific test, it's easy
-                    // to override this default by setting the `RUST_LOG` environment variable.
-                    //
-                    // Since tracing_subscriber does prefix matching, the `matrix_sdk=` directive
-                    // takes effect for all the main crates (`matrix_sdk_base`, `matrix_sdk_crypto`
-                    // and so on).
-                    "info,matrix_sdk=debug".into()
-                }))
-                .with(tracing_subscriber::fmt::layer().with_test_writer())
-                .init();
+                tracing_subscriber::registry()
+                    .with(tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                        // Output is only printed for failing tests, but still we shouldn't overload
+                        // the output with unnecessary info. When debugging a specific test, it's easy
+                        // to override this default by setting the `RUST_LOG` environment variable.
+                        //
+                        // Since tracing_subscriber does prefix matching, the `matrix_sdk=` directive
+                        // takes effect for all the main crates (`matrix_sdk_base`, `matrix_sdk_crypto`
+                        // and so on).
+                        "info,matrix_sdk=debug".into()
+                    }))
+                    .with(tracing_subscriber::fmt::layer().with_test_writer())
+                    .init();
+            }
         }
     };
 }
@@ -35,7 +37,7 @@ macro_rules! init_tracing_for_tests {
 #[doc(hidden)]
 pub mod __macro_support {
     #[cfg(not(target_family = "wasm"))]
-    pub use ctor::ctor;
+    pub use ctor::declarative::ctor;
     #[cfg(not(target_family = "wasm"))]
     pub use tracing_subscriber;
 }
