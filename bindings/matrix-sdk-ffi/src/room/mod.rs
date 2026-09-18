a36b4f12017f1554901873b0b965c065e4487b6a
bxdxnn <267911624+bxdxnn@users.noreply.github.com>
2026-09-18 08:24:47 +0000

    feat(ffi): Add Circles (MSC4546) support
    Signed-off-by: bxdxnn <267911624+bxdxnn@users.noreply.github.com>

--- bindings/matrix-sdk-ffi/src/room/mod.rs
+++ bindings/matrix-sdk-ffi/src/room/mod.rs
@@ -1731,6 +1731,7 @@ impl From<SdkDraftAttachment> for DraftAttachment {
                         thumbnail_info,
                         thumbnail_source: None,
                         blurhash,
+                        circle: None,
                     },
                     source: UploadSource::Data { bytes: data, filename: value.filename },
                     thumbnail_source,
